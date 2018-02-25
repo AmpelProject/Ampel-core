769983c31549f2faf264a949ea9cc32890adcad7
vbrinnel <vsbrin@gmail.com>
2018-02-25 16:19:45 +0100

    Much improved LightCurveLoader class

--- src/ampel/pipeline/common/LightCurveLoader.py
+++ src/ampel/pipeline/common/LightCurveLoader.py
@@ -3,7 +3,7 @@
 # File              : ampel/pipeline/common/LightCurveLoader.py
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 13.01.2018
-# Last Modified Date: 25.01.2018
+# Last Modified Date: 15.02.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from ampel.flags.PhotoPointFlags import PhotoPointFlags
@@ -12,162 +12,197 @@ from ampel.flags.FlagUtils import FlagUtils
 from ampel.base.PhotoPoint import PhotoPoint
 from ampel.base.LightCurve import LightCurve
 from werkzeug.datastructures import ImmutableList
+from ampel.pipeline.logging.LoggingUtils import LoggingUtils
+from ampel.pipeline.common.DBQueryResultOrganizer import DBQueryResultOrganizer
 import logging
 
 
 class LightCurveLoader:
 	"""
-	Class with static methods all returning an instance of ampel.pipeline.common.LightCurve
-	Either through DB query (load_through_db_query) 
-	or through parsing of DB query results (load_from_db_results, load_from_global_db_results)
-
-	Please see method docstrings for more info
-
-	Note: The function 'load_through_db_query' requires static initialization through 
-	the method set_mongo to work properly. 
-	In short: call LightCurveLoader.set_mongo(...) before using this class and everything will be fine.
+	Class with methods each returning an instance of ampel.pipeline.common.LightCurve
+	Either through DB query (load_through_db_query) or through parsing of DB query results 
 	"""
 
 
-	@classmethod
-	def set_mongo(cls, db):
+	def __init__(self, db, read_only=True, logger=None, collection="main"):
 		"""
-		sets reference to a pymongo.database.Database instance 
-		so that this class can perform DB queries
+		Parameters:
+		-----------
+		db: instance of pymongo.database.Database 
+		read_only: if True, the LightCurve instance returned by the methods of this class will be:
+			* a frozen class
+			* containing a ImmutableList of PhotoPoint
+			* whereby each PhotoPoint is a frozen class as well
+			* and each PhotoPoint dict content is an ImmutableDict
 		"""
-		cls.col = db["main"]
+		self.col = db[collection]
+		self.logger = LoggingUtils.get_logger() if logger is None else logger
+		self.read_only = read_only
 
 
-	@classmethod
-	def load_by_db_query(cls, tran_id, compound_id, logger, read_only=True):
+	def load_from_db(self, tran_id, compound_id):
 		"""
 		Load a lightcurve by performing a DB query and feeding the results 
 		to the method 'load_from_db_results' from this class.
 		This function returns an instance of ampel.pipeline.common.LightCurve
 
 		Parameters:
-		tran_id: transient id 
-		compound_id: compound id
-		read_only: see docstring of method 'load_from_db_results'
+		tran_id: transient id (string)
+		compound_id: compound id (string)
 		"""
 
 		# TODO : provide list or cursor as func parameter ?
 		# T3 will have larger queries (including t3 results)
-		cursor = cls.col.find(
+		cursor = self.col.find(
 			{	
 				"tranId": tran_id, 
 				"$or": [
 					{"alDocType": AlDocTypes.PHOTOPOINT},
-					{"alDocType": AlDocTypes.COMPOUND, "_id": compound_id}
+					{	
+						"alDocType": AlDocTypes.COMPOUND, 
+						"_id": compound_id
+					}
 				]
 			}
 		)
 
-		if (cursor.count()) == 0:
-			logger.warn("No LightCurve found for tranId: "+str(tran_id))
+		if cursor.count() == 0:
+			self.logger.warn("No LightCurve found for tranId: %s " % tran_id)
 			return None
 
-		logger.info(
-			"DB query for tranId: " + str(tran_id) +
-			" and compoundId: "+ compound_id + 
-			" returned " + str(cursor.count()) + " documents"
+		self.logger.info(
+			"DB query for tranId: %s and compoundId: %s returned %i documents" %
+			(tran_id, compound_id, cursor.count())
 		)
 
-		return cls.load_from_targeted_db_results(cursor, read_only)
+		results = list(cursor)
+
+		pps, comp = DBQueryResultOrganizer.get_lightcurve_constituents(
+			results, compound_id=compound_id
+		)
+
+		return self.load_using_results(pps, comp)
 
 
-	@classmethod
-	def load_from_broad_db_results(cls, doc_list, tran_id, compound_id, read_only=True):
+	def load_using_results(self, photopoints, compound_dict, frozen_pps_dict=None):
 		"""
-		Load a lightcurve using db results containing multiple transients.
-		This function is mainly used at the T3 level.
-		It returns an instance of ampel.pipeline.common.LightCurve
+		Creates and returns an instance of ampel.base.LightCurve using db results.
+		This function is used at both T2 and T3 levels 
+		(T3 would typically set the optional 'target' parameter, see below for more info).
 
-		Parameters:
-		doc_list: see docstring of method 'load_from_db_results'
-		tran_id: transient id 
-		compound_id: compound id
-		read_only: see docstring of method 'load_from_db_results'
+		Required parameters:
+		--------------------
 
-		Functionwise:
-		A filtering of the documents contained in doc_list is performed based on tran_id. 
-		All docs with type AlDocTypes.PHOTOPOINT and a single doc with type ==
-		AlDocTypes.COMPOUND and with compound ID == 'compound_id' are appended to a list 
-		and provided to the method 'load_from_db_results'.
-		"""
-		sub_list = []
+		'doc_list': 
+			-> List of documents or pymongo cursor retrieved from mongo DB.
+			-> Typically, it could be: db_collection.find({...}) or list(db_collection.find({...})) 
 
-		for doc in doc_list:
+		Optional parameters:
+		--------------------
 
-			if doc['tranId'] != tran_id:
-				continue
-			
-			if doc["alDocType"] == AlDocTypes.COMPOUND and doc["_id"] == compound_id:
-				sub_list.append(doc)
+		'target':
+			-> Mostly used at the T3 level were broad DB queries are performed 
+			  (T2 is more likely to execute targeted queries)
+			-> Must be a dict containing both the keys 'tran_id' and 'compound_id'
+			-> a filtering of the documents in doc_list is performed based on tran_id. 
+			   Docs with type AlDocTypes.PHOTOPOINT and a single doc with 
+			   type == AlDocTypes.COMPOUND and with compound ID == 'compound_id' are used
+			   as basis for instancianting the LightCurve object
 
-			elif doc["alDocType"] == AlDocTypes.PHOTOPOINT:
-				sub_list.append(doc)
-
-		return cls.load_from_targeted_db_results(sub_list, read_only)
-		
-
-	@classmethod
-	def load_from_targeted_db_results(cls, doc_list, read_only=True):
-
-		"""
-		doc_list: 
-		list of documents retrieved from DB.
-		Typically, it could be: 
-			list(
-				db_collection.find({...})
-			)
-
-		read_only: 
-		if True, the LightCurve instance returned by this method will be:
-			* a frozen class
-			* containing a ImmutableList of PhotoPoint
-			* whereby each PhotoPoint is a frozen class as well
-			* and each PhotoPoint dict content is an ImmutableDict
+		'frozen_pps_dict': 
+			Dict that can be provided for optimization purposes, which should contain frozen PhotoPoint instances.
+			-> key: photopoint id 
+			-> value: is the corresponding PhotoPoint instance
+			-> can be empty (will then be populated)
+			-> will be populated in case new photopoint instance(s) is(are) created (i.e pp id not in dict)
+			-> PhotoPoint instances are 're-used' when already existing. 
+			  (rather than instanciating a new PhotoPoint obj for every LightCurve object)
+			-> contains only PhotoPoint instances without custom policy
+			-> will be used only if read_only is True (see class constructor)
 		"""
 
-		comp = None
-		ppd = dict()
-		
-		# Loop through query photopoints
-		for doc in doc_list:
+		# Robustness check 
+		if compound_dict is None:
+			raise ValueError("Required parameter 'compound_dict' cannot be None")
 
-			if doc["alDocType"] == AlDocTypes.COMPOUND:
-				comp = doc
+		if photopoints is None:
+			raise ValueError("Required parameter 'photopoints' cannot be None")
 
-			if doc["alDocType"] == AlDocTypes.PHOTOPOINT:
-				ppd[doc['_id']] = PhotoPoint(doc)
-			
+		if type(photopoints) is dict:
+			pass
 
-		# Check wrong result
-		if comp == None:
-			raise ValueError("Required compound not found")
+		elif type(photopoints) is list:
 
-		# List of *PhotoPoint* object
-		pps_list = []
+			pp_list = photopoints
+			photopoints = {}
+
+			# Loop through photopoints 
+			for pp in pp_list:
+
+				# Build dict
+				photopoints[pp['_id']] = pp
+
+		else:
+			raise ValueError("'photopoints' argument must be an instance of list or dict")
+
+
+		# List of *PhotoPoint* instances
+		al_pps_list = []
 
 		# Loop through compound photopoints ids and options
-		for el in comp['pps']:
+		for el in compound_dict['pps']:
 
-			# Get corresponding photopoint instance from ppd dict
-			al_pp = ppd[el['pp']]
+			# Get corresponding photopoint content (dict) from res_pps dict
+			try:
+				pp_dict = photopoints[el['pp']]
+			except KeyError:
 
-			# Update list
-			pps_list.append(al_pp)
+				self.logger.warn("KeyError exception occured while accessing photopoint %i" % el['pp'])
+				self.logger.info("Trying to recover from this error")
 
-			# If custom options avail (if dict contains more that dict key 'pp')
+				# TODO: populate 'troubleshoot collection'
+				cursor = self.col.find(
+					{
+						"_id": el['pp'], 
+						"alDocType": AlDocTypes.PHOTOPOINT
+					}
+				)
+
+				if (cursor.count()) == 0:
+					self.logger.error("PhotoPoint with id %i not found" % el['pp'])
+					raise ValueError("PhotoPoint with id %i not found" % el['pp'])
+
+				pp_dict = next(cursor)
+
+
+			# If custom options avail (if dict contains more than the dict key 'pp')
 			if (len(el.keys()) > 1):
+				
+				# Create photopoint wrapper instance
+				al_pp = PhotoPoint(pp_dict, read_only=False)
 
 				# Update pp options dict and cast internal to ImmutableDict if required
-				al_pp.set_policy(el, read_only)
+				al_pp.set_policy(el, self.read_only)
 
+			# Photopoint defined in the compound has no special policy, i.e len(el.keys()) == 1
 			else:
-				if read_only: 
-					al_pp.set_policy(read_only=read_only)
-				
 
-		return LightCurve(pps_list, read_only)
+				# A dict containing frozen PhotoPoint instances might have been provided
+				if self.read_only and frozen_pps_dict is not None: 
+
+					# In this case, rather than instanciating a new PhotoPoint obj, we use pre-existing one
+					if pp_dict['_id'] in frozen_pps_dict:
+						al_pp = frozen_pps_dict[pp_dict['_id']]
+
+					# update frozen_pps_dict dict if pp id not found in the dict keys
+					else:
+						al_pp = PhotoPoint(pp_dict, self.read_only)
+						frozen_pps_dict[pp_dict['_id']] = al_pp
+				else:
+					al_pp = PhotoPoint(pp_dict, self.read_only)
+
+
+			# Update internal list of PhotoPoint instances
+			al_pps_list.append(al_pp)
+
+		return LightCurve(compound_dict, al_pps_list, self.read_only)
