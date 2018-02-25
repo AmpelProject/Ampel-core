d91c2dc9665a974e174bf5a370bcb01796c76b90
vbrinnel <vsbrin@gmail.com>
2018-02-25 16:24:30 +0100

    Updated ChannelsConfig to reflect new config shema

--- src/ampel/pipeline/utils/ChannelsConfig.py
+++ src/ampel/pipeline/utils/ChannelsConfig.py
@@ -3,7 +3,7 @@
 # File              : ampel/pipeline/utils/ChannelsConfig.py
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 04.01.2018
-# Last Modified Date: 11.02.2018
+# Last Modified Date: 22.02.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from ampel.flags.T2RunnableIds import T2RunnableIds
@@ -12,40 +12,44 @@ from ampel.flags.ChannelFlags import ChannelFlags
 
 class ChannelsConfig:
 
-	def __init__(self, d_config_channels=None, config_file=None):
+	def __init__(self, channels_config=None, ampel_config_path=None):
 		"""
+		Please provide either
+		* channels_config: dict instance containing channel configrations
+		* ampel_config_path: path to an ampel json config file
 		"""
-		if not config_file is None:
-			self.config = self.load_config_from_file(config_file)
+		if ampel_config_path is None:
+			self.chans_config = channels_config
 		else:
-			self.config = d_config_channels
+			self.chans_config = self.load_chans_config_from_file(ampel_config_path)
+
 
 		self.d_chanlabel_chanflag = dict()
 		self.d_chanlabel_t2sflag = dict()
 
 
-	def load_config_from_file(self, file_name):
+	def load_chans_config_from_file(self, file_path):
 		"""
 		"""
 		import json
-		with open(file_name, "r") as data_file:
-			config = json.load(data_file)
+		with open(file_path, "r") as data_file:
+			chans_config = json.load(data_file)
 
-		return config
+		return chans_config
 
 
 	def get_available_channel_names(self):
-		return self.config["channels"].keys()
+		return self.chans_config["channels"].keys()
 
 	
 	def get_channel_input_parameters(self, channel_name, instrument="ZTF", alerts="IPAC"):
 		"""	
 			Dict path lookup shortcut function
 		"""	
-		if channel_name not in self.config:
+		if channel_name not in self.chans_config:
 			raise ValueError('Channel %s not found' % channel_name)
 
-		for el in self.config[channel_name]['input']:
+		for el in self.chans_config[channel_name]['input']:
 			if el['instrument'] == instrument and el['alerts'] == alerts:
 				d = el
 
@@ -59,7 +63,7 @@ class ChannelsConfig:
 		"""	
 		
 		# Check for unknown channel names
-		if channel_name not in self.config:
+		if channel_name not in self.chans_config:
 			raise ValueError('Channel %s not found' % channel_name)
 
 		# Check for previously generated enum flag
@@ -78,7 +82,7 @@ class ChannelsConfig:
 			Generate T2RunnableIds flags for channel_name if not previously generated,
 			otherwise return previously generated enum flag
 		"""	
-		if channel_name not in self.config:
+		if channel_name not in self.chans_config:
 			raise ValueError('Channel %s not found' % channel_name)
 
 		# Check for previously generated enum flag
@@ -87,7 +91,7 @@ class ChannelsConfig:
 
 		t2s_flag = T2RunnableIds(0)
 
-		for d_entry in self.config[channel_name]['t2Compute']:
+		for d_entry in self.chans_config[channel_name]['t2Compute']:
 			t2s_flag |= T2RunnableIds[d_entry['id']]
 
 		self.d_chanlabel_t2sflag[channel_name] = t2s_flag
@@ -99,30 +103,30 @@ class ChannelsConfig:
 		"""	
 			Dict path lookup shortcut function
 		"""	
-		if channel_name not in self.config:
+		if channel_name not in self.chans_config:
 			raise ValueError('Channel %s not found' % channel_name)
 
-		return self.config[channel_name]['t0Filter']
+		return self.chans_config[channel_name]['t0Filter']
 
 
 	def set_channel_filter_parameter(self, channel_name, param_name, param_value):
 		"""	
 			Dict path lookup shortcut function
 		"""	
-		if channel_name not in self.config:
+		if channel_name not in self.chans_config:
 			raise ValueError('Channel %s not found' % channel_name)
 
-		self.config[channel_name]['alertFilter']['parameters'][param_name] = param_value
+		self.chans_config[channel_name]['t0Filter']['parameters'][param_name] = param_value
 
 
 	def get_channel_t2_param(self, channel_name, t2_runnable_name):
 		"""	
 			Dict path lookup shortcut function
 		"""	
-		if channel_name not in self.config:
+		if channel_name not in self.chans_config:
 			raise ValueError('Channel %s not found' % channel_name)
 
-		for el in self.config[channel_name]['t2Compute']:
+		for el in self.chans_config[channel_name]['t2Compute']:
 			if el['id'] == t2_runnable_name:
 				return el['paramId']
 
