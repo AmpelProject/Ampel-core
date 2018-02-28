47a7e0b94658b16004883c093db0fd926b0a1ed9
vbrinnel <vsbrin@gmail.com>
2018-02-28 13:53:13 +0100

    NoFilter update

--- src/ampel/contrib/hu/t0/NoFilter.py
+++ src/ampel/contrib/hu/t0/NoFilter.py
@@ -3,14 +3,17 @@
 # File              : ampel/contrib/hu/t0/NoFilter.py
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 25.01.2018
+# Last Modified Date: 28.02.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 from ampel.abstract.AbstractTransientFilter import AbstractTransientFilter
 
 class NoFilter(AbstractTransientFilter):
 
 	def set_filter_parameters(self, d):
-		self.parameters = d
+		pass
+
+	def get_version(self):
+		return 1.0
 
 	def apply(self, ampel_alert):
-		return True
+		return self.on_match_default_flags
