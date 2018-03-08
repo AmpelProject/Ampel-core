c02451b499debb225bf027646ed992a9e2f7e950
vbrinnel <vsbrin@gmail.com>
2018-03-08 14:25:23 +0100

    Abstract classes update

---
+++ src/ampel/abstract/AbsT3Unit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/abstract/AbsT3Unit.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 23.02.2018
+# Last Modified Date: 07.03.2018
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from ampel.abstract.AmpelABC import AmpelABC, abstractmethod
+
+class AbsT3Unit(metaclass=AmpelABC):
+	"""
+	"""
+
+	@abstractmethod
+	def __init__(self, logger, base_config=None):
+		pass
+
+	@abstractmethod
+	def run(self, run_config, transients=None):
+		pass
+
+	@abstractmethod
+	def get_version(self):
+		pass
