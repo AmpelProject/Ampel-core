8a6a76afcdddcbb55b17ef5f237763df9db55617
vbrinnel <vsbrin@gmail.com>
2018-04-24 18:16:03 +0200

    new abstract classes

---
+++ src/ampel/abstract/AbsAlertLoader.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/abstract/AbsAlertLoader.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 24.04.2018
+# Last Modified Date: 24.04.2018
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from abc import ABC, abstractmethod
+
+class AbsAlertLoader(ABC):
+
+	@abstractmethod
+	def get_alert_bytes(self):
+		pass
