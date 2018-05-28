968312e2edfa7f382520a4f1c034173d513bc527
vbrinnel <vsbrin@gmail.com>
2018-05-28 22:47:21 +0200

    minor: header update

--- src/ampel/pipeline/common/Schedulable.py
+++ src/ampel/pipeline/common/Schedulable.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/pipeline/common/Runnable.py
+# File              : ampel/pipeline/common/Schedulable.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 26.05.2018
