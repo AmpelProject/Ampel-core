d3ac35bc7951eabcf0b8d1284537881e08084930
Jakob van Santen <jakob.van.santen@desy.de>
2018-05-30 21:27:39 +0200

    Add an entrypoint for AmpelStatsPublisher

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='ampel',
       entry_points = {
           'console_scripts' : [
               'ampel-alertprocessor = ampel.pipeline.t0.AlertProcessor:run_alertprocessor',
+              'ampel-statspublisher = ampel.pipeline.common.AmpelStatsPublisher:run',
               'ampel-init-db = ampel.pipeline.t0.AlertProcessor:init_db',
               'ampel-init-archive = ampel.archive:init_db',
               'ampel-check-broker = ampel.pipeline.t0.ZIAlertFetcher:list_kafka',
--- src/ampel/pipeline/common/AmpelStatsPublisher.py
+++ src/ampel/pipeline/common/AmpelStatsPublisher.py
@@ -282,3 +282,19 @@ class AmpelStatsPublisher(DBWired, Schedulable):
 				},
 				{'tranId': 1, '_id': 0}
 			).count()
+
+def run():
+	from ampel.archive import docker_env
+	from os import environ
+	
+	mongo_uri = 'mongodb://{}:{}@{}/'.format(
+		docker_env('MONGO_INITDB_ROOT_USERNAME'),
+		docker_env('MONGO_INITDB_ROOT_PASSWORD'),
+		environ['MONGO']
+	)
+	
+	asp = AmpelStatsPublisher(
+		mongodb_uri=mongo_uri, 
+		publish_stats=['print', 'graphite']
+	)
+	asp.start()
