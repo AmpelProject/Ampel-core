d2fada9154869f073978ddcdfbdaee5f52d1fb21
Jakob van Santen <jakob.van.santen@desy.de>
2018-05-31 17:20:47 +0200

    Split minimal ingestion config into separate files for loading with mongorestore

---
+++ config/initdb.d/load_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Initialize configuration database in docker entrypoint
+
+import=( mongoimport --host 127.0.0.1 --port 27017 --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --jsonArray -d Ampel_config --file )
+
+cd  /docker-entrypoint-initdb.d
+for f in *.json; do
+	${import[@]} $f
+done
+
