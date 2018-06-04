b1d05f2d42e16426deee0f8280274c2825ae0609
Jakob van Santen <jvansanten@gmail.com>
2018-06-04 19:43:13 +0200

    Sync split-file config with messy_preliminary_config.json

--- config/initdb.d/load_config.sh
+++ config/initdb.d/load_config.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Initialize configuration database in docker entrypoint
 
-import=( mongoimport --host 127.0.0.1 --port 27017 --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --jsonArray -d Ampel_config --file )
+import=( mongoimport --host 127.0.0.1 --port 27017 --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin -d Ampel_config --file )
 
 cd  /docker-entrypoint-initdb.d
 for f in *.json; do
