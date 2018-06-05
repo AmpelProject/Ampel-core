fa268cc71fdc5e3594d279bea5033aeb48a49c0e
Jakob van Santen <jvansanten@gmail.com>
2018-06-05 14:38:53 +0200

    Remove duplicated file_env function
    bash functions remain defined in subshells. Who knew?

--- config/initdb.d/load_config.sh
+++ config/initdb.d/load_config.sh
@@ -2,28 +2,6 @@
 
 # Initialize configuration database in docker entrypoint
 
-# usage: file_env VAR [DEFAULT]
-#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
-# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
-#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
-file_env() {
-	local var="$1"
-	local fileVar="${var}_FILE"
-	local def="${2:-}"
-	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-		exit 1
-	fi
-	local val="$def"
-	if [ "${!var:-}" ]; then
-		val="${!var}"
-	elif [ "${!fileVar:-}" ]; then
-		val="$(< "${!fileVar}")"
-	fi
-	export "$var"="$val"
-	unset "$fileVar"
-}
-
 import=( mongoimport --host 127.0.0.1 --port 27017 --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin -d Ampel_config --file )
 
 cd  /docker-entrypoint-initdb.d
