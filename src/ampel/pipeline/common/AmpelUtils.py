1e7f55fd00abaa7c5d7343d87b8dfd293c6436e8
vbrinnel <vsbrin@gmail.com>
2018-06-11 19:12:13 +0200

    AmpelUtils: new util method

--- src/ampel/pipeline/common/AmpelUtils.py
+++ src/ampel/pipeline/common/AmpelUtils.py
@@ -4,9 +4,10 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.06.2018
-# Last Modified Date: 07.06.2018
+# Last Modified Date: 11.06.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
+import collections
 
 class AmpelUtils():
 	""" 
@@ -18,3 +19,33 @@ class AmpelUtils():
 		converts input to set 
 		"""
 		return {el for el in arg} if type(arg) in (list, tuple) else {arg}
+
+
+	@staticmethod
+	def check_seq_inner_type(seq, types, multi_type=False):
+		"""
+		check type of all elements contained in a sequence.
+		*all* members of the provided sequence must match with:
+			* multi_type == False: one of the provided type.
+			* multi_type == False: any of the provided type.
+
+		check_seq_inner_type((1,2), int) -> True
+		check_seq_inner_type([1,2], int) -> True
+		check_seq_inner_type((1,2), float) -> False
+		check_seq_inner_type(('a','b'), str) -> True
+		check_seq_inner_type((1,2), (int, str)) -> True
+		check_seq_inner_type((1,2,'a'), (int, str)) -> False
+		check_seq_inner_type((1,2,'a'), (int, str), multi_type=True) -> True
+		"""
+
+		# monotype
+		if type(types) not in (list, tuple):
+			return all(type(el) is types for el in seq)
+		# different types accepted (or connected)
+		else:
+			if multi_type:
+				return all(type(el) in types for el in seq)
+			else:
+				return any(
+					tuple(AmpelUtils.check_seq_inner_type(seq, _type) for _type in types)
+				)
