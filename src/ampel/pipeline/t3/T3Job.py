812b8eaadddfbf9b6112bc8c77cb6483934d085c
vbrinnel <vsbrin@gmail.com>
2018-06-11 17:16:42 +0200

    removed unused code

--- src/ampel/pipeline/t3/T3Job.py
+++ src/ampel/pipeline/t3/T3Job.py
@@ -4,101 +4,32 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 06.03.2018
-# Last Modified Date: 07.06.2018
+# Last Modified Date: 11.06.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from functools import reduce
-from voluptuous import Schema, Required, Any, Optional, ALLOW_EXTRA
-from ampel.pipeline.t3.T3TaskLoader import T3TaskLoader
 
 class T3Job:
 	"""
 	"""
 
-	_docs_schema = Schema(Any("TRANSIENT", "PHOTOPOINT", "UPPERLIMIT", "COMPOUND", "T2RECORD"))
-
-	job_schema = Schema(
-		{
-			Required('active'): bool,
-			Required('schedule'): Any(
-				{
-					Required('mode'): 'fixed_time',
-					Required('time'): str
-				},
-				{
-					Required('mode'): 'fixed_rate',
-					Required('interval'): int
-				}
-			),
-			Required('task(s)'): Any(
-				T3TaskLoader.t3_task_schema, 
-				[T3TaskLoader.t3_task_schema]
-			),
-			'input': {
-				Required('select'): {
-					'created': {
-						'timedelta': dict, 
-						'from': Any(
-							{'unixTime': float},
-							{'strTime': str, 'strFormat': str}
-						), 
-						'until': Any(
-							{'unixTime': float},
-							{'strTime': str, 'strFormat': str}
-						)
-					},
-					'modified': {
-						'timedelta': dict, 
-						'from': Any(
-							{'unixTime': float},
-							{'strTime': str, 'strFormat': str}
-						), 
-						'until': Any(
-							{'unixTime': float},
-							{'strTime': str, 'strFormat': str}
-						)
-					},
-					'channel(s)': Any(str, [str]),
-					'withFlag(s)': Any(str, [str]),
-					'withoutFlag(s)': Any(str, [str])
-				},
-				Required('load'): {
-					Required('state'): Any("all", "latest"),
-					'doc(s)': Any(_docs_schema, [_docs_schema]),
-					't2(s)': Any(str, [str]),
-					'verbose': bool
-				},
-				'chunk': int
-			},
-			'onError': {
-				'sendMail': {
-					Required('to'): str,
-					Required('excStack'): bool
-				},
-				Optional('stopAmpel', default=False): bool,
-				Optional('retry', default=False): bool
-			}
-		}, 
-		extra=ALLOW_EXTRA
-	)
-
-
-	def __init__(self, job_name, job_doc, t3_tasks):
+	def __init__(self, job_name, job_doc, t3_task_instances):
 		"""
-		This class is mainly made of config validity tests.
-		NOTE: channel name existence is not checked on purpose.
+		job_name: string
+		job_doc: dict instance
+		t3_task_instances: list of instances of ampel.pipeline.t3.T3Task
 		"""
 
 		self.job_name = job_name
 		self.job_doc = job_doc
-		self.t3_tasks = t3_tasks
+		self.t3_task_instances = t3_task_instances
 
 
 	def get_tasks(self):
 		""" 
 		Returns the loaded tasks associated with this job
 		"""
-		return self.t3_tasks
+		return self.t3_task_instances
 
 
 	def get_chunk(self):
