3a485d22e5ab2666d631e2e6541579a918b8a0a8
vbrinnel <vsbrin@gmail.com>
2018-06-13 17:17:33 +0200

    new flags

--- src/ampel/flags/CompoundFlags.py
+++ src/ampel/flags/CompoundFlags.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 20.03.2018
-# Last Modified Date: 15.05.2018
+# Last Modified Date: 13.06.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from enum import Flag
@@ -12,7 +12,17 @@ from enum import Flag
 class CompoundFlags(Flag):
 	"""
 	"""
-	PARTNERSHIP_DATA    = 1 
-	HAS_REPROCESSED_PPS = 2 
-	HAS_UPPER_LIMITS 	= 4
-	EXCLUDED_PPS   		= 8
+
+	HAS_UPPER_LIMITS 			= 1
+	HAS_AUTOCOMPLETED_PHOTO		= 2
+	HAS_SUPERSEEDED_PPS   		= 4
+	HAS_EXCLUDED_PPS   			= 8
+	HAS_MANUAL_EXCLUSION   		= 16
+	HAS_DATARIGHTS_EXCLUSION  	= 32
+
+	WITH_CUSTOM_POLICES  		= 64
+
+	INST_ZTF					= 128
+	SRC_IPAC					= 256
+	ZTF_COLLAB_DATA     		= 512
+	ZTF_REPROC_PPS 				= 1024
