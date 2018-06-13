424eda6d72b4b88344868ba2c43721b247213575
vbrinnel <vsbrin@gmail.com>
2018-06-13 17:22:43 +0200

    new flags

--- src/ampel/flags/CompoundFlags.py
+++ src/ampel/flags/CompoundFlags.py
@@ -13,16 +13,16 @@ class CompoundFlags(Flag):
 	"""
 	"""
 
-	HAS_UPPER_LIMITS 			= 1
-	HAS_AUTOCOMPLETED_PHOTO		= 2
-	HAS_SUPERSEEDED_PPS   		= 4
-	HAS_EXCLUDED_PPS   			= 8
-	HAS_MANUAL_EXCLUSION   		= 16
-	HAS_DATARIGHTS_EXCLUSION  	= 32
+	HAS_UPPER_LIMITS            = 1
+	HAS_AUTOCOMPLETED_PHOTO     = 2
+	HAS_SUPERSEEDED_PPS         = 4
+	HAS_EXCLUDED_PPS            = 8
+	HAS_MANUAL_EXCLUSION        = 16
+	HAS_DATARIGHTS_EXCLUSION    = 32
 
-	WITH_CUSTOM_POLICES  		= 64
+	WITH_CUSTOM_POLICES         = 64
 
-	INST_ZTF					= 128
-	SRC_IPAC					= 256
-	ZTF_COLLAB_DATA     		= 512
-	ZTF_REPROC_PPS 				= 1024
+	INST_ZTF                    = 128
+	SRC_IPAC                    = 256
+	ZTF_COLLAB_DATA             = 512
+	ZTF_REPROC_PPS              = 1024
