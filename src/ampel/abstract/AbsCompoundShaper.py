0904ab0b886b2bde22ead253933e029342295722
vbrinnel <vsbrin@gmail.com>
2018-06-13 17:24:25 +0200

    method renamed

--- src/ampel/abstract/AbsCompoundShaper.py
+++ src/ampel/abstract/AbsCompoundShaper.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/abstract/AbsCompElement.py
+# File              : ampel/abstract/AbsCompoundShaper.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.05.2018
-# Last Modified Date: 07.05.2018
+# Last Modified Date: 13.06.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from ampel.abstract.AmpelABC import AmpelABC, abstractmethod
 
-class AbsCompElement(metaclass=AmpelABC):
+class AbsCompoundShaper(metaclass=AmpelABC):
+	"""
+	"""
 
 	@abstractmethod
-	def gen_dict(self, input_d, channel_name):
+	def gen_compound_item(self, input_d, channel_name):
+		""" """
 		pass
-
