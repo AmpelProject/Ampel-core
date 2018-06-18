e1c5003a541e95e5b25f282267d7a6f55a42fb62
Jakob van Santen <jvansanten@gmail.com>
2018-06-18 14:15:52 +0200

    Remove auto-initialization and dev mode

--- src/ampel/pipeline/config/AmpelConfig.py
+++ src/ampel/pipeline/config/AmpelConfig.py
@@ -12,41 +12,12 @@ from types import MappingProxyType
 from ampel.pipeline.common.AmpelUtils import AmpelUtils
 
 class AmpelConfig:
-	
 
-	# path relative the AmpelConfig path (<Ampel base dir>/src/ampel/pipeline/config
-	_fallback_config_path = '../../../../config/ampel.config'
 	_global_config = None
-	_dev_mode = False
-
 
 	@classmethod
 	def initialized(cls):
 		return cls._global_config is not None
-
-
-	@classmethod
-	def _auto_initialize(cls):
-		try:
-			import json, os
-			cls.set_config(
-				json.load(open("%s/%s" % (
-					os.path.dirname(os.path.abspath(__file__)),
-					cls._fallback_config_path), 'r'
-				))
-			)
-
-		except Exception as e:
-			print("Auto-initialization failed")
-			print(e)
-			# TODO: try something else ?
-			pass
-
-
-	@classmethod
-	def _developer_mode(cls):
-		cls._dev_mode = True
-
 
 	@classmethod
 	def get_config(cls, key=None, validate=None):
@@ -58,11 +29,8 @@ class AmpelConfig:
 			* if config is not frozen since frozen configs are created using validation
 			* if key is provided since only parts of the global config follow given schemas
 		"""
-		if cls._global_config is None:
-			# try to auto initialize
-			cls._auto_initialize()
-			if cls._global_config is None:
-				raise RuntimeError("Ampel global config not set")
+		if not cls.initialized():
+			raise RuntimeError("Ampel global config not set")
 
 		if key is None:
 			return cls._global_config
@@ -84,13 +52,12 @@ class AmpelConfig:
 		""" """
 		if cls._global_config is not None:
 			warnings.warn("Resetting global configuration")
-		cls._global_config = config if cls._dev_mode else AmpelConfig.freeze(config)
+		cls._global_config = AmpelConfig.freeze(config)
 		return cls._global_config
 
 	@classmethod
 	def is_frozen(cls):
 		return type(cls._global_config) is MappingProxyType
-	
 
 	@classmethod
 	def freeze(cls, collection):
