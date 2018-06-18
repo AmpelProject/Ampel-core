61ea171f7ad56e6ade1669869391f17f571fbcee
vbrinnel <vsbrin@gmail.com>
2018-06-18 16:15:42 +0200

    AlertProcessor now uses AmpelConfig, AmpelUtils and AmpelDB

--- src/ampel/pipeline/t0/AlertProcessor.py
+++ src/ampel/pipeline/t0/AlertProcessor.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 10.10.2017
-# Last Modified Date: 15.06.2018
+# Last Modified Date: 18.06.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import pymongo, time, numpy as np
@@ -12,17 +12,20 @@ from ampel.pipeline.t0.alerts.AlertSupplier import AlertSupplier
 from ampel.pipeline.t0.alerts.ZIAlertShaper import ZIAlertShaper
 from ampel.pipeline.t0.ingesters.ZIAlertIngester import ZIAlertIngester
 from ampel.pipeline.logging.LoggingUtils import LoggingUtils
-from ampel.pipeline.logging.DBJobReporter import DBJobReporter
 from ampel.pipeline.logging.DBLoggingHandler import DBLoggingHandler
 from ampel.pipeline.logging.InitLogBuffer import InitLogBuffer
-from ampel.pipeline.db.DBWired import DBWired
+from ampel.pipeline.db.AmpelDB import AmpelDB
 from ampel.pipeline.common.GraphiteFeeder import GraphiteFeeder
+from ampel.pipeline.common.AmpelUtils import AmpelUtils
 from ampel.pipeline.config.ChannelLoader import ChannelLoader
+from ampel.pipeline.config.AmpelConfig import AmpelConfig
 from ampel.base.AmpelAlert import AmpelAlert
 from ampel.flags.AlDocTypes import AlDocTypes
 from ampel.flags.AlertFlags import AlertFlags
+from ampel.flags.FlagUtils import FlagUtils
 
-class AlertProcessor(DBWired):
+
+class AlertProcessor():
 	""" 
 	Class handling T0 pipeline operations.
 
@@ -36,9 +39,7 @@ class AlertProcessor(DBWired):
 	iter_max = 5000
 
 	def __init__(
-		self, channels=None, source="ZTFIPAC", mongodb_uri='localhost', 
-		config=None, central_db=None, publish_stats=['graphite', 'jobs'], 
-		load_ingester=True
+		self, channels=None, source="ZTFIPAC", publish_stats=['graphite', 'jobs'], load_ingester=True
 	):
 		"""
 		Parameters:
@@ -48,10 +49,6 @@ class AlertProcessor(DBWired):
 		   - String: channel with the provided id will be loaded
 		   - List of strings: channels with the provided ids will be loaded 
 		'source': name of input stream (string - see set_stream() docstring)
-		'mongodb_uri': URI of the server hosting mongod.
-		   Example: 'mongodb://user:password@localhost:27017'
-		'config': see ampel.pipeline.db.DBWired.load_config() docstring
-		'central_db': see ampel.pipeline.db.DBWired.plug_central_db() docstring
 		'publish_stats': publish performance stats:
 		   * graphite: send t0 metrics to graphite (graphite server must be defined 
 		     in Ampel_config)
@@ -64,11 +61,8 @@ class AlertProcessor(DBWired):
 		self.logger.addHandler(self.ilb)
 		self.logger.info("Setting up new AlertProcessor instance")
 
-		# Setup instance variable referencing ampel databases
-		self.plug_databases(self.logger, mongodb_uri, config, central_db)
-
 		# Load channels
-		cl = ChannelLoader(self.config, source=source, tier=0)
+		cl = ChannelLoader(source=source, tier=0)
 		self.channels = cl.load_channels(channels, self.logger);
 		self.chan_enum = list(enumerate(self.channels))
 		self.chan_auto_complete = len(self.channels) * [False]
@@ -103,32 +97,27 @@ class AlertProcessor(DBWired):
 		"""
 		Depending on which instrument and institution the alerts originate,
 		(as of March 2018 only ZTF & IPAC), this method performs the following:
-		-> defines the alert loading function.
 		-> sets required static settings in AmpelAlert
 		-> instantiates the adequate ingester class
 		"""
 
 		if source == "ZTFIPAC":
 
-			# TODO: log something ? 
-			conf = self.global_config['sources'][source]
-
 			# Set static AmpelAlert alert flags
-			alert_flags = None
-			for flag_str in conf['alerts']['flags']:
-				if alert_flags is None:
-					alert_flags = AlertFlags[flag_str]
-				else:
-					alert_flags |= AlertFlags[flag_str]
-			AmpelAlert.add_class_flags(alert_flags)
+			AmpelAlert.add_class_flags(
+				FlagUtils.list_flags_to_enum_flags(
+					AmpelConfig.get_config('global.sources.ZTFIPAC.alerts.flags'),
+					AlertFlags
+				)
+			)
 
 			# Set static AmpelAlert dict keywords
-			AmpelAlert.set_alert_keywords(conf['alerts']['mappings'])
+			AmpelAlert.set_alert_keywords(
+				AmpelConfig.get_config('global.sources.ZTFIPAC.alerts.mappings')
+			)
 	
 			if load_ingester:
-				self.ingester = ZIAlertIngester(
-					self.config, self.central_db, self.channels, logger=self.logger
-				)
+				self.ingester = ZIAlertIngester(self.channels, logger=self.logger)
 	
 		else:
 			# more sources may be defined later
@@ -176,12 +165,14 @@ class AlertProcessor(DBWired):
 		
 		self.logger.info("Returning iterable for file paths in folder: %s" % base_dir)
 
-		if source not in self.config['global']['sources']:
+		if AmpelConfig.get_config('global.sources.%s' % source) is None:
 			raise ValueError("Unknown source %s, please check your config" % source)
 
 		# Instantiate class shaping alert dicts
 		if not hasattr(self, "alert_shaper"):
-			shaper_class_full_path = self.config['global']['sources'][source]['alerts']['processing']['shape']
+			shaper_class_full_path = AmpelConfig.get_config(
+				'global.sources.%s.alerts.processing.shape' % source
+			)
 			shaper_module = importlib.import_module(shaper_class_full_path)
 			shaper_class = getattr(shaper_module, shaper_class_full_path.split(".")[-1])
 			self.alert_shaper = shaper_class(self.logger)
@@ -225,22 +216,12 @@ class AlertProcessor(DBWired):
 		# Remove logger saving "log headers" before job(s) 
 		self.logger.removeHandler(self.ilb)
 
-		# Create JobReporter instance
-		db_job_reporter = DBJobReporter(self.central_db['logs'])
-
-		# Create new job document in the logs collection
-		db_job_reporter.insert_new(
-			params = {
-				"alertProc": str(self.version),
-				"ingesterClass": str(self.ingester.__class__)
-			},
-			tier = 0
-		)
-	
 		# Create DB logging handler instance (logging.Handler child class)
 		# This class formats, saves and pushes log records into the DB
 		db_logging_handler = DBLoggingHandler(
-			db_job_reporter, previous_logs=self.ilb.get_logs()
+			tier=0, 
+			info={"alertProc": str(self.version), "ingesterClass": str(self.ingester.__class__)}, 
+			previous_logs=self.ilb.get_logs()
 		)
 
 		# Add db logging handler to the logger stack of handlers 
@@ -261,8 +242,8 @@ class AlertProcessor(DBWired):
 
 		# Forward jobId to ingester instance 
 		# (will be inserted in the transient documents)
-		self.ingester.set_job_id(
-			db_job_reporter.get_job_id()
+		self.ingester.set_log_id(
+			db_logging_handler.get_log_id()
 		)
 
 		# Loop variables
@@ -324,7 +305,7 @@ class AlertProcessor(DBWired):
 		filtering_stats = job_info['duration']['filtering']
 		job_count_stats = job_info['count']['t0Job']
 		alert_counts = job_info['count']['ingestion']['alerts']
-		col = self.central_db['main']
+		col = AmpelDB.get_collection('main')
 
 		# Save pre run time
 		pre_run = time_now()
@@ -393,14 +374,14 @@ class AlertProcessor(DBWired):
 
 				except:
 
-					self.report_exception(
+					AmpelUtils.report_exception(self.logger,
 						{
 							'section': 'filter',
 							'channel': channel.name,
 							'tranId': tran_id,
-							'jobId':  db_job_reporter.get_job_id(),
-						},
-						shaped_alert = shaped_alert
+							'jobId':  db_logging_handler.get_log_id(),
+							'alert': AlertProcessor._alert_essential(shaped_alert)
+						}
 					)
 
 				# Unset channel id <-> log entries association
@@ -417,20 +398,20 @@ class AlertProcessor(DBWired):
 				# stats
 				ingested_count += 1
 
-				# TODO: build tran_id <-> alert_id map (replayibility)
+				# TODO: build tran_id <-> alert_id map (replayability)
 				#processed_alert[tran_id]
 				try: 
 					self.ingester.ingest(
 						tran_id, shaped_alert['pps'], shaped_alert['uls'], scheduled_t2_units
 					)
 				except:
-					self.report_exception(
+					AmpelUtils.report_exception(self.logger,
 						{
 							'section': 'ingest',
 							'tranId': tran_id,
-							'jobId':  db_job_reporter.get_job_id(),
-						},
-						shaped_alert = shaped_alert
+							'jobId':  db_logging_handler.get_log_id(),
+							'alert': AlertProcessor._alert_essential(shaped_alert)
+						}
 					)
 
 			# Unset log entries association with transient id
@@ -505,16 +486,16 @@ class AlertProcessor(DBWired):
 				self.gather_and_send_stats(post_run, job_info)
 
 			# Insert job stats
-			job_info['_id'] = db_job_reporter.get_job_id()
+			job_info['_id'] = db_logging_handler.get_log_id()
 			job_info['tier'] = 0
-			self.central_db['stats'].insert_one(job_info)
+			AmpelDB.get_collection('stats').insert_one(job_info)
 
 		except:
 
-			self.report_exception(
+			AmpelUtils.report_exception(self.logger,
 				{
 					'section': 'run_end',
-					'jobId':  db_job_reporter.get_job_id(),
+					'jobId':  db_logging_handler.get_log_id(),
 				}
 			)
 			
@@ -533,8 +514,32 @@ class AlertProcessor(DBWired):
 
 		# Remove DB logging handler
 		if iter_count > 0:
-			db_job_reporter.set_flush_job_info()
-			db_logging_handler.flush()
+
+			try:
+				db_logging_handler.flush()
+			except Exception as e:
+				# error msg will be passed to the logging handlers of higher level
+				# as self.logger.propagate was set back to True previously
+				self.logger.removeHandler(db_logging_handler)
+				self.logger.error("DB Log flushing has failed")
+				self.logger.error(e)
+
+				try: 
+					# This will fail as well if we have DB connectivity issues
+					AmpelUtils.report_exception(self.logger,
+						{
+							'section': 'flush_logs',
+							'jobId':  db_logging_handler.get_log_id(),
+						}
+					)
+				except Exception as e:
+					self.logger.error("Could not add new doc to 'troubles' collection, DB offline?")
+					self.logger.error(e)
+		else:
+
+			db_logging_handler.remove_log_entry()
+
+
 		self.logger.removeHandler(db_logging_handler)
 		
 		# Return number of processed alerts
@@ -556,7 +561,7 @@ class AlertProcessor(DBWired):
 			# GraphiteSendException: Socket closed before able to send data to ('localhost', 52003), 
 			# with error: [Errno 32] Broken pipe
 			# So we re-create a GraphiteClient every time we send something to graphite...
-			gfeeder = self.config['resources']['graphite']()
+			gfeeder = AmpelConfig.get_config('resources.graphite')()
 
 			if t0_stats is not None:
 				gfeeder.add_stats_with_mean_std(t0_stats)
@@ -564,47 +569,6 @@ class AlertProcessor(DBWired):
 				gfeeder.add_stats(t0_stats)
 
 			gfeeder.send()
-
-
-	def report_exception(self, further_info=None, shaped_alert=None):
-		"""
-		further_info: non-nested dict instance
-		"""
-		import traceback
-
-		self.logger.propagate = True
-		self.logger.critical("Exception occured", exc_info=1)
-
-		exception_str = traceback.format_exc().replace("\"", "'")
-
-		insert_dict = {
-			'tier': 0,
-			'exception': exception_str.split("\n")
-		}
-
-		if shaped_alert is not None:
-
-			if 'alert_id' in shaped_alert:
-				insert_dict['alertId'] = shaped_alert['alert_id'] 
-	
-			if not "KeyboardInterrupt" in exception_str:
-	
-				if (
-					'pps' in shaped_alert and 
-					len(shaped_alert['pps']) > 0 and 
-					'jd' in shaped_alert['pps'][0]
-				):
-					insert_dict['alertDt'] = shaped_alert['pps'][0]['jd']
-		
-				if further_info is not None:
-					for key in further_info:
-						insert_dict[key] = further_info[key]
-		
-				insert_dict['alertPPS'] = shaped_alert['pps']
-				insert_dict['alertULS'] = shaped_alert['uls']
-
-		self.get_trouble_col().insert_one(insert_dict)
-		self.logger.propagate = False
 
 
 	def compute_stat(self, seq, mean=np.mean, std=np.std):
@@ -629,7 +593,7 @@ class AlertProcessor(DBWired):
 		Otherwise, tran_ids_before[i] will be None
 		"""
 
-		col = self.central_db['main']
+		col = AmpelDB.get_collection('main')
 		tran_ids = len(self.channels) * [None]
 
 		# Loop through activated channels
@@ -652,6 +616,15 @@ class AlertProcessor(DBWired):
 				}
 
 		return tran_ids
+
+
+	@staticmethod	
+	def _alert_essential(shaped_alert):
+		return {
+			'id': shaped_alert.get('alert_id'),
+			'pps': shaped_alert.get('pps'),
+			'uls': shaped_alert.get('uls')
+		}
 
 
 def init_db():
@@ -723,7 +696,7 @@ def run_alertprocessor():
 	# partially parse command line to get config
 	opts, argv = parser.parse_known_args()
 	# flesh out parser with resources required by t0 units
-	loader = ChannelLoader(opts.config, source="ZTFIPAC", tier=0)
+	loader = ChannelLoader(source="ZTFIPAC", tier=0)
 	parser.require_resources(*loader.get_required_resources())
 	# parse again
 	opts = parser.parse_args()
@@ -750,7 +723,7 @@ def run_alertprocessor():
 		loader = iter(fetcher)
 
 	alert_supplier = AlertSupplier(loader, ZIAlertShaper(), serialization="avro", archive=ArchiveDB(archive))
-	processor = AlertProcessor(mongodb_uri=mongo, publish_stats=["jobs", "graphite"], config=opts.config)
+	processor = AlertProcessor(publish_stats=["jobs", "graphite"])
 
 	while alert_processed == AlertProcessor.iter_max:
 		t0 = time.time()
