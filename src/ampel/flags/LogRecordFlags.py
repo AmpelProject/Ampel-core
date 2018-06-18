fe78fc984424dbceb24e31a5060af28317d96997
vbrinnel <vsbrin@gmail.com>
2018-06-18 21:22:47 +0200

    LogRecordFlags: use AmpelMetaFlags. Discard auto method()

--- src/ampel/flags/LogRecordFlags.py
+++ src/ampel/flags/LogRecordFlags.py
@@ -4,23 +4,23 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 12.05.2018
+# Last Modified Date: 16.06.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from enum import Flag, auto
+from ampel.flags.AmpelMetaFlags import AmpelMetaFlags
 
-class LogRecordFlags(Flag):
+class LogRecordFlags(metaclass=AmpelMetaFlags):
 	"""
 	Flag used for each log entry stored in the DB
 	"""
 	# log level
-	INFO      			 = auto()
-	DEBUG      			 = auto()
-	WARNING    			 = auto()
-	ERROR    			 = auto()
-	CRITICAL   			 = auto()
+	INFO      			 = 1048576
+	DEBUG      			 = 2097152
+	WARNING    			 = 4194304
+	ERROR    			 = 8388608
+	CRITICAL   			 = 16777216
 
 	# Convenience flag
-	ILB	                 = auto()
-	HAS_CANDID           = auto()
-	HAS_CHAN             = auto()
+	ILB	                 = 33554432
+	HAS_CANDID           = 67108864
+	HAS_CHAN             = 134217728
