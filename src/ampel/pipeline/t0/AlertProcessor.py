AmpelProject/Ampel-core

commit e0b48c99891bbab8ec4d9f3e2ed6005ff5d047bd

Do not attempt to publish stats if no alerts were seen
Arrays of NaN do not play nice with BSON serialization

src/ampel/pipeline/t0/AlertProcessor.py
@@ -491,10 +491,10 @@ class AlertProcessor():
 
 				self.gather_and_send_stats(post_run, job_info)
 
-			# Insert job stats
-			job_info['_id'] = db_logging_handler.get_log_id()
-			job_info['tier'] = 0
-			AmpelDB.get_collection('stats').insert_one(job_info)
+				# Insert job stats
+				job_info['_id'] = db_job_reporter.get_job_id()
+				job_info['tier'] = 0
+				AmpelDB.get_collection('stats').insert_one(job_info)
 
 		except:
 

commit ca09d757c0c3a7ef60d497952691ba1df6e78fdc

Fix bug introduced by commit e0b48c99891bbab8ec4d9f3e2ed6005ff5d047bd

src/ampel/pipeline/t0/AlertProcessor.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 10.10.2017
-# Last Modified Date: 18.06.2018
+# Last Modified Date: 19.06.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import pymongo, time, numpy as np
@@ -492,7 +492,7 @@ class AlertProcessor():
 				self.gather_and_send_stats(post_run, job_info)
 
 				# Insert job stats
-				job_info['_id'] = db_job_reporter.get_job_id()
+				job_info['_id'] = db_logging_handler.get_log_id()
 				job_info['tier'] = 0
 				AmpelDB.get_collection('stats').insert_one(job_info)
 