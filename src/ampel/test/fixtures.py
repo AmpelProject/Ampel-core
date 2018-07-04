476f391309f97448ce4a533b612cbf430a898b90
Jakob van Santen <jvansanten@gmail.com>
2018-07-04 09:40:48 +0200

    Add alert factory for creating alerts with arbitrary field values

--- src/ampel/test/fixtures.py
+++ src/ampel/test/fixtures.py
@@ -81,6 +81,142 @@ def alert_generator(alert_tarball):
 	yield alerts
 
 @pytest.fixture
+def ampel_alerts(alert_generator):
+	from ampel.pipeline.t0.alerts.AlertSupplier import AlertSupplier
+	from ampel.pipeline.t0.alerts.ZIAlertShaper import ZIAlertShaper
+	from ampel.base.AmpelAlert import AmpelAlert
+	def ampelize(shaped_alert):
+		return AmpelAlert(shaped_alert['tran_id'], shaped_alert['ro_pps'], shaped_alert['ro_uls'])
+	yield map(ampelize, AlertSupplier(alert_generator(), ZIAlertShaper()))
+
+@pytest.fixture
+def latest_schema():
+	with open(join(dirname(__file__), '..', '..', '..', 'alerts', 'schema_2.0.json')) as f:
+		return json.load(f)
+
+import copy
+from ampel.pipeline.t0.alerts.ZIAlertShaper import ZIAlertShaper
+from ampel.base.AmpelAlert import AmpelAlert
+class AlertFactoryFixture(object):
+	def __init__(self, schema):
+		native_types = {'int': int, 'long': int, 'float': float, 'double': float, 'string': str}
+		def get_defaults(schema):
+			required = dict()
+			default = dict()
+			for f in schema['fields']:
+				if 'default' in f:
+					default[f['name']] = f['default']
+				elif isinstance(f['type'], dict):
+					required[f['name']] = get_defaults(f['type'])
+				else:
+					required[f['name']] = native_types[f['type']]
+			default.update(required)
+			return default
+		alert = get_defaults(schema)
+		def set(mapping, k, v):
+			if isinstance(mapping[k], type):
+				assert isinstance(v, mapping[k])
+			mapping[k] = v
+		set(alert, 'schemavsn', schema['version'])
+		set(alert, 'publisher', 'zardoz')
+		set(alert, 'candid', 1001)
+		set(alert, 'objectId', 'ZTF10aaaaaaaa')
+		
+		self._template = alert
+		self._shaper = ZIAlertShaper()
+	def __call__(self, **kwargs):
+		alert = copy.deepcopy(self._template)
+		defaults = {
+			'jd': 2458276.6933449,
+			'fid': 2,
+			'pid': 522193343615,
+			'diffmaglim': 20.660852432250977,
+			'programid': 1,
+			'candid': 522193343615015004,
+			'isdiffpos': 't',
+			'tblid': 4,
+			'nid': 522,
+			'rcid': 36,
+			'field': 477,
+			'xpos': 1176.86962890625,
+			'ypos': 2029.61376953125,
+			'ra': 214.2322306,
+			'dec': 5.8014371,
+			'magpsf': 19.52411460876465,
+			'sigmapsf': 0.09479492902755737,
+			'chipsf': 8.641731262207031,
+			'magap': 19.622299194335938,
+			'sigmagap': 0.1695999950170517,
+			'distnr': 9.204261779785156,
+			'magnr': 22.899999618530273,
+			'sigmagnr': 0.3330000042915344,
+			'chinr': 0.9470000267028809,
+			'sharpnr': -0.15600000321865082,
+			'sky': -0.06626152992248535,
+			'magdiff': 0.09818483889102936,
+			'fwhm': 2.1700000762939453,
+			'classtar': 1.0,
+			'mindtoedge': 1050.88623046875,
+			'magfromlim': 1.0385527610778809,
+			'seeratio': 1.0293431282043457,
+			'aimage': 0.5260000228881836,
+			'bimage': 0.38100001215934753,
+			'aimagerat': 0.2423963099718094,
+			'bimagerat': 0.17557603120803833,
+			'elong': 1.3805774450302124,
+			'nneg': 5,
+			'nbad': 0,
+			'rb': 0.25999999046325684,
+			'ssdistnr': -999.0,
+			'ssmagnr': -999.0,
+			'ssnamenr': 'null',
+			'sumrat': 0.7873558402061462,
+			'magapbig': 19.44849967956543,
+			'sigmagapbig': 0.1850000023841858,
+			'ranr': 214.2333544,
+			'decnr': 5.8037704,
+			'sgmag1': 22.059799194335938,
+			'srmag1': 22.004600524902344,
+			'simag1': 21.603599548339844,
+			'szmag1': 21.28969955444336,
+			'sgscore1': 0.9783329963684082,
+			'distpsnr1': 10.150647163391113,
+			'ndethist': 1,
+			'ncovhist': 3,
+			'jdstarthist': 2458276.6933449,
+			'jdendhist': 2458276.6933449,
+			'scorr': 26.1721801757812,
+			'tooflag': 0,
+			'objectidps1': 114962142293772824,
+			'objectidps2': 114952142289005042,
+			'sgmag2': 20.21619987487793,
+			'srmag2': 19.808000564575195,
+			'simag2': 19.678800582885742,
+			'szmag2': 19.605600357055664,
+			'sgscore2': 1.0,
+			'distpsnr2': 24.609607696533203,
+			'objectidps3': 114952142250229131,
+			'sgmag3': -999.0,
+			'srmag3': 21.59040069580078,
+			'simag3': 20.763399124145508,
+			'szmag3': 20.44099998474121,
+			'sgscore3': 0.0,
+			'distpsnr3': 27.410842895507812,
+			'nmtchps': 5,
+			'rfid': 477120236,
+			'jdstartref': 2458186.957326,
+			'jdendref': 2458258.750185,
+			'nframesref': 15}
+		alert['candidate'].update(defaults)
+		alert['candidate'].update(kwargs)
+		shaped = self._shaper.shape(alert)
+		return AmpelAlert(shaped['tran_id'], shaped['ro_pps'], shaped['ro_uls'])
+
+@pytest.fixture
+def alert_factory(latest_schema):
+	return AlertFactoryFixture(latest_schema)
+	
+@pytest.fixture
 def minimal_ingestion_config(mongod):
 	from ampel.pipeline.config.AmpelConfig import AmpelConfig
 	
