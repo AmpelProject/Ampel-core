00e991cf91c41f2901c6812237b464d8e11f6594
vbrinnel <vsbrin@gmail.com>
2018-07-05 12:05:06 +0200

    Ignore negativ diffmaglim

--- src/ampel/pipeline/t0/ingesters/ZIPhotoDictShaper.py
+++ src/ampel/pipeline/t0/ingesters/ZIPhotoDictShaper.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 04.07.2018
+# Last Modified Date: 05.07.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from ampel.base.flags.PhotoFlags import PhotoFlags
@@ -58,7 +58,7 @@ class ZIPhotoDictShaper:
 		tran_id: transient id (string)
 		photo_list: list of dict instance (respresenting photopoint/upper limit measurements).
 		ids_set: set of strings containing photopoint/upperlimit ids. 
-		Only the elemts from photo_list matching the provided ids will be processed.
+		Only the elements from photo_list matching the provided ids will be processed.
 		"""
 
 		ret_list = []
@@ -106,23 +106,26 @@ class ZIPhotoDictShaper:
 
 			else:
 
-				# update list of dicts
-				ret_list.append(
-					{
-						'_id': photo_dict['_id'],
-						# 'alDocType': AlDocTypes.UPPERLIMIT,
-						'alFlags': dbflags,
-						'jd': photo_dict['jd'],
-	 					'diffmaglim': photo_dict['diffmaglim'],
-	 					'rcid': photo_dict['rcid'],
-						'fid': photo_dict['fid']
-						#'pdiffimfilename': fname
-						# IMPORTANT: 'tranId' is not set here on purpose since it 
-						# then conflict with the addToSet operation
-						#'pid': photo_dict['pid'],
-						#'programid': photo_dict['programid'],
-					}
-				)
+				# rarely, meaningless upper limits with negativ diffmaglim are provided by IPAC
+				if photo_dict['diffmaglim'] > 0:
+
+					# update list of dicts
+					ret_list.append(
+						{
+							'_id': photo_dict['_id'],
+							# 'alDocType': AlDocTypes.UPPERLIMIT,
+							'alFlags': dbflags,
+							'jd': photo_dict['jd'],
+							'diffmaglim': photo_dict['diffmaglim'],
+							'rcid': photo_dict['rcid'],
+							'fid': photo_dict['fid']
+							#'pdiffimfilename': fname
+							# IMPORTANT: 'tranId' is not set here on purpose since it 
+							# then conflicts with the addToSet operation
+							#'pid': photo_dict['pid'],
+							#'programid': photo_dict['programid'],
+						}
+					)
 
 		# Return created list
 		return ret_list
