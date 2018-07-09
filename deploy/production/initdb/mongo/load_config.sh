040111ecdf7c0ac22ca5a9f80eda6456b9ee0d93
Jakob van Santen <jvansanten@gmail.com>
2018-07-09 10:54:34 +0200

    Update mongo db names

--- deploy/production/initdb/mongo/load_config.sh
+++ deploy/production/initdb/mongo/load_config.sh
@@ -18,18 +18,21 @@ file_env 'LOGGER_PASSWORD'
 
 mongo=( mongo --host 127.0.0.1 --port 27017 --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin )
 
+# writer: needs _readWrite_ on Ampel_data, Ampel_reports and _read_ on Ampel_logs
+# logger: needs _readWrite_ on Ampel_logs
+
 # munge into database names ino js objects
 "${mongo[@]}" "$rootAuthDatabase" <<-EOJS
 	db.createUser({
 		user: $(_js_escape "$MONGO_USER"),
 		pwd: $(_js_escape "$MONGO_PASSWORD"),
-		roles: $(echo '"Ampel Ampel_config Ampel_troubles"' | jq 'split(" ") | [{db : .[], role : "readWrite"}]')
+		roles: $(echo '"Ampel_data Ampel_reports"' | jq 'split(" ") | [{db : .[], role : "readWrite"}]')
 	})
+	db.grantRolesToUser($(_js_escape "$MONGO_USER"), [{"role": "read", "db": "Ampel_logs"}])
 	db.createUser({
 		user: $(_js_escape "$LOGGER_USER"),
 		pwd: $(_js_escape "$LOGGER_PASSWORD"),
 		roles: $(echo '"Ampel_logs"' | jq 'split(" ") | [{db : .[], role : "readWrite"}]')
 	})
 	db.grantRolesToUser($(_js_escape "$LOGGER_USER"), [{"role": "clusterMonitor", "db": "admin"}])
-	db.grantRolesToUser($(_js_escape "$LOGGER_USER"), $(echo '"Ampel Ampel_config Ampel_troubles"' | jq 'split(" ") | [{db : .[], role : "read"}]'))
 EOJS
