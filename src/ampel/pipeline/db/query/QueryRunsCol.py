1351209c156f56df006873371f6a9559f3214566
vbrinnel <vsbrin@gmail.com>
2018-07-13 00:28:59 +0200

    QueryRunsCol: new methods

--- src/ampel/pipeline/db/query/QueryRunsCol.py
+++ src/ampel/pipeline/db/query/QueryRunsCol.py
@@ -9,15 +9,57 @@
 
 from datetime import datetime, timedelta
 
-class QueryLastJobRun:
+class QueryRunsCol:
 	"""
 	"""
 
 	@staticmethod
-	def get(job_name, days_back=10):
+	def get_job_last_run(job_name, days_back=10):
 		"""
+		"""
+
+		ret = QueryRunsCol.get(
+			tier=3, job_name=job_name, days_back=days_back
+		)
+
+		ret.append(
+			{'$limit': 1} # take only the first entry
+		)
+
+		return ret
+
+
+	@staticmethod
+	def get_t0_stats(dt):
+		"""
+		"""
+
+		ret = QueryRunsCol.get(
+			tier=0, dt=dt,
+			days_back=(datetime.now()-datetime.fromtimestamp(dt)).days + 1
+		)
+
+		ret.append(
+			{
+                "$group": {
+                    "_id": 1,
+                    "alerts": {
+                        "$sum": "$jobs.metrics.count.alerts"
+                    }
+                }
+            }
+		)
+
+		return ret
+
+
+	@staticmethod
+	def get(tier=0, job_name=None, days_back=10, dt=None):
+		"""
+		tier: positive integer between 0 and 3
 		job_name: string
 		days_back: positive integer
+		dt: unix time
 		"""
 
 		# Array returned by this method
@@ -49,12 +91,19 @@ class QueryLastJobRun:
 				{'$match': {'_id': {'$in': match}}}
 			)
 
+		second_match_stage = {'jobs.tier': tier}
+
+		if job_name is not None:
+			second_match_stage['jobs.name'] = job_name
+
+		if dt is not None:
+			second_match_stage['jobs.dt'] = {'$gt': dt}
+
 		ret.extend(
 			[
 				{'$unwind': '$jobs'},
-				{'$match': {'jobs.tier': 3, 'jobs.name': job_name}},
+				{'$match': second_match_stage},
 				{'$sort': {'jobs.dt': -1}}, # sort jobs by descending datetime
-				{'$limit': 1} # take only the first entry
 			]
 		)
 
