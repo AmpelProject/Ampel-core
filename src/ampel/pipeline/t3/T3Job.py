723f50207df003546579216bd75e51e0a9b9fcc5
vbrinnel <vsbrin@gmail.com>
2018-07-13 11:41:58 +0200

    header

--- src/ampel/pipeline/t3/T3Job.py
+++ src/ampel/pipeline/t3/T3Job.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 26.02.2018
-# Last Modified Date: 09.07.2018
+# Last Modified Date: 13.07.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from datetime import datetime, timezone
