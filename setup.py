9ff8c7ea2d122f450a42c242d44e76d3b4ccc8d3
Jakob van Santen <jakob.van.santen@desy.de>
2018-08-22 17:37:01 +0200

    Make packageable

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
-from distutils.core import setup
-setup(name='ampel',
+from setuptools import setup
+setup(name='Ampel-core',
       version='0.4.0',
       package_dir={'':'src'},
-      packages=['ampel'],
+      package_data = {'': ['*.json']},
+      packages=[
+          'ampel.archive',
+          'ampel.core',
+          'ampel.core.abstract',
+          'ampel.core.flags',
+          'ampel.pipeline',
+          'ampel.pipeline.config',
+          'ampel.pipeline.common',
+          'ampel.pipeline.db',
+          'ampel.pipeline.db.query',
+          'ampel.pipeline.logging',
+          'ampel.pipeline.t0',
+          'ampel.pipeline.t0.alerts',
+          'ampel.pipeline.t0.filters',
+          'ampel.pipeline.t0.ingesters',
+          'ampel.pipeline.t2',
+          'ampel.pipeline.t3',
+          'ampel.test',
+          'ampel.utils',
+      ],
       entry_points = {
           'console_scripts' : [
               'ampel-alertprocessor = ampel.pipeline.t0.AlertProcessor:run_alertprocessor',
