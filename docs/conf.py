2d3400fc0aa825a262d62c0b6ebf60c4e7ad680e
Jakob van Santen <jvansanten@gmail.com>
2018-08-23 13:48:56 +0200

    Remove source code views from generated docs

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
+    # 'sphinx.ext.viewcode',
     'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
