f9fcda7f6d7d77479870f6f0f34cc9c920f25fad
vbrinnel <vsbrin@gmail.com>
2018-09-30 11:26:04 +0200

    pydantic based TimeConstraint

--- src/ampel/pipeline/t3/TimeConstraint.py
+++ src/ampel/pipeline/t3/TimeConstraint.py
@@ -4,127 +4,34 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 06.06.2018
-# Last Modified Date: 13.07.2018
+# Last Modified Date: 29.09.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from datetime import datetime, timedelta
-from voluptuous import Schema, Any, Required
-from types import MappingProxyType
 from ampel.pipeline.config.AmpelConfig import AmpelConfig
 from ampel.pipeline.db.AmpelDB import AmpelDB
+from ampel.pipeline.config.time.TimeDeltaConfig import TimeDeltaConfig
+from ampel.pipeline.config.time.TimeLastRunConfig import TimeLastRunConfig
+from ampel.pipeline.config.time.TimeStringConfig import TimeStringConfig
+from ampel.pipeline.config.time.UnixTimeConfig import UnixTimeConfig
 
 
 class TimeConstraint:
 	"""
-	* 'after' and 'before': either:
-		-> None
-		-> datetime.datetime
-		-> datetime.timedelta
-		-> dict (fulfilling TimeConstraint._sub_schema criteria)
-
-	dict examples fulfilling TimeConstraint._sub_schema criteria:
-
-		Example 1:
-			{
-				"after": {
-					"use": "$lastRunTime",
-					"jobName": "val_test"
-				},
-				"before": {
-					"use": "unixTime",
-					"value": 1531306299
-				}
-			}
-
-		Example 2:
-			{
-				"after": {
-					"use": "$timeDelta",
-					"arguments" :  {
-                       	"days" : -40
-                   	}
-				},
-				"before": {
-					"use": "formattedString",
-					"dateTimeStr": "21/11/06 16:30",
-					"dateTimeFormat": "%d/%m/%y %H:%M"
-				}
-			}
 	"""
 
-	_sub_schema = Schema(
-		Any(
-			None, datetime, timedelta,
-			{
-				Required('use'): '$timeDelta',
-				Required('arguments'): dict
-			},
-			{
-				Required('use'): '$lastRunTime',
-				Required('jobName'): str
-			},
-			{
-				Required('use'): 'formattedString',
-				Required('dateTimeStr'): str,
-				Required('dateTimeFormat'): str
-			},
-			{
-				Required('use'): 'unixTime',
-				Required('value'): int
-			}
-		)
-	)
-
-	schema = Schema(
-		Any(
-			None,
-			{
-				"after": _sub_schema, 
-				"before": _sub_schema
-			}
-		)
-	)
-
-
-	@staticmethod
-	def from_parameters(
-		time_constraint_dict, 
-		time_constraint_obj=None, runs_col=None, force_schema_check=False
-	):
-		"""	
-		"""	
-		if time_constraint_dict is None:
-			return None
-
-		if AmpelConfig.is_frozen():
-			if force_schema_check:
-				from ampel.pipeline.common.DevUtils import DevUtils
-				TimeConstraint.schema(
-					DevUtils.recursive_unfreeze(
-						time_constraint_dict
-					)
-				)
-		else:
-			TimeConstraint.schema(time_constraint_dict) # Robustness
-
-		tc = TimeConstraint() if time_constraint_obj is None else time_constraint_obj
-		tc.set_after(time_constraint_dict.get('after'), False)
-		tc.set_before(time_constraint_dict.get('before'), False)
-		return tc
-
-
-	def __init__(self, parameters=None):
+	def __init__(self, tc_config=None):
 		"""
-		parameters: dict instance with keys: 'after' and/or 'before'
-		* 'after' and 'before' dict values, either: 
-			-> datetime.datetime 
-			-> datetime.timedelta 
-		   	-> dict fulfilling TimeConstraint._sub_schema criteria (see class docstring)
+		:param TimeConstraintConfig tc_config: TimeConstraintConfig instance (see docstring) 
+		if not provided, please make sure to use the method 
+		:func:`set <ampel.pipeline.t3.TimeConstraint.set>` 
 		"""
+
 		self.constraints = {}
 
-		if parameters is not None:
-			self.from_parameters(parameters, self)
+		if tc_config is not None:
+			self.set("before", tc_config.before)
+			self.set("after", tc_config.after)
 
 
 	def has_constraint(self):
@@ -132,88 +39,87 @@ class TimeConstraint:
 		return len(self.constraints) > 0
 
 
-	def set_after(self, value, check_schema=True):
-		""" """
-		self._set('after', value, check_schema)
-
-
-	def set_before(self, value, check_schema=True):
-		""" """
-		self._set('before', value, check_schema)
-
-
 	def get_after(self):
 		""" """ 
-		return self._get('after')
+		return self.get('after')
 	
 
 	def get_before(self):
 		""" """ 
-		return self._get('before')
+		return self.get('before')
 
 
-	def _set(self, name, value, check_schema=True):
-		""" """ 
-		if check_schema:
-			TimeConstraint._sub_schema(value)
-		self.constraints[name] = value
+	def set(self, constraint_name, value):
+		""" 
+		:param str constraint_name: "before" or "after"
+		:param value: "before" or "after"
+		:type value: datetime or timedelta or TimeDeltaConfig or 
+		TimeLastRunConfig or UnixTimeConfig or TimeStringConfig
+		""" 
+
+		if constraint_name not in ["before", "after"]:
+			raise ValueError("constraint_name must be 'before' or 'after'")
+
+		if type(value) not in [
+			datetime, timedelta, TimeDeltaConfig, TimeLastRunConfig, 
+			UnixTimeConfig, TimeStringConfig
+		]: 
+			raise ValueError("Unsupported type")
+
+		self.constraints[constraint_name] = value
 
 
-	def _get(self, param):
+	def get(self, param):
 		""" 
 		param: either 'after' or 'before'
 		Schema validation ensures val can be only either None, dict, datetime or timedelta
 		"""
 
-		constraint = self.constraints.get(param)
+		tc = self.constraints.get(param, None)
 
-		if constraint is None:
+		if tc is None:
 			return None
 
-		if type(constraint) is datetime:
-			return constraint
+		tc_type = type(tc)
 
-		elif type(constraint) is timedelta:
-			return datetime.today() + constraint
+		if tc_type is datetime:
+			return tc
 
-		elif type(constraint) in (dict, MappingProxyType):
+		elif tc_type is timedelta:
+			return datetime.today() + tc
 
-			if constraint['use'] == '$timeDelta':
-				return datetime.today() + timedelta(**constraint['arguments'])
+		elif tc_type is TimeDeltaConfig:
+			return datetime.today() + timedelta(**tc.arguments)
 
-			if constraint['use'] == '$lastRunTime':
+		elif tc_type is TimeLastRunConfig:
 
-				from ampel.pipeline.db.query.QueryRunsCol import QueryRunsCol
-				col = AmpelDB.get_collection('runs')
+			from ampel.pipeline.db.query.QueryRunsCol import QueryRunsCol
+			col = AmpelDB.get_collection('jobs')
+			res = next(
+				col.aggregate(
+					QueryRunsCol.get_job_last_run(tc.jobName)
+				),
+				None
+			)
+
+			if res is None:
 				res = next(
 					col.aggregate(
-						QueryRunsCol.get_job_last_run(constraint['jobName'])
-					),
+						QueryRunsCol.get_job_last_run(
+							tc.jobName, days_back=None
+						)
+					), 
 					None
 				)
-
 				if res is None:
-					res = next(
-						col.aggregate(
-							QueryRunsCol.get_job_last_run(
-								constraint['jobName'], days_back=None
-							)
-						), 
-						None
-					)
-					if res is None:
-						return None
+					return None
 
-				return datetime.fromtimestamp(res['jobs']['dt'])
+			return datetime.fromtimestamp(res['jobs']['dt'])
 
-			if constraint['use'] == 'unixTime':
-				return datetime.fromtimestamp(constraint['value'])
+		elif tc_type is UnixTimeConfig:
+			return datetime.fromtimestamp(tc.value)
 
-			if constraint['use'] == 'formattedString':
-				# Schema validation ensures 'dateTimeFormat' is set when 'dateTimeStr' is
-				return datetime.strptime(
-					constraint['dateTimeStr'], 
-					constraint['dateTimeFormat']
-				)
+		elif tc_type is TimeStringConfig:
+			return datetime.strptime(tc.dateTimeStr, tc.dateTimeFormat)
 
-		raise ValueError("Illegal argument (type: %s)" % type(constraint))
+		raise ValueError("Illegal argument (type: %s)" % tc_type)
