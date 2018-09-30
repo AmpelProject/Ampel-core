9404847bcbd6fc4fa9d792230642af4cad36a5d8
vbrinnel <vsbrin@gmail.com>
2018-09-30 11:24:31 +0200

    T3JobConfig: removing voluptous checks. Moving ScheduleEvaluator in its own file

--- src/ampel/pipeline/t3/T3JobConfig.py
+++ src/ampel/pipeline/t3/T3JobConfig.py
@@ -7,15 +7,14 @@
 # Last Modified Date: 27.08.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-import ast
-import re
+import ast, re
 from functools import reduce
 from types import MappingProxyType
 from multiprocessing import Process
-from voluptuous import Schema, Required, Any, Optional, ALLOW_EXTRA
 
 from ampel.pipeline.common.AmpelUtils import AmpelUtils
 from ampel.pipeline.config.AmpelConfig import AmpelConfig
+from ampel.pipeline.config.ScheduleEvaluator import ScheduleEvaluator
 from ampel.pipeline.logging.AmpelLogger import AmpelLogger
 from ampel.pipeline.t3.T3TaskConfig import T3TaskConfig
 from ampel.pipeline.t3.TimeConstraint import TimeConstraint
@@ -25,55 +24,6 @@ from ampel.pipeline.t3.T3Job import T3Job
 class T3JobConfig:
 	"""
 	"""
-
-	_ampel_docs = Schema(
-		Any("TRANSIENT", "PHOTOPOINT", "UPPERLIMIT", "COMPOUND", "T2RECORD")
-	)
-
-	# static schema used for dict/json validation
-	job_schema = Schema(
-		{
-			Required('active'): bool,
-			Required('schedule'): Any(str, [str]),
-				# Possible 'schedule' values (https://schedule.readthedocs.io/en/stable/):
-				# "every(10).minutes"
-				# "every().hour"
-				# "every().day.at("10:30")"
-				# "every().monday"
-				# "every().wednesday.at("13:15")"
-			Required('task(s)'): Any(
-				T3TaskConfig.t3_task_schema, 
-				[T3TaskConfig.t3_task_schema]
-			),
-			'input': {
-				Required('select'): {
-					'created': TimeConstraint.schema,
-					'modified': TimeConstraint.schema,
-					'channel(s)': Any(str, [str]),
-					'withFlag(s)': Any(str, [str]),
-					'withoutFlag(s)': Any(str, [str])
-				},
-				Required('load'): {
-					Required('state'): Any("$all", "$latest"),
-					'doc(s)': Any(_ampel_docs, [_ampel_docs]),
-					't2(s)': Any(str, [str]),
-					Optional('verbose', default=True): bool,
-					Optional('debug', default=False): bool
-				},
-				'chunk': int
-			},
-			Optional('globalInfo', default=False): bool,
-			'onError': {
-				'sendMail': {
-					Required('to'): str,
-					Required('excStack'): bool
-				},
-				Optional('stopAmpel', default=False): bool,
-				Optional('retry', default=False): bool
-			}
-		}, 
-		extra=ALLOW_EXTRA
-	)
 
 	@classmethod
 	def load(cls, job_name, logger=None):
@@ -85,31 +35,23 @@ class T3JobConfig:
 		if logger is None:
 			logger = AmpelLogger.get_logger()
 
-		# Get, check and set defaults of t3 task doc 
-		job_doc = AmpelConfig.get_config(
-			't3Jobs.%s' % job_name, 
-			cls.job_schema
-		)
+		job_doc = AmpelConfig.get_config('t3Jobs.%s' % job_name)
 
 		# Robustness
 		if job_doc is None:
 			raise ValueError("Job %s not found" % job_name)
 
 		logger.info("Loading job %s" % job_name)
-		return cls.from_doc(job_name, job_doc, logger, validate=False)
+		return cls.from_doc(job_name, job_doc, logger)
 
 
 	@classmethod
-	def from_doc(cls, job_name, job_doc, logger=None, validate=True):
+	def from_doc(cls, job_name, job_doc, logger=None):
 		"""
 		"""
 
 		if logger is None:
 			logger = AmpelLogger.get_logger()
-
-		# validate
-		if validate:
-			job_doc = cls.job_schema(job_doc)
 
 		t3_task_configs = []
 		all_tasks_sels = {} 
@@ -221,38 +163,3 @@ class T3JobConfig:
 		evaluator = ScheduleEvaluator()
 		for sched_el in scheds if AmpelUtils.is_sequence(scheds) else [scheds]:
 			evaluator(scheduler, sched_el).do(self.launch_t3_job)
-
-import ast
-class ScheduleEvaluator(ast.NodeVisitor):
-	"""
-	Safely evaluate scheduling lines of the form
-	- `every(10).minutes`
-	- `every().hour``
-	- `every().day.at('10:30')`
-	- `every().monday`
-	- `every().wednesday.at('13:15')`
-	
-	Allows literal numbers, strings, calling member functions of schedule.Scheduler 
-	"""
-	def __call__(self, scheduler, line):
-		self._scheduler = scheduler
-		elem = ast.parse(line).body[0]
-		return self.visit(elem)
-	def generic_visit(self, node):
-		raise ValueError("Illegal operation {}".format(type(node)))
-	def visit_Num(self, node):
-		return node.n
-	def visit_Str(self, node):
-		return node.s
-	def visit_Name(self, node):
-		return node.id
-	def visit_Attribute(self, node):
-		return getattr(self.visit(node.value), node.attr)
-	def visit_Call(self, node):
-		args = [self.visit(arg) for arg in node.args]
-		func = self.visit(node.func)
-		if isinstance(func, str):
-			func = getattr(self._scheduler, func)
-		return func(*args)
-	def visit_Expr(self, node):
-		return self.visit(node.value)
