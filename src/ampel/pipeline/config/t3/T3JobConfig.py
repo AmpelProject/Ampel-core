f3befdd2f4188b29b42dccd200321a1b28b8c951
vbrinnel <vsbrin@gmail.com>
2018-09-30 15:48:30 +0200

    T3JobConfig: validated 'schedule' parameter. Inherit GettableConfig for convenience

--- src/ampel/pipeline/config/t3/T3JobConfig.py
+++ src/ampel/pipeline/config/t3/T3JobConfig.py
@@ -7,14 +7,20 @@
 # Last Modified Date: 29.09.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel
+import schedule
+from pydantic import BaseModel, validator
 from typing import Union, List
 from ampel.pipeline.common.docstringutils import gendocstring
+from ampel.pipeline.config.t3.ScheduleEvaluator import ScheduleEvaluator
 from ampel.pipeline.config.t3.T3JobTranConfig import T3JobTranConfig
+from ampel.pipeline.config.GettableConfig import GettableConfig
 from ampel.pipeline.config.channel.T3TaskConfig import T3TaskConfig
 
+def nothing():
+	pass
+
 @gendocstring
-class T3JobConfig(BaseModel):
+class T3JobConfig(BaseModel, GettableConfig):
 	"""
 	Possible 'schedule' values (https://schedule.readthedocs.io/en/stable/):
 	"every(10).minutes"
@@ -23,9 +29,21 @@ class T3JobConfig(BaseModel):
 	"every().monday"
 	"every().wednesday.at("13:15")"
 	"""
-	name: str
+	job: str
 	active: bool = True
 	globalInfo: bool = False
 	schedule: Union[str, List[str]]
 	transients: Union[None, T3JobTranConfig]
 	tasks: Union[T3TaskConfig, List[T3TaskConfig]]
+
+
+	@validator('schedule')
+	def schedule_must_not_contain_bad_things(cls, v):
+		"""
+		Safety check for "schedule" parameters 
+		"""
+		scheduler = schedule.Scheduler()
+		evaluator = ScheduleEvaluator()
+		for el in v if type(v) is str else [v]:
+			evaluator(scheduler, v).do(nothing)
+		return v
