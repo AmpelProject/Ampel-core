f44a8e83a1c2b10e48e259051a8339f786749407
vbrinnel <vsbrin@gmail.com>
2018-10-03 00:58:41 +0200

    Further T3 validation

--- src/ampel/pipeline/config/t3/TranContentConfig.py
+++ src/ampel/pipeline/config/t3/TranContentConfig.py
@@ -1,32 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/pipeline/config/t3/T3TranLoadConfig.py
+# File              : ampel/pipeline/config/t3/TranContentConfig.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 29.09.2018
-# Last Modified Date: 29.09.2018
+# Last Modified Date: 02.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, ValidationError
 from typing import Dict, Union, List
+from ampel.pipeline.common.AmpelUtils import AmpelUtils
 from ampel.pipeline.common.docstringutils import gendocstring
-from ampel.pipeline.config.t3.T3LoadableDocs import T3LoadableDocs
+from ampel.pipeline.config.GettableConfig import GettableConfig
+from ampel.pipeline.config.t3.LoadableContent import LoadableContent
 
 @gendocstring
-class T3TranLoadConfig(BaseModel):
+class TranContentConfig(BaseModel, GettableConfig):
 	""" """
-	state: str = "$latest"
-	docs: Union[T3LoadableDocs, List[T3LoadableDocs]] = None
-	t2s: Union[None, str, List[str]] = None
-	verbose: bool = True
-	debug: bool = False
+	docs: Union[LoadableContent, List[LoadableContent]]
+	t2SubSelection: Union[None, str, List[str]] = None
 
 
-	@validator('state')
-	def validate_state(cls, v):
-		"""
-		"""
-		if v != "$latest" and v != "$all":
-			raise ValueError('Parameter "state" must be either "$latest" of "$all"')
-
+	@validator('docs', 't2SubSelection', pre=True, whole=True)
+	def make_it_a_list(cls, v):
+		if type(v) is not list:
+			return [v]
 		return v
+
+
+	@validator('t2SubSelection', whole=True)
+	def check_correct_use_of_subselection(cls, t2SubSelection, values, **kwargs):
+		"""
+		Check transients->select->t2s
+		"""
+
+		if t2SubSelection and values.get("docs"):
+
+			docs = values.get("docs")
+			if not AmpelUtils.is_sequence(docs):
+				docs = [docs]
+
+			if LoadableContent.T2RECORD not in docs:
+				AmpelUtils.print_and_raise(
+					"T3 config error",
+					"T2RECORD must be defined in transients->select->docs\n"+
+					"when transients->content->t2SubSelection (%s) filtering\nis requested." % t2SubSelection
+				)
+
+		return t2SubSelection
--- src/ampel/pipeline/config/t3/TranSelectConfig.py
+++ src/ampel/pipeline/config/t3/TranSelectConfig.py
@@ -7,16 +7,23 @@
 # Last Modified Date: 29.09.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Union, List
 from ampel.pipeline.config.time.TimeConstraintConfig import TimeConstraintConfig
+from ampel.pipeline.config.GettableConfig import GettableConfig
 from ampel.pipeline.common.docstringutils import gendocstring
 
 @gendocstring
-class T3TranSelectConfig(BaseModel):
+class TranSelectConfig(BaseModel, GettableConfig):
 	""" """
 	created: Union[None, TimeConstraintConfig] = None
 	modified: Union[None, TimeConstraintConfig] = None
 	channels: Union[None, str, List[str]] = None
 	withFlags: Union[None, str, List[str]] = None
 	withoutFlags: Union[None, str, List[str]] = None
+
+	@validator('channels', 'withFlags', 'withoutFlags', pre=True, whole=True)
+	def make_it_a_list(cls, v):
+		if type(v) is not list:
+			return [v]
+		return v
