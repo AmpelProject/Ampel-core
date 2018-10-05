a07098421cfee19f733338c50dd1c0859329cb3f
vbrinnel <vsbrin@gmail.com>
2018-10-05 18:27:18 +0200

    T3Task v0.5

--- src/ampel/pipeline/t3/T3Task.py
+++ src/ampel/pipeline/t3/T3Task.py
@@ -4,33 +4,58 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 06.03.2018
-# Last Modified Date: 04.08.2018
+# Last Modified Date: 03.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import logging
 from ampel.pipeline.common.AmpelUtils import AmpelUtils
+from ampel.pipeline.common.AmpelUnitLoader import AmpelUnitLoader
+from ampel.pipeline.config.AmpelConfig import AmpelConfig
+from ampel.pipeline.config.t3.T3TaskConfig import T3TaskConfig
 from ampel.pipeline.logging.AmpelLogger import AmpelLogger
 from ampel.base.TransientView import TransientView
 from ampel.pipeline.logging.DBLoggingHandler import DBLoggingHandler
 
 class T3Task:
 	"""
-	Instances of this class are typically created by the method create_tasks
-	of an instance of ampel.pipeline.t3.T3TaskBody
 	"""
 
-	def __init__(self, t3_task_config, channels, logger, global_info=None):
+	@classmethod
+	def load_by_name(cls, job_name, task_name, logger=None):
 		"""
-		:param t3_task_config: instance of child class of ampel.pipeline.t3.T3TaskConfig
-		:param channels: channel(s) sub-selection: string or list of strings
-		:param logger: logger from python module 'logging'
-		:param global_info: optional dict instance containing info such as:
-			* last time the associated job was run
-			* number of alerts processed since
+		:param str job_name: name of the job parent of this task
+		:param str task_name: name of this task
+		:param Logger logger: logger instance from python module 'logging'
+		:returns: an instance of ampel.pipeline.t3.T3TaskConfig
 		"""
 
-		self.task_config = t3_task_config
-		self.channels = channels
+		job_dict = AmpelConfig.get_config('t3Jobs').get(job_name)
+
+		if job_dict is None:
+			raise ValueError("Job %s not found" % job_name)
+
+		for task_dict in job_dict['tasks']:
+			if task_dict['task'] == task_name:
+				return cls(
+					T3TaskConfig(**task_dict), 
+					logger
+				)
+
+		raise ValueError("Task %s not found" % task_name)
+
+
+
+	def __init__(self, task_config, logger, global_info=None):
+		"""
+		:param T3TaskConfig task_config: child instance of ampel.pipeline.config.t3.T3TaskConfig
+		:param channels: optional channels override: string or list of strings
+		:param Logger logger: logger instance (python module 'logging')
+		:param dict global_info: optional dict containing info such as:
+		* last time the associated job was run
+		* number of alerts processed since
+		"""
+
+		self.task_config = task_config
 		self.logger = logger
 		self.db_logging_handler = None
 		self.fed_tran_ids = []
@@ -41,18 +66,24 @@ class T3Task:
 				self.db_logging_handler = handler
 
 		# Instanciate t3 unit
-		self.t3_instance = t3_task_config.t3_unit_class(
-			logger, 
-			t3_task_config.t3_resources,
-			t3_task_config.t3_unit_run_config,
-			global_info
+		T3Unit = AmpelUnitLoader.get_class(
+			tier=3, unit_name=task_config.unitId
+		)
+
+		# Gather resources associated with this unit
+		unit_resources = {
+			k: AmpelConfig.get_config('resources.{}'.format(k)) 
+			for k in getattr(T3Unit, 'resources', [])
+		}
+
+		self.t3_instance = T3Unit(
+			logger, unit_resources, 
+			task_config.runConfig, global_info
 		)
 
 		# Feedback
 		logger.propagate_log(
-			logging.INFO,
-			"%s: task instantiated" % 
-			t3_task_config.log_header
+			logging.INFO, "%s: task instantiated" % task_config.log_header
 		)
 
 
@@ -65,9 +96,7 @@ class T3Task:
 
 	def update(self, tran_register):
 		"""
-		tran_register: dict instance.
-			key: transient id
-			value: instance of ampel.pipeline.t3.TransientData
+		:param dict tran_register: key: transient id, value: instance of ampel.pipeline.t3.TransientData
 		"""
 
 		# Build specific array of ampel TransientView instances where each transient 
@@ -76,7 +105,7 @@ class T3Task:
 
 		# Append channel info to upcoming DB logging entries
 		if self.db_logging_handler:
-			self.db_logging_handler.set_channels(self.channels)
+			self.db_logging_handler.set_channels(self.task_config.channels)
 
 		self.logger.info("~"*60)
 		self.logger.info("%s: creating TransientViews" % self.task_config.log_header)
@@ -84,7 +113,7 @@ class T3Task:
 		for tran_id, tran_data in tran_register.items():
 
 			# Create transientView for specified channels using transient data
-			tran_view = tran_data.create_view(self.channels, self.task_config.t2_ids)
+			tran_view = tran_data.create_view(self.task_config.channels, self.task_config.t2_ids)
 
 			# No view exists for the given channel(s)
 			if tran_view is None:
@@ -97,7 +126,7 @@ class T3Task:
 			# Feedback
 			self.logger.debug(
 				"TransientView created: ID: %s, CN: %s, %s" % 
-				(tran_id, self.channels, TransientView.content_summary(tran_view))
+				(tran_id, self.task_config.channels, TransientView.content_summary(tran_view))
 			)
 
 			# Save ids of created views (used later for updating transient journal)
