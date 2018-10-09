ff137684c88f062c31cb3d79ef5ac0a61cbdef82
vbrinnel <vsbrin@gmail.com>
2018-10-09 19:41:16 +0200

    ChannelConfig: out of the box support for frozen channel configurations

--- src/ampel/pipeline/config/channel/ChannelConfig.py
+++ src/ampel/pipeline/config/channel/ChannelConfig.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/pipeline/config/ChannelConfig.py
+# File              : ampel/pipeline/config/channel/ChannelConfig.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 02.09.2018
-# Last Modified Date: 02.09.2018
+# Last Modified Date: 09.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from pydantic import BaseModel, validator
-from typing import List, Dict, Any, Union
+from typing import List, Sequence, Any, Union, Tuple
 from ampel.pipeline.common.docstringutils import gendocstring
 from ampel.pipeline.config.channel.StreamConfig import StreamConfig
 from ampel.pipeline.config.t3.T3TaskConfig import T3TaskConfig
+from ampel.pipeline.config.ReadOnlyDict import ReadOnlyDict
 
 @gendocstring
 class ChannelConfig(BaseModel):
@@ -21,14 +22,26 @@ class ChannelConfig(BaseModel):
 	channel: str
 	active: bool = True
 	author: str = "Unspecified"
-	sources: Union[StreamConfig, List[StreamConfig]]
-	t3Supervise: Union[None, T3TaskConfig, List[T3TaskConfig]] = None
+	# pydantic does not support typing.Sequence
+	# https://github.com/samuelcolvin/pydantic/issues/185
+	sources: Union[List[StreamConfig], Tuple[StreamConfig]]
+	t3Supervise: Union[None, List[T3TaskConfig], Tuple[T3TaskConfig]] = None
+
+	def __init__(self, **arg):
+
+		# Because we allow - out of convenience - for sources or t3Supervise
+		# to be a single dict (a cast into tuple happens then, which modifies input)
+		if isinstance(arg['sources'], dict) or isinstance(arg.get('t3Supervise'), dict):
+			super().__init__(**dict(arg)) # shallow copy
+		else:
+			super().__init__(**arg)
+
 
 	@validator('sources', pre=True, whole=True)
-	def make_it_a_list(cls, sources):
+	def cast_to_tuple(cls, sources):
 		""" """
-		if type(sources) is not list:
-			return [sources]
+		if type(sources) is dict:
+			return (sources,)
 		return sources
 
 	def get_stream_config(self, source):
--- src/ampel/pipeline/config/channel/StreamConfig.py
+++ src/ampel/pipeline/config/channel/StreamConfig.py
@@ -4,11 +4,11 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 02.09.2018
-# Last Modified Date: 03.09.2018
+# Last Modified Date: 09.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel
-from typing import List, Dict, Any, Union
+from pydantic import BaseModel, validator
+from typing import Tuple, Dict, Union, Any, List
 from ampel.pipeline.common.docstringutils import gendocstring
 from ampel.pipeline.config.channel.T0UnitConfig import T0UnitConfig
 from ampel.pipeline.config.channel.T2UnitConfig import T2UnitConfig
@@ -20,6 +20,14 @@ class StreamConfig(BaseModel):
 	-> values defined in channel configuration section 'sources'
 	"""
 	stream: str
-	parameters: Union[None, Dict[str, Any]] = None
+	parameters: Union[None, Any] = None
 	t0Filter: T0UnitConfig
-	t2Compute: Union[None, List[T2UnitConfig]] = None
+	# pydantic does not support typing.Sequence
+	t2Compute: Union[None, List[T2UnitConfig], Tuple[T2UnitConfig]] = None
+
+	@validator('t2Compute', pre=True, whole=True)
+	def cast_to_tuple(cls, arg):
+		""" """
+		if type(arg) is dict:
+			return (arg,)
+		return arg
