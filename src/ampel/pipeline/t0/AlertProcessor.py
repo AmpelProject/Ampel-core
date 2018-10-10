b1dc18292593840b212ed7a057d43e6edb0424a0
vbrinnel <vsbrin@gmail.com>
2018-10-10 16:27:21 +0200

    ! AMPEL CODE ANNIVERSARY !

--- src/ampel/pipeline/t0/AlertProcessor.py
+++ src/ampel/pipeline/t0/AlertProcessor.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 10.10.2017
-# Last Modified Date: 09.10.2018
+# Last Modified Date: 10.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import time, pkg_resources, numpy as np
