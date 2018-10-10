871b82550d29734f7cb85c513ba9de30a54bf399
vbrinnel <vsbrin@gmail.com>
2018-10-11 00:50:35 +0200

    Channel config improvements

--- src/ampel/pipeline/config/channel/T0UnitConfig.py
+++ src/ampel/pipeline/config/channel/T0UnitConfig.py
@@ -7,14 +7,44 @@
 # Last Modified Date: 17.09.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
+import pkg_resources
 from typing import Dict, Any, Union
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from ampel.pipeline.common.docstringutils import gendocstring
+from ampel.pipeline.config.AmpelConfig import AmpelConfig
 
 @gendocstring
 class T0UnitConfig(BaseModel):
 	"""
 	Config holder for T0 units (filters)
 	"""
+
 	unitId: str
 	runConfig: Union[None, Dict[str, Any]] = None
+
+
+	@validator('unitId', pre=True)
+	def validate_units(cls, value):
+		""" """
+
+		if (
+			"t0" in AmpelConfig._ignore_unavailable_units or 
+			value in AmpelConfig._ignore_unavailable_units
+		):
+			return value
+
+		if next(
+			pkg_resources.iter_entry_points('ampel.pipeline.t0.units', value), 
+			None
+		) is None:
+			cls.print_and_raise(
+				header="t0Filter->unitId config error",
+				msg="Unknown T0 unit: '%s'\n" % value +
+					"Please either install the corresponding Ampel plugin\n" +
+					"or explicitely ignore unavailable T0 units by calling\n" + 
+					"AmpelConfig.ignore_unavailable_units('t0')\n" +
+					"or alternatively load ampel defaults using:\n" +
+					"AmpelConfig.load_defaults(ignore_unavailable_units=['t0'])"
+			)
+
+		return value
--- src/ampel/pipeline/config/channel/T2UnitConfig.py
+++ src/ampel/pipeline/config/channel/T2UnitConfig.py
@@ -1,19 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/pipeline/config/channel/T0UnitConfig.py
+# File              : ampel/pipeline/config/channel/T2UnitConfig.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 02.09.2018
-# Last Modified Date: 17.09.2018
+# Last Modified Date: 10.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel
+import pkg_resources
+from pydantic import BaseModel, validator
 from ampel.pipeline.common.docstringutils import gendocstring
+from ampel.pipeline.config.AmpelModelExtension import AmpelModelExtension
+from ampel.pipeline.config.AmpelConfig import AmpelConfig
+
 
 @gendocstring
-class T2UnitConfig(BaseModel):
+class T2UnitConfig(BaseModel, AmpelModelExtension):
 	"""
 	Config holder for T2 units
 	"""
+
 	unitId: str
 	runConfig: str = "default"
+
+
+	@validator('unitId', pre=True)
+	def validate_units(cls, value):
+		""" """
+		if (
+			"t2" in AmpelConfig._ignore_unavailable_units or 
+			value in AmpelConfig._ignore_unavailable_units
+		):
+			return value
+
+		if next(
+			pkg_resources.iter_entry_points('ampel.pipeline.t2.units', value), 
+			None
+		) is None:
+			cls.print_and_raise(
+				header="t2Compute->unitId config error",
+				msg="Unknown T2 unit: '%s'\n" % value +
+					"Please either install the corresponding Ampel plugin\n" +
+					"or allow explicitely unavailable T2 units by calling\n" + 
+					"AmpelConfig.ignore_unavailable_units('t2')\n" +
+					"or alternatively load ampel defaults using:\n" +
+					"AmpelConfig.load_defaults(ignore_unavailable_units=['t2'])"
+			)
+
+		return value
