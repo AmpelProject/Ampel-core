c622ee2b6b6f7b336fbcdbb447e6ba467ff41f4d
vbrinnel <vsbrin@gmail.com>
2018-10-10 18:40:38 +0200

    ChannelConfigLoader: docstring update. v0.5 compatibility

--- src/ampel/pipeline/config/channel/ChannelConfigLoader.py
+++ src/ampel/pipeline/config/channel/ChannelConfigLoader.py
@@ -4,29 +4,45 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 03.05.2018
-# Last Modified Date: 17.09.2018
+# Last Modified Date: 10.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from ampel.pipeline.config.AmpelConfig import AmpelConfig
 from ampel.pipeline.config.channel.ChannelConfig import ChannelConfig
 
-class ChannelLoader:
+class ChannelConfigLoader:
 	"""
+	Convenience class that creates :obj:`ChannelConfig <ampel.pipeline.config.channel.ChannelConfig>`
+	using channel dicts loaded from AmpelConfig (retrieved by channel names).
 	"""
 
 	@staticmethod
-	def load_channels(chan_names=None, logger=None):
+	def load_configurations(chan_names=None, tier="all", logger=None):
 		"""
-		:param chan_names: list of strings
+		Convenience method that creates :obj:`ChannelConfig <ampel.pipeline.config.channel.ChannelConfig>`
+		using channel dicts loaded from AmpelConfig (retrieved by channel names).
+		If no channel name is provided, all _active_ channels are loaded.
+		If a list of name is provided, a warning will be printer (provided argument _logger_ is not None)
+		when loaded non-active channels.
+		Loading lighter ChannelConfig instances taylored for usage at given tier level is possible 
+		using the _tier_ argument (see _tier_ parameter for more info)
+
+		:param chan_names: list of channel names
+		:type chan_names: list(str), str
 		:param logger: logger instance (python logging module)
+		:param tier: at which tier level the returned ChannelConfig will be used. 
+		Possible values are: 'all', 0, 3.
+		Less checks are performed when restricting tier to 0 or 3 which yields 
+		a lighter and quicker ChannelConfig loading procedure. For example, with tier=0, 
+		T3 units existence or T3 run configurations are not checked.
+		:type tier: str, int
+		:raises NameError: if a channel is not found
+		:raises ValueError: if duplicate values are found in chan_names
 		:returns: list of ampel.pipeline.config.channel.ChannelConfig instances
-		:raises ValueError: if
-		  - duplicate values are found in chan_names
-		  - channels with duplicate names are found (chan_names == None)
-		:raises NameError: if a channel name (chan_names) is not found
+		:rtype: list(ampel.pipeline.config.channel.ChannelConfig)
 		"""
 
-		# list of chan docs
+		# list of chan configs
 		ret = []
 
 		# Robustness check
@@ -42,12 +58,12 @@ class ChannelLoader:
 					continue
 
 				ret.append(
-					ChannelConfig.parse_obj(chan_doc)
+					ChannelConfig.create(tier, **chan_doc)
 				)
 
 		else:
 
-			if type(chan_names) is str:
+			if isinstance(chan_names, str):
 				chan_names = [chan_names]
 
 			if len(chan_names) != len(set(chan_names)):
@@ -62,17 +78,7 @@ class ChannelLoader:
 					logger.info("Loading requested non-active channel %s" % chan_doc['channel'])
 
 				ret.append(
-					ChannelConfig.parse_obj(chan_doc)
+					ChannelConfig.create(tier, **chan_doc)
 				)
-
-			if len(ret) != len(chan_names):
-				for chan_name in chan_names:
-					if next(
-						filter(
-							lambda x: x['channel'] == chan_name, 
-							AmpelConfig.get_config('channels')
-						), None
-					) is None:
-						raise NameError("Channel '%s' not found" % chan_name)
 
 		return ret
