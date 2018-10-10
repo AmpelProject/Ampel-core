53bf8f70e31e76d675520fc628c7d3dbc4e7f05a
vbrinnel <vsbrin@gmail.com>
2018-10-10 13:35:17 +0200

    AmpelModelExtension: own exception. better traceback

--- src/ampel/pipeline/config/AmpelModelExtension.py
+++ src/ampel/pipeline/config/AmpelModelExtension.py
@@ -11,22 +11,48 @@ from ampel.pipeline.common.AmpelUtils import AmpelUtils
 
 class AmpelModelExtension:
 
+	class ValidationError(Exception):
+		"""
+		We use our own ValidationError because pydantic catches ValueErrors a-like 
+		expressions in order to build a summary of validation errors.
+		Pblm is: this summary is hardly understandable and distracts attention 
+		from the root cause of the pblm. We could rise a - say RunTimeError - 
+		but this would be misleading since it is a validation problem. 
+		So we create our own ValidationError (that is not caught by pydantic like ValueError) 
+		which as a consequence breaks the chain of validation as soon as it is raised.
+		"""
+		pass
+
+
 	def get(self, path):
 		return AmpelUtils.get_nested_attr(self, path)
 
-	@staticmethod	
-	def print_and_raise(msg, header=None):
+
+	@classmethod	
+	def print_and_raise(cls, msg, header=None):
 		"""
 		Prints a msg and raises a ValueError with the same msg.
 		Main use: sometimes, pydantic ValueError do not propagate properly
 		and secondary Exceptions occur. 
 		Printing the msg helps troubleshooting bad configurations.
 		"""
+
+		len_msg=0
+		output="\n\n"
+		for el in msg.split('\n'):
+			if len(el) > len_msg:
+				len_msg = len(el)
+
 		if header:
-			print("#"*len(header))
-			print(header)
+			output += "#"*len(header)
+			output += "\n" + header
+			if len(msg.split('\n')) > 1:
+				output += "\n" + "-"*len(header)
 		else:
-			print("#"*len(msg))
-		print(msg)
-		print("#"*len(msg))
-		raise ValueError(msg)
+			output += "#"*len_msg
+
+		output += "\n" + msg
+		output += "\n" + "#"*len_msg + "\n"
+
+		print(output)
+		raise cls.ValidationError(output) from None
