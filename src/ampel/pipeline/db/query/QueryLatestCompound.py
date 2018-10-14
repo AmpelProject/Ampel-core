2475f0c6bb8bcabe5dabe2073be97d9878b7bfc5
vbrinnel <vsbrin@gmail.com>
2018-10-15 00:26:45 +0200

    QueryLatestCompound: use new logic input schema. Autodoc compatible docstring

--- src/ampel/pipeline/db/query/QueryLatestCompound.py
+++ src/ampel/pipeline/db/query/QueryLatestCompound.py
@@ -4,15 +4,13 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 13.01.2018
-# Last Modified Date: 04.07.2018
+# Last Modified Date: 12.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import collections, bson
 from ampel.core.flags.AlDocType import AlDocType
-from ampel.core.flags.FlagUtils import FlagUtils
 from ampel.pipeline.common.AmpelUtils import AmpelUtils
-from ampel.pipeline.db.query.QueryMatchFlags import QueryMatchFlags
-from ampel.pipeline.db.query.QueryMatchCriteria import QueryMatchCriteria
+from ampel.pipeline.db.query.QueryMatchSchema import QueryMatchSchema
 
 class QueryLatestCompound:
 	"""
@@ -21,74 +19,63 @@ class QueryLatestCompound:
 	@staticmethod
 	def fast_query(tran_ids, channels=None):
 		"""
-		Note:
-		-----
-		! Must be used on transients whose compounds were solely created by T0 !
+		| **Must be used on transients whose compounds were solely created by T0** \
 		(i.e with no T3 compounds)
+		| Should perform faster than general_query.
 
-		Returns:
-		--------
-		A dict instance to be used with the mongoDB *aggregation* framework.
+		:param tran_ids: transient id(s), query can/should be performed on multiple ids at once.
+		:type tran_id: str, list(str), set(str). 
 
-		Example:
-		--------
-		In []: cursor = col.aggregate(
-			QueryLatestCompound.fast_query(
-				ZTFUtils.to_ampel_id(
-					['ZTF18aaayyuq', 'ZTF17aaagvng', 'ZTF18aaabikt']
+		:type channels: str, dict
+		:param channels: string (one channel only) or a dict schema \
+		(see :obj:`QueryMatchSchema <ampel.pipeline.db.query.QueryMatchSchema>` \
+		for syntax details). None (no criterium) means all channels are considered. 
+
+		:returns: a dict instance to be used with the mongoDB **aggregation** framework.
+		:rtype: dict
+
+		Example:\n
+		.. sourcecode:: python\n
+			In []: cursor = col.aggregate(
+				QueryLatestCompound.fast_query(
+					ZTFUtils.to_ampel_id(
+						['ZTF18aaayyuq', 'ZTF17aaagvng', 'ZTF18aaabikt']
+					)
 				)
 			)
-		)
 
-		In []: list(cursor)
-		Out[]: 
-		[
-			{'_id': Binary(b'T6TG\x96\x80\x1d\x86\x9f\x11\xf2G\xe7\xf4\xe0\xc3', 5), 'tranId': 'ZTF18aaayyuq'},
- 			{'_id': Binary(b'\xaaL|\x94?\xa4\xa1D\xbe\x0c[D\x9b\xc6\xe6o', 5), 'tranId': 'ZTF18aaabikt'},
- 			{'_id': Binary(b'\xaaL|\x14?\xb4\xc3D\xd2\x2a?L\x9a\xa6\xa1o', 5), 'tranId': 'ZTF17aaagvng'}
-		]
-
-		Parameters	
-		----------
-		tran_id: transient id(s) (string, list of string, set of strings). 
-		Query can be performed on multiple ids at once.
-
-		channels: 
-			-> a list of strings (whereby string = channel id)
-			-> a 2d list of strings 
-				* outer list: flag ids connected by OR
-				* innerlist: flag ids connected by AND
-
-		Should perform faster than general_query.
+			In []: list(cursor)
+			Out[]: 
+			[
+				{'_id': Binary(b'T6TG\x96\x80\x1d\x86\x9f\x11\xf2G\xe7\xf4\xe0\xc3', 5), 'tranId': 'ZTF18aaayyuq'},
+ 				{'_id': Binary(b'\xaaL|\x94?\xa4\xa1D\xbe\x0c[D\x9b\xc6\xe6o', 5), 'tranId': 'ZTF18aaabikt'},
+ 				{'_id': Binary(b'\xaaL|\x14?\xb4\xc3D\xd2\x2a?L\x9a\xa6\xa1o', 5), 'tranId': 'ZTF17aaagvng'}
+			]
 		"""
 
 		# Robustness
 		type_tran_ids = type(tran_ids)
 		if type_tran_ids in (list, tuple, set):
 			if not AmpelUtils.check_seq_inner_type(tran_ids, (int, str, bson.int64.Int64)):
-				raise ValueError("Elements of tran_ids sequence must be have type str or int")
+				raise ValueError("Elements of tran_ids sequence must have type str or int")
 		elif not type_tran_ids in (int, str, bson.int64.Int64):
 			raise ValueError("tran_ids must have type str or int (or sequence of these types)")
 
-		match_dict = {
-			'alDocType': AlDocType.COMPOUND
-		}
+		query = {'alDocType': AlDocType.COMPOUND}
 
-		match_dict['tranId'] = ( 
+		query['tranId'] = ( 
 			tran_ids if type(tran_ids) in (str, int)
 			else {'$in': tran_ids if type(tran_ids) is list else list(tran_ids)}
 		)
 
 		if channels is not None:
-			QueryMatchCriteria.add_from_list(
-				match_dict, 'channels',
-				(channels if not FlagUtils.contains_enum_flag(channels) 
-				else FlagUtils.enum_flags_to_lists(channels)), 
+			QueryMatchSchema.apply_schema(
+				query, 'channels', channels
 			)
 
 		return [
 			{
-				'$match': match_dict
+				'$match': query
 			},
 			{
 				'$project': {
@@ -126,91 +113,90 @@ class QueryLatestCompound:
 	@staticmethod
 	def general_query(tran_id, project=None, channels=None):
 		"""
-		Can be used on any ampel transients.
-		There is a very detailed explanation of each step of the aggragetion 
+		| Can be used on any ampel transients.
+		| There is a very detailed explanation of each step of the aggragetion \
 		documented in the python notebook "T3 get_lastest_compound"
 
-		Returns:
-		--------
-		A dict instance to be used with the mongoDB *aggregation* framework.
+		:param tran_id: transient id (string). Query **CANNOT** be performed on multiple ids at once.
+		:param dict project: optional projection stage at the end of the aggregation
 
-		Parameters	
-		----------
-		tran_id: transient id (string). Query can *not* be done on multiple ids at once.
-		project: optional projection stage at the end of the aggregation (dict instance)
-		channels: see 'fast_query' docstring
+		:type channels: str, dict
+		:param channels: string (one channel only) or a dict schema \
+		(see :obj:`QueryMatchSchema <ampel.pipeline.db.query.QueryMatchSchema>` \
+		for syntax details). None (no criterium) means all channels are considered. 
 
-		Examples:
-		---------
-		IMPORTANT NOTE: the following two examples show the output of the aggregation framework 
-		from mongodb (i.e the output after having performed a DB query *using the output 
-		of this fuunction as parameter*), they do not show the output of this function.
+		:returns: A dict instance intended to be used with the mongoDB **aggregation** framework.
+		:rtype: dict
 
-		*MONGODB* Output example 1:
-		---------------------------
-		In []: list(
-			col.aggregate(
-				QueryLatestCompound.general_query('ZTF18aaayyuq')
-			)
-		)
-		Out[]: 
-		[
-		  {
-			'_id': Binary(b'T6TG\x96\x80\x1d\x86\x9f\x11\xf2G\xe7\xf4\xe0\xc3', 5),
-			  'added': 1520796310.496276,
-			  'alDocType': 2,
-			  'channels': ['HU_SN1'],
-			  'lastppdt': 2458158.7708565,
-			  'len': 12,
-			  'pps': [{'pp': 375300016315010040},
-			   {'pp': 375320176315010034},
-			   {'pp': 375337116315010046},
-			   {'pp': 375356366315010056},
-			   {'pp': 377293446315010009},
-			   {'pp': 377313156315010027},
-			   {'pp': 377334096315010020},
-			   {'pp': 377376126315010004},
-			   {'pp': 377416496315010000},
-			   {'pp': 378293006315010001},
-			   {'pp': 378334946315010000},
-			   {'pp': 404270856315015007}],
-			  'tier': 0,
-			  'tranId': 'ZTF18aaayyuq'
-			}
-		]
+		IMPORTANT NOTE: the following two examples show the output of the aggregation framework \
+		from mongodb (i.e the output after having performed a DB query **using the output \
+		of this fuunction as parameter**), they do not show the output of this function.
 
-		*MONGODB* Output example 2:
-		---------------------------
-		In []: list(
-			col.aggregate(
-				QueryLatestCompound.general_query(
-					'ZTF18aaayyuq', project={'$project': {'tranId':1}}
+		**MONGODB** Output example 1:\n
+		.. sourcecode:: python\n
+			In []: list(
+				col.aggregate(
+					QueryLatestCompound.general_query('ZTF18aaayyuq')
 				)
 			)
-		)
-		Out[]: 
-			[{'_id': '5de2480f28bfca0bd3baae890cb2d2ae', 'tranId': 'ZTF18aaayyuq'}]
+			Out[]: 
+			[
+			  {
+				'_id': Binary(b'T6TG\x96\x80\x1d\x86\x9f\x11\xf2G\xe7\xf4\xe0\xc3', 5),
+				  'added': 1520796310.496276,
+				  'alDocType': 2,
+				  'channels': ['HU_SN1'],
+				  'lastppdt': 2458158.7708565,
+				  'len': 12,
+				  'pps': [{'pp': 375300016315010040},
+				   {'pp': 375320176315010034},
+				   {'pp': 375337116315010046},
+				   {'pp': 375356366315010056},
+				   {'pp': 377293446315010009},
+				   {'pp': 377313156315010027},
+				   {'pp': 377334096315010020},
+				   {'pp': 377376126315010004},
+				   {'pp': 377416496315010000},
+				   {'pp': 378293006315010001},
+				   {'pp': 378334946315010000},
+				   {'pp': 404270856315015007}],
+				  'tier': 0,
+				  'tranId': 'ZTF18aaayyuq'
+				}
+			]
+
+		**MONGODB** Output example 2:\n
+		.. sourcecode:: python\n
+			In []: list(
+				col.aggregate(
+					QueryLatestCompound.general_query(
+						'ZTF18aaayyuq', project={'$project': {'tranId':1}}
+					)
+				)
+			)
+			Out[]: 
+				[{'_id': '5de2480f28bfca0bd3baae890cb2d2ae', 'tranId': 'ZTF18aaayyuq'}]
 		"""
 
 		# Robustness
 		if isinstance(tran_id, collections.Sequence) or not type(tran_id) in (str, int):
-			raise ValueError("Type of tran_id must be a string or an int (multi tran_id queries not supported)")
+			raise ValueError(
+				"Type of tran_id must be a string or an int (multi tran_id queries not supported)"
+			)
 
-		match_dict = {
+		query = {
 			'tranId': tran_id, 
 			'alDocType': AlDocType.COMPOUND
 		}
 
 		if channels is not None:
-			QueryMatchCriteria.add_from_list(
-				match_dict, 'channels',
-				(channels if not FlagUtils.contains_enum_flag(channels) 
-				else FlagUtils.enum_flags_to_lists(channels)), 
+			QueryMatchSchema.apply_schema(
+				query, 'channels', channels
 			)
 
 		ret = [
 			{
-				'$match': match_dict
+				'$match': query
 			},
 			{
 				'$group': {
