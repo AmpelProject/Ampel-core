80c1f9beae53919e4293078eacae07c10c6e7873
vbrinnel <vsbrin@gmail.com>
2018-10-15 00:28:11 +0200

    QueryMatchTransients: use new logic input schema. Autodoc compatible docstring

--- src/ampel/pipeline/db/query/QueryMatchTransients.py
+++ src/ampel/pipeline/db/query/QueryMatchTransients.py
@@ -4,18 +4,13 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 13.01.2018
-# Last Modified Date: 04.07.2018
+# Last Modified Date: 12.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-
 from bson.objectid import ObjectId
-from datetime import datetime, timedelta
-
 from ampel.core.flags.AlDocType import AlDocType
-from ampel.core.flags.FlagUtils import FlagUtils
 from ampel.pipeline.t3.TimeConstraint import TimeConstraint
-from ampel.pipeline.db.query.QueryMatchFlags import QueryMatchFlags
-from ampel.pipeline.db.query.QueryMatchCriteria import QueryMatchCriteria
+from ampel.pipeline.db.query.QueryMatchSchema import QueryMatchSchema
 
 
 class QueryMatchTransients:
@@ -27,21 +22,29 @@ class QueryMatchTransients:
 		channels=None, with_flags=None, without_flags=None, time_created=None, time_modified=None
 	):
 		"""
-		'channels': list of strings (can be nested up to one level)
-		** See QueryMatchCriteria.add_from_list docstring for more info **
+		Merely a shortcut method made of several function calls
 
-		'with_flags': 
-		-> instance of ampel.base.flags.TransientFlags or list of instances of TransientFlags
-		Transient matching the with_flags criteria will be included.
-		See QueryMatchFlags.add_match_criteria docstring for more info
+		:type channels: str, dict
+		:param channels: string (one channel only) or a dict schema \
+		(see :obj:`QueryMatchSchema <ampel.pipeline.db.query.QueryMatchSchema>` \
+		for syntax details). None (no criterium) means all channels are considered. 
 
-		'without_flags': 
-		-> instance of ampel.base.flags.TransientFlags or list of instances of TransientFlags
-		Transient matching with the with_flags criteria will be excluded.
-		See QueryMatchFlags.add_nomatch_criteria docstring for more info
+		:type with_flags: str, int, dict
+		:param with_flags: string/int (one flag only) or a dict schema \
+		(see :obj:`QueryMatchSchema <ampel.pipeline.db.query.QueryMatchSchema>` \
+		for syntax details). Important: dict schema must contain **db flags** \
+		(integers representing enum members position within enum class), please see \
+		:func:`FlagUtils.to_dbflags_schema <ampel.core.flags.FlagUtils.to_dbflags_schema>` \
+		docstring for more info.
 
-		'time_created': instance of ampel.pipeline.t3.TimeConstraint
-		'time_modified': instance of ampel.pipeline.t3.TimeConstraint
+		:type without_flags: str, int, dict
+		:param without_flags: similar to parameter with_flags, except it's without.
+
+		:param TimeConstraint time_created: instance of ampel.pipeline.t3.TimeConstraint
+		:param TimeConstraint time_modified: instance of ampel.pipeline.t3.TimeConstraint
+
+		:rtype: dict
+		:returns: query dict with matching criteria
 		"""
 
 		query = {
@@ -49,25 +52,23 @@ class QueryMatchTransients:
 		}
 
 		if with_flags is not None:
-			QueryMatchFlags.add_match_criteria(
-				with_flags, query, 'alFlags'
+			QueryMatchSchema.apply_schema(
+				query, 'alFlags', with_flags
 			)
 
-		# Order matters, add_nomatch_criteria(...) must be called *after* add_match_criteria(...)
+		# Order matters, parse_dict(...) must be called *after* parse_excl_dict(...)
 		if without_flags is not None:
-			QueryMatchFlags.add_nomatch_criteria(
-				without_flags, query, 'alFlags'
+			QueryMatchSchema.apply_excl_schema(
+				query, 'alFlags', without_flags
 			)
 
 		if channels is not None:
-			QueryMatchCriteria.add_from_list(
-				query, 'channels',
-				(channels if not FlagUtils.contains_enum_flag(channels) 
-				else FlagUtils.enum_flags_to_lists(channels))
+			QueryMatchSchema.apply_schema(
+				query, 'channels', channels
 			)
 	
 		if time_created is not None:
-			if not type(time_created) is TimeConstraint:
+			if type(time_created) is not TimeConstraint:
 				raise ValueError("Parameter 'time_created' must be a TimeConstraint instance")
 			QueryMatchTransients._add_time_constraint(
 				query, '_id', time_created, oid_match=True
