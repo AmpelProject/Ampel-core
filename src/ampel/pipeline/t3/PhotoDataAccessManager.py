20f9cbf92b953830a73ae1f6cea4143c81d8d25a
vbrinnel <vsbrin@gmail.com>
2018-10-16 16:49:22 +0200

    PhotoDataAccessManager: temp v0.5 fix. To be improved later

--- src/ampel/pipeline/t3/PhotoDataAccessManager.py
+++ src/ampel/pipeline/t3/PhotoDataAccessManager.py
@@ -4,51 +4,69 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 02.06.2018
-# Last Modified Date: 22.08.2018
+# Last Modified Date: 16.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from ampel.pipeline.config.AmpelConfig import AmpelConfig
 from ampel.base.flags.PhotoFlags import PhotoFlags
-from ampel.pipeline.config.Channel import Channel
+from ampel.pipeline.config.channel.ChannelConfig import ChannelConfig
 
 class PhotoDataAccessManager:
 	"""
+	BETA CLASS.
+	TODO: make it instrument specific and move it to Ampel-ZTF, Ampel-ASASN, etc...
 	"""
 
 	def __init__(self, chan_name):
 		"""
 		"""
 
-		al_config = AmpelConfig.get_config()
-
-		if chan_name not in al_config['channels']:
-			raise ValueError("Unknown channel name '%s', please check your config" % chan_name)
-
-		channel = Channel(chan_name, al_config['channels'][chan_name])
 		self.photo_flags = None
+		chan = AmpelConfig.get_config('channels.%s' % chan_name)
+
+		if chan is None:
+			raise ValueError(
+				"Unknown channel name '%s', please check your config" % 
+				chan_name
+			)
+
+		channel = ChannelConfig.create(tier=0, **chan)
+
+		###############
+		# TODO: improve
+		###############
+
+		stream_config = channel.get_stream_config("ZTFIPAC")
+
+		# pylint: disable=no-member
+		self.photo_flags = PhotoFlags.INST_ZTF
+
+		if not channel.get("parameters.ZTFPartner"):
+			self.photo_flags |= PhotoFlags.ZTF_PUBLIC
+
 
 		# Loop through channel sources (ZTFIPAC, ASASN, etc..)
-		for src_name in channel.get_sources().keys():
-
-			# pylint: disable=unsubscriptable-object
-			current_photo_flag = PhotoFlags[
-				AmpelConfig.get_config('global.sources.%s.flags.photo' % src_name)			
-			]
-
-			if (
-				src_name == "ZTFIPAC" and 
-				not channel.get_config("parameters.ZTFPartner", source="ZTFIPAC")
-			):
-				current_photo_flag |= PhotoFlags.ZTF_PUBLIC
-
-			if current_photo_flag is not None:
-				if self.photo_flags is None:
-					self.photo_flags = current_photo_flag
-				else:
-					if type(self.photo_flags) is list:
-						self.photo_flags.append(current_photo_flag)
-					else:
-						self.photo_flags = [self.photo_flags, current_photo_flag]
+#		for src_name in channel.get_sources().keys():
+#
+#			# pylint: disable=unsubscriptable-object
+#			current_photo_flag = PhotoFlags[
+#				AmpelConfig.get_config('global.sources.%s.flags.photo' % src_name)			
+#			]
+#
+#			if (
+#				src_name == "ZTFIPAC" and 
+#				not channel.get_config("parameters.ZTFPartner", source="ZTFIPAC")
+#			):
+#				current_photo_flag |= PhotoFlags.ZTF_PUBLIC
+#
+#			if current_photo_flag is not None:
+#				if self.photo_flags is None:
+#					self.photo_flags = current_photo_flag
+#				else:
+#					if type(self.photo_flags) is list:
+#						self.photo_flags.append(current_photo_flag)
+#					else:
+#						self.photo_flags = [self.photo_flags, current_photo_flag]
 					
 
 	def check_flags(self, el, flags):
