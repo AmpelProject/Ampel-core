e778669a8f7f815c6738a6ebb0a638e37aa2de7d
vbrinnel <vsbrin@gmail.com>
2018-10-16 16:52:46 +0200

    TimeLastRunConfig: jobs are events

--- src/ampel/pipeline/config/time/TimeLastRunConfig.py
+++ src/ampel/pipeline/config/time/TimeLastRunConfig.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 # File              : ampel/pipeline/config/time/TimeLastRunConfig.py
 # License           : BSD-3-Clause
-#gendocstring Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 29.09.2018
-# Last Modified Date: 29.09.2018
+# Last Modified Date: 16.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from pydantic import BaseModel, constr
+from typing import Union, Dict
 from ampel.pipeline.common.docstringutils import gendocstring
 
 @gendocstring
 class TimeLastRunConfig(BaseModel):
-    use: constr(regex='.timeLastRun$')
-    jobName: str
+	use: constr(regex='.timeLastRun$')
+	event: str
+	fallback: Union[None, Dict] = None
