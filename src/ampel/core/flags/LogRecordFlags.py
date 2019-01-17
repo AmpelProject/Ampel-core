79eae079663e9e22fe056f147fc316692f0ba2fd
vbrinnel <vsbrin@gmail.com>
2019-01-18 00:55:26 +0100

    LogRecordFlags is now a standalone IntFlag

--- src/ampel/core/flags/LogRecordFlags.py
+++ src/ampel/core/flags/LogRecordFlags.py
@@ -4,38 +4,36 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 18.10.2018
+# Last Modified Date: 17.01.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.base.flags.AmpelMetaFlags import AmpelMetaFlags
+from enum import IntFlag
 
-class LogRecordFlags(metaclass=AmpelMetaFlags):
+class LogRecordFlags(IntFlag):
 	"""
 	| Flag used for each log entry stored in the DB.
 	| Value fits in a MongoDB int32.
-	| First values are imported from generic AmpelFlags \
-	and contain information such as INST_ZTF, SRC_IPAC
 	"""
 
 	# Log level
-	DEBUG                   = 65536 # 2**16
-	VERBOSE                 = 131072
-	INFO                    = 262144
-	WARNING                 = 524288
-	ERROR                   = 1048576
+	DEBUG                   = 1
+	VERBOSE                 = 2
+	INFO                    = 4
+	WARNING                 = 8
+	ERROR                   = 16
 
 	# Execution layer
-	T0                      = 2097152
-	T1                      = 4194304
-	T2                      = 8388608
-	T3                      = 16777216
+	T0                      = 32
+	T1                      = 64
+	T2                      = 128
+	T3                      = 256
 
 	# Section
-	CORE                    = 33554432 
-	UNIT                    = 67108864 
-	JOB                     = 134217728
-	TASK                    = 268435456
+	CORE                    = 512 
+	UNIT                    = 1024 
+	JOB                     = 2048
+	TASK                    = 4096
 
 	# Run type
-	SCHEDULED_RUN           = 536870912
-	MANUAL_RUN              = 1073741824 # 2**30
+	SCHEDULED_RUN           = 8192
+	MANUAL_RUN              = 16384
