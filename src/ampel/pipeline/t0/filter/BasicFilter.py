41565f30fdc53bc8aa8d3ab5b8750ed0280bb43a
Jakob van Santen <jvansanten@gmail.com>
2019-01-31 11:56:14 +0100

    BasicFilter: allow dict subclasses

--- src/ampel/pipeline/t0/filter/BasicFilter.py
+++ src/ampel/pipeline/t0/filter/BasicFilter.py
@@ -30,7 +30,7 @@ class BasicFilter(AbsAlertFilter):
 
 		self.on_match_default_t2_units = on_match_t2_units
 
-		if run_config is None or type(run_config) not in (dict, MappingProxyType):
+		if run_config is None or not isinstance(run_config, (dict, MappingProxyType)):
 			raise ValueError("run_config type must be a dict or MappingProxyType (got {})".format(type(run_config)))
 
 		self.param = {
