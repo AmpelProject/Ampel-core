AmpelProject/Ampel-core

commit fb51727c13820d73990b8a6f79eaaed100380545

T2Controller: add one-shot mode

src/ampel/pipeline/t2/T2Controller.py
@@ -453,7 +453,7 @@ def run():
 	parser = AmpelArgumentParser()
 	parser.add_argument('-v', '--verbose', default=False, action="store_true")
 	parser.add_argument('--units', default=None, nargs='+', help='T2 units to run')
-	parser.add_argument('--interval', default=10, type=int, help='Seconds to wait between database polls')
+	parser.add_argument('--interval', default=10, type=int, help='Seconds to wait between database polls. If < 0, exit after one poll')
 	parser.add_argument('--batch-size', default=200, type=int, help='Process this many T2 docs at a time')
 	
 	parser.require_resource('mongo', ['writer', 'logger'])
@@ -472,4 +472,5 @@ def run():
 	if not opts.verbose:
 		controller.logger.quieten_console()
 	controller.process_new_docs()
-	controller.run()
+	if parser.interval >= 0:
+		controller.run()

commit 31d1f0fdb9cc4495b9db84a9e2b24024cb89cb2c

T2Controller: correct typo introduced in fb51727c13820d73990b8a6f79eaaed100380545

src/ampel/pipeline/t2/T2Controller.py
@@ -472,5 +472,5 @@ def run():
 	if not opts.verbose:
 		controller.logger.quieten_console()
 	controller.process_new_docs()
-	if parser.interval >= 0:
+	if opts.interval >= 0:
 		controller.run()