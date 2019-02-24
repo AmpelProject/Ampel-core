68399201726213fdbe5b40204d4ba610ca8e3239
vbrinnel <vsbrin@gmail.com>
2019-02-24 08:42:53 +0100

    New custom exceptions: DBUpdateError/AmpelLoggingError

---
+++ src/ampel/pipeline/db/DBUpdateError.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/pipeline/db/DBUpdateError.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 03.12.2018
+# Last Modified Date: 03.12.2018
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+class DBUpdateError(Exception):
+	pass
