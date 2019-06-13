52c33471e92b77c4c866e7ce73e892cad28ad517
vbrinnel <valery.brinnel@gmail.com>
2019-06-13 19:38:58 +0200

    New class SVGPlot

---
+++ src/ampel/view/plot/SVGPlot.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/view/plot/SVGPlot.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 13.06.2019
+# Last Modified Date: 13.06.2019
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from ampel.view.plot.SVGUtils import SVGUtils
+
+class SVGPlot:
+	"""
+	"""
+
+	display_div = '<div style="display: inline-flex; justify-content: center;">'
+
+	def __init__(
+		self, content, scale=1.0, title=None, tags=None, tran_id=None, 
+		title_left_padding=0, center=True
+	):
+		""" 
+		"""
+
+		self._title = title
+		self._content = content
+		self._scale = scale
+		self._tags = tags
+		self._title_left_padding = title_left_padding
+		self._center = center
+		self._tran_id = tran_id
+
+
+	def rescale(self, scale=1.0):
+		""" """
+		if self._scale == scale:
+			return
+		self._content = SVGUtils.rescale(self._content, scale)
+		self._scale = scale
+
+
+	def has_tag(self, tag):
+		""" """
+		return tag in self._tags
+
+
+	def has_tags(self, tags):
+		""" """
+		return all(el in self._tags for el in tags)
+
+
+	def _repr_html_(
+		self, scale=None, show_title=True, title_prefix=None, 
+		show_tags=False, padding_bottom=0
+	):
+		"""
+		:param int scale: if None, native scaling is used
+		"""
+
+		html = "<center>" if self._center else ""
+
+		html += '<div style="padding-bottom: %ipx">' % padding_bottom
+
+		if show_title:
+			html += '<h3 style="padding-left:%ipx">%s %s</h3>' % (
+				self._title_left_padding, 
+				"" if title_prefix is None else title_prefix, 
+				self._title
+			)
+
+		if show_tags:
+			html += '<h3>' + str(self._tags) + '</h3>'
+
+		html += SVGPlot.display_div
+		
+		if scale is not None and isinstance(scale, (float, int)):
+			html += SVGUtils.rescale(self._content, scale)
+		else:
+			html += self._content 
+		
+		return html + '</div></div></center>' if self._center else '</div></div>'
+
+
+	def show_html(self, **kwargs):
+		"""
+		:param **kwargs: see _repr_html_ arguments for details
+		"""
+		from IPython.display import HTML
+		return HTML(
+			self._repr_html_(**kwargs)
+		)
