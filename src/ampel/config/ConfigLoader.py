dd48f39c105f0272bda2b7da12a6f72904943dcd
vbrinnel <valery.brinnel@gmail.com>
2019-08-24 22:27:59 +0200

    ConfigLoader: load new resources, remove pointless try/catch, updated BASE_CONFIG

--- src/ampel/config/ConfigLoader.py
+++ src/ampel/config/ConfigLoader.py
@@ -4,37 +4,40 @@
 # License           : BSD-3-Clause
 # Author            : Jakob van Santen <jakob.van.santen@desy.de>
 # Date              : Unspecified
-# Last Modified Date: 13.08.2019
+# Last Modified Date: 24.08.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-import json, pkg_resources, traceback, logging
+import json, pkg_resources
 from os.path import join, dirname, abspath, realpath, exists
 from ampel.config.channel.ChannelConfig import ChannelConfig
-from ampel.config.AmpelConfig import AmpelConfig
 from ampel.config.t3.T3JobConfig import T3JobConfig
+from ampel.logging.AmpelLogger import AmpelLogger
 
-AmpelConfig.load_tags()
 
 class ConfigLoader:
 
 	DEFAULT_CONFIG = join(
 		abspath(dirname(realpath(__file__))), 
-		'ztf_config.json'
+		'ampel_config.json'
 	)
-	BASE_CONFIG = AmpelConfig.recursive_freeze({
-	    "AmpelDB" : {
-	      "prefix": "Ampel"
-	    },
-	    "resources": {},
-	    "channels": {},
-	    "t0Filters": {},
-	    "t2RunConfig": {},
-	    "t2Units": {},
-	    "t3Jobs": {},
-	    "t3Tasks": {},
-	    "t3Units": {},
-	    "pwds": []
-	})
+
+	BASE_CONFIG = {
+		"AmpelDB" : {
+		  "prefix": "Ampel"
+		},
+		"resources": {},
+		"channels": {},
+		"t0Units": {},
+		"t0Filters": {},
+		"t1Units": {},
+		"t2Units": {},
+		"t2RunConfig": {},
+		"t3Units": {},
+		"t3Jobs": {},
+		"t3Tasks": {},
+		"pwds": []
+	}
+
 
 	@staticmethod
 	def load_config(path=DEFAULT_CONFIG, tier="all", gather_plugins=True):
@@ -49,167 +52,187 @@ class ConfigLoader:
 		T0/T2 units and run configurations are not loaded/checked.
 		"""
 
-		try:
-			config = AmpelConfig.recursive_unfreeze(ConfigLoader.BASE_CONFIG)
-			if exists(path):
-				with open(path) as f:
-					config.update(json.load(f))
-			else:
-				config.update(json.loads(path))
-			if not gather_plugins:
-				return config
-	
+		config = ConfigLoader.BASE_CONFIG.copy()
 
-			# Channels
-			##########
+		if exists(path):
+			with open(path) as f:
+				config.update(json.load(f))
+		else:
+			config.update(json.loads(path))
 
-			if tier in ("all", 0, 3):
+		if not gather_plugins:
+			return config
 
-				from ampel.db.DBUtils import DBUtils
-	
-				for resource in pkg_resources.iter_entry_points('ampel.channels'):
-	
-					# Channel can be provided as single dict or list of dicts
-					chan_resource = resource.resolve()()
-					if type(chan_resource) is dict:
-						chan_resource = [chan_resource]
-		
-					for chan_dict in chan_resource:
-	
-						# Check duplicated channel names
-						if chan_dict['channel'] in config['channels']:
-							raise KeyError(
-								"Channel {} (defined as entry point {} in {}) {}".format(
-									chan_dict['channel'], resource.name, resource.dist,
-									"already exists in the provided config file"
-								)
-							)
-		
-						# Schema validation
-						try:
-							ChannelConfig.create(tier, **chan_dict)
-							chan_dict["b2hash"] = DBUtils.b2_hash(chan_dict['channel'])
-							config['channels'][chan_dict['channel']] = chan_dict
-						except Exception as e:
-							log.error("Error in {} from {}".format(resource.name, resource.dist))
-							raise
-	
 
-			# T0 filters
-			############
-	
-			if tier in ("all", 0):
+		# Channels
+		##########
 
-				for resource in pkg_resources.iter_entry_points('ampel.pipeline.t0.units'):
-					klass = resource.resolve()
-					name = resource.name
-					if name in config['t0Filters']:
+		if tier in ("all", 0, 3):
+
+			from ampel.db.DBUtils import DBUtils
+
+			for resource in pkg_resources.iter_entry_points('ampel.channels'):
+
+				# Channel can be provided as single dict or list of dicts
+				chan_resource = resource.resolve()()
+
+				if type(chan_resource) is dict:
+					chan_resource = [chan_resource]
+	
+				for chan_dict in chan_resource:
+
+					# Check duplicated channel names
+					if chan_dict['channel'] in config['channels']:
 						raise KeyError(
-							"{} (defined as entry point {} in {}) {}".format(
+							"Channel {} (defined as entry point {} in {}) {}".format(
+								chan_dict['channel'], resource.name, resource.dist,
+								"already exists in the provided config file"
+							)
+						)
+	
+					# Schema validation
+					try:
+						ChannelConfig.create(tier, **chan_dict)
+						chan_dict["b2hash"] = DBUtils.b2_hash(chan_dict['channel'])
+						config['channels'][chan_dict['channel']] = chan_dict
+					except Exception as e:
+						AmpelLogger.get_logger().error(
+							"Error in {} from {}".format(
+								resource.name, resource.dist
+							)
+						)
+						raise
+
+
+		# T0 units & filters
+		####################
+
+		if tier in ("all", 0):
+
+			for resource in pkg_resources.iter_entry_points('ampel.t0.units'):
+
+				klass = resource.resolve()
+				name = resource.name
+
+				if name in config['t0Filters']:
+					raise KeyError(
+						"{} (defined as entry point {} in {}) {}".format(
+							name, resource.name, resource.dist,
+							"already exists in the provided config file"
+						)
+					)
+
+				config['t0Filters'][name] = dict(classFullPath=klass.__module__)
+
+
+		# T2 RunConfigs
+		###############
+
+		if tier in ("all", 2):
+
+			for resource in pkg_resources.iter_entry_points('ampel.t2.configs'):
+
+				for name, t2config in resource.resolve()().items():
+
+					if name in config['t2RunConfig']:
+						raise KeyError(
+							"T2 run config {} (defined as entry point {} in {}) {}".format(
 								name, resource.name, resource.dist,
 								"already exists in the provided config file"
 							)
 						)
-					config['t0Filters'][name] = dict(classFullPath=klass.__module__)
+
+					if name != '{t2Unit}_{runConfig}'.format(**t2config):
+						raise KeyError(
+							"T2 run config {} (defined as entry point {} in {}) {}".format(
+								name, resource.name, resource.dist,
+								"should be named {t2Unit}_{runConfig}".format(**t2config)
+							)
+						)
+
+					config['t2RunConfig'][name] = t2config
+
+
+		# T3 Jobs and Tasks
+		###################
+
+		if tier in ("all", 3):
+
+			# T3 Jobs 
+			#########
+
+			for resource in pkg_resources.iter_entry_points('ampel.t3.jobs'):
+
+				try:
+					# T3 job can be provided as single dict
+					job_resource = resource.resolve()()
+					if type(job_resource) is dict:
+						job_resource = [job_resource]
 	
+					for job_dict in job_resource:
 
-			# T2 RunConfigs
-			###############
-
-			if tier in ("all", 2):
-
-				for resource in pkg_resources.iter_entry_points('ampel.pipeline.t2.configs'):
-					for name, t2config in resource.resolve()().items():
-						if name in config['t2RunConfig']:
+						# Check duplicated job names
+						if job_dict['job'] in config['t3Jobs']:
 							raise KeyError(
-								"T2 run config {} (defined as entry point {} in {}) {}".format(
-									name, resource.name, resource.dist,
+								"T3 job {} (defined as entry point {} in {}) {}".format(
+									job_dict['job'], resource.name, resource.dist,
 									"already exists in the provided config file"
 								)
 							)
-						if name != '{t2Unit}_{runConfig}'.format(**t2config):
-							raise KeyError(
-								"T2 run config {} (defined as entry point {} in {}) {}".format(
-									name, resource.name, resource.dist,
-									"should be named {t2Unit}_{runConfig}".format(**t2config)
-								)
-							)
-						config['t2RunConfig'][name] = t2config
-	
-			# T3 Jobs and Tasks
-			###################
-
-			if tier in ("all", 3):
-
-				# T3 Jobs 
-				#########
-	
-				for resource in pkg_resources.iter_entry_points('ampel.pipeline.t3.jobs'):
-					try:
-						# T3 job can be provided as single dict
-						job_resource = resource.resolve()()
-						if type(job_resource) is dict:
-							job_resource = [job_resource]
-		
-						for job_dict in job_resource:
-	
-							# Check duplicated job names
-							if job_dict['job'] in config['t3Jobs']:
-								raise KeyError(
-									"T3 job {} (defined as entry point {} in {}) {}".format(
-										job_dict['job'], resource.name, resource.dist,
-										"already exists in the provided config file"
-									)
-								)
-							# Schema validation
-							T3JobConfig(**job_dict)
-							config['t3Jobs'][job_dict['job']] = job_dict
-					except Exception as e:
-						log.error("Error in {} from {}".format(resource.name, resource.dist))
-						raise
-
-				# T3 Tasks
-				##########
-
-				for resource in pkg_resources.iter_entry_points('ampel.channels'):
-	
-					# Channel can be provided as single dict or list of dicts
-					chan_resource = resource.resolve()()
-					if type(chan_resource) is dict:
-						chan_resource = [chan_resource]
-		
-					for chan_dict in chan_resource:
-	
 						# Schema validation
-						try:
+						T3JobConfig(**job_dict)
+						config['t3Jobs'][job_dict['job']] = job_dict
 
-							cc = ChannelConfig.create(3, **chan_dict)
+				except Exception as e:
+					AmpelLogger.get_logger().error(
+						"Error in {} from {}".format(
+							resource.name, resource.dist
+						)
+					)
+					raise
 
-							for source in cc.sources:
+			# T3 Tasks
+			##########
 
-								if not hasattr(source, 't3Supervise') or not cc.active:
-									continue
-							
-								for t3_task in getattr(source, 't3Supervise', []):
-					
-									# Check duplicated task names
-									if t3_task.get('task') in config['t3Tasks']:
-										raise KeyError(
-											"Task {} (defined as entry point {} in {}) {}".format(
-												t3_task.get('task'), resource.name, resource.dist,
-												"already exists in the provided config file"
-											)
+			for resource in pkg_resources.iter_entry_points('ampel.channels'):
+
+				# Channel can be provided as single dict or list of dicts
+				chan_resource = resource.resolve()()
+
+				if type(chan_resource) is dict:
+					chan_resource = [chan_resource]
+	
+				for chan_dict in chan_resource:
+
+					# Schema validation
+					try:
+
+						cc = ChannelConfig.create(3, **chan_dict)
+
+						for source in cc.sources:
+
+							if not hasattr(source, 't3Supervise') or not cc.active:
+								continue
+						
+							for t3_task in getattr(source, 't3Supervise', []):
+				
+								# Check duplicated task names
+								if t3_task.get('task') in config['t3Tasks']:
+									raise KeyError(
+										"Task {} (defined as entry point {} in {}) {}".format(
+											t3_task.get('task'), resource.name, resource.dist,
+											"already exists in the provided config file"
 										)
+									)
 
-									config['t3Tasks'][t3_task.get('task')] = t3_task.dict()
+								config['t3Tasks'][t3_task.get('task')] = t3_task.dict()
 
-						except Exception as e:
-							log.error("Error in {} from {}".format(resource.name, resource.dist))
-							raise
+					except Exception as e:
+						AmpelLogger.get_logger().error(
+							"Error in {} from {}".format(
+								resource.name, resource.dist
+							)
+						)
+						raise
 	
-
-	
-		except Exception as e:
-			raise
-
 		return config
