27738df41c01a03138f3aa850e19ad60c5cf827d
vbrinnel <valery.brinnel@gmail.com>
2019-10-05 17:59:50 +0200

    AmpelBaseModel: no longer raise custom exc, pydantic handles that well now

--- src/ampel/config/AmpelBaseModel.py
+++ src/ampel/config/AmpelBaseModel.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/config/AmpelModelExtension.py
+# File              : ampel/config/AmpelBaseModel.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 30.09.2018
@@ -8,7 +8,6 @@
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from ampel.common.AmpelUtils import AmpelUtils
-from ampel.config.ValidationError import ValidationError
 from pydantic import BaseModel, BaseConfig
 import logging
 
@@ -29,7 +28,7 @@ def to_camel_case(arg: str) -> str:
 	)
 
 # TODO: rename to AmpelBaseSchema
-class AmpelModelExtension(BaseModel):
+class AmpelBaseModel(BaseModel):
 
 	class Config(BaseConfig):
 		"""
@@ -58,33 +57,3 @@ class AmpelModelExtension(BaseModel):
 			value = getattr(model, key)
 			if isinstance(value, BaseModel):
 				cls.recursive_lock(value)
-
-
-	@classmethod
-	def print_and_raise(cls, msg, header=None):
-		"""
-		Prints a msg and raises a ValueError with the same msg.
-		Main use: sometimes, pydantic ValueError do not propagate properly
-		and secondary Exceptions occur. 
-		Printing the msg helps troubleshooting bad configurations.
-		"""
-
-		len_msg=0
-		output="\n\n"
-		for el in msg.split('\n'):
-			if len(el) > len_msg:
-				len_msg = len(el)
-
-		if header:
-			output += "#"*len(header)
-			output += "\n" + header
-			if len(msg.split('\n')) > 1:
-				output += "\n" + "-"*len(header)
-		else:
-			output += "#"*len_msg
-
-		output += "\n" + msg
-		output += "\n" + "#"*len_msg + "\n"
-
-		log.error(output)
-		raise ValidationError(output) from None
