f965b467271a5a8d4b9a2dccc5b50e8f11ea7cda
vbrinnel <valery.brinnel@gmail.com>
2019-10-06 15:32:55 +0200

    ChannelConfig: remove t3Supervise section

--- src/ampel/config/channel/ChannelConfig.py
+++ src/ampel/config/channel/ChannelConfig.py
@@ -4,21 +4,19 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 02.09.2018
-# Last Modified Date: 21.02.2019
+# Last Modified Date: 05.10.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import json, pkg_resources
 from pydantic import BaseModel, validator
-from typing import List, Sequence, Any, Union, Tuple
-from ampel.common.AmpelUtils import AmpelUtils
+from typing import List, Union, Tuple
 from ampel.common.docstringutils import gendocstring
-from ampel.config.AmpelModelExtension import AmpelModelExtension
+from ampel.config.AmpelBaseModel import AmpelBaseModel
 from ampel.config.channel.StreamConfig import StreamConfig
-from ampel.config.t3.T3TaskConfig import T3TaskConfig
 from ampel.config.ReadOnlyDict import ReadOnlyDict
 
 @gendocstring
-class ChannelConfig(AmpelModelExtension):
+class ChannelConfig(AmpelBaseModel):
 	"""
 	Config holder for AMPEL channels
 
@@ -26,107 +24,29 @@ class ChannelConfig(AmpelModelExtension):
 	https://github.com/samuelcolvin/pydantic/issues/185
 	We are thus forced to use Union[List, Tuple]
 	"""
-	channel: Union[int, str]
-	b2hash: int = None
+	name: Union[int, str]
+	hash: int = None
 	active: bool = True
 	author: str = "Unspecified"
 	sources: Union[List[StreamConfig], Tuple[StreamConfig]]
-	t3Supervise: Union[None, List[T3TaskConfig], Tuple[T3TaskConfig]] = None
-
-	def __init__(self, **values):
-
-		# We allow - for convenience - sources or t3Supervise to be defined as single dicts.
-		# A cast into sequence (tuple) is necessary in this case (validator cast_to_tuple). 
-		# Since a such cast modifies input, a shallow dict copy is necessary.
-		if isinstance(values['sources'], dict) or isinstance(values.get('t3Supervise'), dict):
-			super().__init__(**dict(values)) # shallow copy
-		else:
-			super().__init__(**values)
-
-
-	@classmethod
-	def create(cls, tier="all", **values):
-		"""
-		:param tier: at which tier level the returned ChannelConfig will be used. \
-		Possible values are: 'all', 0, 3. \
-		Less checks are performed when restricting tier to 0 or 3 which yields  \
-		a lighter and thus quicker ChannelConfig loading procedure. For example, \
-		with tier=0, T3 units existence or T3 run configurations are not checked.
-		"""
-		if tier == "all":
-			if hasattr(StreamConfig, '__tier__'):
-				delattr(StreamConfig, '__tier__')
-		else:
-			setattr(StreamConfig, '__tier__', tier)
-			setattr(cls, '__tier__', tier)
-		return cls(**values)
 
 
 	@validator('sources', pre=True, whole=True)
-	def validate_source(cls, sources, values, **kwargs):
+	def validate_source(cls, sources):
 		""" """
-
 		# cast to tuple if dict
 		if isinstance(sources, dict):
 			return (sources, )
 
-		# StreamConfig.__tier__ = 3/"all" is set by method create
-		if hasattr(cls, "__tier__") and cls.__tier__ not in (3, "all"):
-
-			sources = [dict(s) for s in sources]
-			for source in sources:
-				if source.get('t3Supervise'):
-					del source['t3Supervise']
-			return sources
-
-		else:
+		return sources
 
 
-			s = []
-
-			# add various required config entries to channel-embedded task config 
-			for source in sources:
-
-				if source.get('t3Supervise'):
-
-					# will raise exc if not found
-					source_setup = next(
-						pkg_resources.iter_entry_points(
-							'ampel.sources', source.get('stream')
-						), None
-					).resolve()()
-
-					# faster deep copy
-					src = json.loads(json.dumps(source))
-					for el in AmpelUtils.iter(src.get('t3Supervise')):
-						if not el.get('task'):
-							el['task'] = "%s_%s_%s" % (
-								values['channel'], el['unitId'], 
-								AmpelUtils.build_unsafe_dict_id(el)
-							)
-						trans = el.get("transients", {})
-						sel = trans.get("select", {})
-						sel["channels"] = values['channel']
-						sel["withTags"] = source_setup.survey_id
-					s.append(src)
-				else:
-					s.append(source)
-	
-			return tuple(s)
-
-
-	@validator('t3Supervise', pre=True, whole=True)
-	def enable_t0_partial_loading(cls, value):
+	def get_stream_config(self, stream_name: str) -> Union[None, StreamConfig]:
 		""" """
-		if hasattr(cls, "__tier__") and cls.__tier__ == 0:
-			return None
-		return value
-
-
-	def get_stream_config(self, source):
-		"""
-		:param str source:
-		:returns: instance of ampel.config.channel.StreamConfig
-		:rtype: StreamConfig
-		"""
-		return next(filter(lambda x: x.stream==source, self.sources), None)
+		return next(
+			filter(
+				lambda x: x.stream == stream_name, 
+				self.sources
+			), 
+			None
+		)
