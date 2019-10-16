ca5910d707fdbbdc55086ddb0a51c414ce627b9e
vbrinnel <valery.brinnel@gmail.com>
2019-10-16 18:35:10 +0200

    Logical operator models moved to ampel.model.operator

--- src/ampel/model/operator/AllOf.py
+++ src/ampel/model/operator/AllOf.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/config/t3/AllOf.py
+# File              : ampel/model/operator/AllOf.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 15.10.2018
-# Last Modified Date: 15.10.2018
+# Last Modified Date: 10.10.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel
+from ampel.model.AmpelBaseModel import AmpelBaseModel
 from typing import List, Union
 
-class AllOf(BaseModel):
+class AllOf(AmpelBaseModel):
 	allOf: Union[List[int], List[str]]
--- src/ampel/model/operator/AnyOf.py
+++ src/ampel/model/operator/AnyOf.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/config/t3/AnyOf.py
+# File              : ampel/model/operator/AnyOf.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 15.10.2018
-# Last Modified Date: 22.10.2018
+# Last Modified Date: 10.10.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel
 from typing import Union, List
-from ampel.config.t3.AllOf import AllOf
+from ampel.model.operator.AllOf import AllOf
+from ampel.model.AmpelBaseModel import AmpelBaseModel
 
-class AnyOf(BaseModel):
+class AnyOf(AmpelBaseModel):
 	anyOf: List[Union[int, str, AllOf]]
