e0dffadffe41783a8cc7db7b3011f67bdf002547
vbrinnel <valery.brinnel@gmail.com>
2019-10-16 18:35:10 +0200

    Logical operator models moved to ampel.model.operator

--- src/ampel/model/operator/OneOf.py
+++ src/ampel/model/operator/OneOf.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/config/t3/OneOf.py
+# File              : ampel/model/operator/OneOf.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 22.10.2018
-# Last Modified Date: 22.10.2018
+# Last Modified Date: 10.10.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel
 from typing import List, Union
+from ampel.model.AmpelBaseModel import AmpelBaseModel
 
-class OneOf(BaseModel):
+class OneOf(AmpelBaseModel):
 	oneOf: Union[List[int], List[str]]
