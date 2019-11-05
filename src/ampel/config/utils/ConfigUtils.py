9c5674e6e472c32a53e39853eaed66a465621994
vbrinnel <valery.brinnel@gmail.com>
2019-11-05 14:46:15 +0100

    ConfigUtils: new methods walk_and_process_dict, set_by_path and del_by_path

--- src/ampel/config/utils/ConfigUtils.py
+++ src/ampel/config/utils/ConfigUtils.py
@@ -4,13 +4,15 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 06.10.2018
-# Last Modified Date: 16.10.2019
+# Last Modified Date: 29.10.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
+from typing import Dict, List, Callable, Union
 from ampel.common.AmpelUtils import AmpelUtils
 
 class ConfigUtils:
-
+	"""
+	"""
 
 	@classmethod
 	def has_nested_type(cls, obj, target_type, strict=True):
@@ -80,17 +82,92 @@ class ConfigUtils:
 		In []: unflatten_dict({'a.0.b.f': 2, 'a.1.c': 2, 'd.e': 1})
 		Out[]: {'a': [{'b': {'f': 2}}, {'c': 2}], 'd': {'e': 1}}
 		"""
-		out = AmpelUtils.unflatten_dict(d)
 
+		out = AmpelUtils.unflatten_dict(d)
 		for k, v in out.items():
 			try:
-				# Check if dict keys are integers
-				_ = [int(el) for el in v]
-				out[k] = [
-					cls.unflatten_dict(out[k][el], separator) 
-					for el in v
-				]
-			except ValueError:
+				[int(el) for el in v]
+				out[k] = [cls.unflatten_dict(out[k][el], separator) for el in v]
+			except Exception:
 				pass
-
 		return out
+
+
+	@classmethod
+	def walk_and_process_dict(
+		cls, arg: Union[Dict, List], callback: Callable, match: List[str], path: str = None, **kwargs
+	) -> None:
+		"""
+		callback is called with 4 arguments:
+		1) the path of the possibly nested entry. Ex: "processor.initConfig.select" or "processor"
+		2) the matching key (from list 'match'). Ex: 'initConfig'
+		3) the matching (sub) dict
+		4) the **kwargs provided to this method
+
+		Simplest callback function:
+		def my_callback(path, k, d):
+			print(f"{path} -> {k}: {d}\n")
+		"""
+
+		if isinstance(arg, list):
+			for i, el in enumerate(arg):
+				cls.walk_and_process_dict(
+					el, callback, match, f"{path}.{i}" if path else f"{i}", **kwargs
+				)
+
+		if isinstance(arg, dict):
+
+			for k, v in arg.items():
+
+				if k in match:
+					callback(path, k, arg, **kwargs)
+
+				if isinstance(v, dict):
+					cls.walk_and_process_dict(
+						v, callback, match, f"{path}.{k}" if path else f"{k}", **kwargs
+					)
+
+				if isinstance(v, list):
+					for i, el in enumerate(v):
+						cls.walk_and_process_dict(
+							el, callback, match, f"{path}.{k}" if path else f"{k}", **kwargs
+						)
+
+
+	@staticmethod
+	def set_by_path(mapping, path, val, delimiter='.', create=True) -> bool:
+		"""
+		:param create: whether to create directory sub-structures if they do not exits 
+		(in this case, this method will alawys return False)
+		:returns: False if the key was successfully set, True otherwise
+		"""
+		d = mapping
+		keys = path.split(delimiter)
+		l = len(keys) - 1
+		for i, k in enumerate(keys):
+			if k not in d:
+				if not create:
+					return True
+				d[k] = {}
+			if i == l:
+				d[k] = val
+				return False
+			d = d[k]
+		return True
+
+	@staticmethod
+	def del_by_path(mapping, path, delimiter='.') -> bool:
+		"""
+		:returns: False if the key was successfully deleted, True otherwise
+		"""
+		d = mapping
+		keys = path.split(delimiter)
+		l = len(keys) - 1
+		for i, k in enumerate(keys):
+			if k not in d:
+				return True
+			if i == l:
+				del d[k]
+				return False
+			d = d[k]
+		return True
