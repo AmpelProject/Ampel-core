2839a7cd7c59197400091fda45ca419026e17c19
vbrinnel <valery.brinnel@gmail.com>
2019-11-05 15:12:14 +0100

    New template models

---
+++ src/ampel/model/template/LegacyChannelTemplate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/model/template/LegacyChannelTemplate.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 16.10.2019
+# Last Modified Date: 27.10.2019
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+
+from pydantic import validator
+from typing import Union, List #, Dict, Any
+from ampel.model.UnitData import UnitData
+from ampel.abstract.AbsChannelTemplate import AbsChannelTemplate
+
+
+class LegacyChannelTemplate(AbsChannelTemplate, abstract=True):
+	""" 
+	Abstract class whose purpose is to maintain compatibility with channel 
+	definitions created for ampel versions < 0.7.
+	This class must be subclassed.
+	Known subclass: ZTFLegacyChannelTemplate
+	"""
+	autoComplete: Union[bool, str]
+	t0Filter: UnitData
+	t2Compute: List[UnitData] = []
+	# bugging with pydantic 1.0, no idea why..
+	#t3Supervize: List[Dict[str, Any]] = []
+	t3Supervize: List = []
+
+
+	@validator('t3Supervize', 't2Compute', pre=True, whole=True)
+	def cast_to_list_if_required(cls, v):
+		if isinstance(v, dict):
+			return [v]
+		return v
---
+++ src/ampel/model/template/LegacyT3Process2.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/model/template/LegacyT3Process2.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 09.10.2019
+# Last Modified Date: 27.10.2019
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import List, Dict, Any
+from ampel.model.legacy.BaseT3Process import BaseT3Process
+from ampel.common.docstringutils import gendocstring
+from ampel.model.AmpelBaseModel import AmpelBaseModel
+
+
+@gendocstring
+class TaskData(AmpelBaseModel):
+	"""
+	Example:
+	"""
+	name: str
+	className: str
+	initConfig: Dict[str, Any]
+
+
+@gendocstring
+class LegacyT3Process2(BaseT3Process):
+	""" 
+	Handles the following formats:
+	{
+		"processName": "RapidLowzMarshal",
+		"tier": 3,
+		"schedule": "every(10).minutes",
+		"transients": {...},
+		"task": [
+			{
+				"className": "MarshalPublisher",
+				"initConfig": {
+					"marshal_program": "AmpelRapid"
+				}
+			},
+			{
+				"className": "MarshalPublisher",
+				"initConfig": {
+					"marshal_program": "AmpelRapid"
+				}
+			}
+		]
+	}
+	whereby tasks cannot defined their own transient/stock selection
+	"""
+	task: List[TaskData]
+
+	def get_process(self, channel: str) -> Dict[str, Any]:
+		"""
+		Override
+		"""
+
+		d = self.__dict__
+		tran = d['transients'].dict(skip_defaults=True)
+		tran['select']['channels'] = channel
+
+		return {
+			"tier": 3,
+			"processName": d['processName'],
+			"distName": d['distName'],
+			"schedule": d['schedule'],
+			"controller": {
+				"className": "T3Controller"
+			},
+			"processor": {
+				"className": "T3MultiUnitExecutor",
+				"initConfig": {
+					"transients": tran,
+					"task": d['task'].dict(skip_defaults=True)
+				}
+			}
+		}
