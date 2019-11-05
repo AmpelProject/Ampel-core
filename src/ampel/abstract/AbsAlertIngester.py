72473e2bb7161e0bad55ec3aa47074db46a03ce4
vbrinnel <valery.brinnel@gmail.com>
2019-11-05 14:41:44 +0100

    v0.7 core abstract classes

--- src/ampel/abstract/AbsAlertIngester.py
+++ src/ampel/abstract/AbsAlertIngester.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/core/abstract/AbsAlertIngester.py
+# File              : ampel/abstract/AbsAlertIngester.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 15.09.2018
+# Last Modified Date: 03.11.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.base.abstract.AmpelABC import AmpelABC, abstractmethod
+from typing import Sequence
+from ampel.model.t0.APChanData import APChanData
+from ampel.abstract.AmpelABC import AmpelABC, abstractmethod
 
 class AbsAlertIngester(metaclass=AmpelABC):
 	"""
@@ -16,9 +18,11 @@ class AbsAlertIngester(metaclass=AmpelABC):
 	@abstractmethod
 	def ingest(self, tran_id, pps_alert, uls_alert, list_of_t2_units):
 		""" """
-		pass
 
 	@abstractmethod
 	def set_log_id(self, log_id):
 		""" """
-		pass
+
+	@abstractmethod
+	def set_config(self, chan_data: Sequence[APChanData]):
+		""" """
---
+++ src/ampel/abstract/AbsChannelTemplate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/abstract/AbsChannelTemplate.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 27.10.2019
+# Last Modified Date: 27.10.2019
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from logging import Logger
+from typing import Dict, Any, Optional
+from ampel.model.ChannelData import ChannelData
+from ampel.abstract.AmpelABC import abstractmethod
+from ampel.abstract.AbsAmpelBaseModel import AbsAmpelBaseModel
+
+
+class AbsChannelTemplate(AbsAmpelBaseModel, ChannelData, abstract=True):
+	""" 
+	"""
+	template: Optional[str]
+
+	@abstractmethod	
+	def get_channel(self, logger: Logger) -> Dict[str, Any]:
+		""" """
+
+	@abstractmethod	
+	def get_processes(self, logger: Logger) -> Dict[str, Any]:
+		""" """
