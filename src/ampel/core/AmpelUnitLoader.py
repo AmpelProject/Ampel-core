992981ac10ddb8e701ff2cb30f9287111ed4a2f8
vbrinnel <valery.brinnel@gmail.com>
2019-12-06 23:15:18 +0100

    AmpelUnitLoader update

--- src/ampel/core/AmpelUnitLoader.py
+++ src/ampel/core/AmpelUnitLoader.py
@@ -4,47 +4,162 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.10.2019
-# Last Modified Date: 03.11.2019
+# Last Modified Date: 06.12.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Dict, Type, Any, Union
+import importlib
+from pydantic import BaseModel
+from typing import Dict, Type, Any, Union, Optional
+
+from ampel.model.UnitModel import UnitModel
 from ampel.common.AmpelUtils import AmpelUtils
-from ampel.core.AmpelUnit import AmpelUnit
-from ampel.core.AmpelLoader import AmpelLoader
 from ampel.abstract.AbsT2Unit import AbsT2Unit
-from ampel.model.UnitData import UnitData
-from ampel.model.AmpelBaseModel import AmpelBaseModel
+from ampel.config.AmpelConfig import AmpelConfig
+from ampel.logging.AmpelLogger import AmpelLogger
+from ampel.abstract.AbsAmpelUnit import AbsAmpelUnit
+from ampel.abstract.AbsAmpelProcessor import AbsAmpelProcessor
 
 
-class AmpelUnitLoader(AmpelLoader):
+class AmpelUnitLoader:
 	"""
-	Central class of the modular ampel design, 
-	whose purpose it to hold information related to 
-	user contributed unit (Filters, T2s, T3s) and configuration
 	"""
 
+	classes = {}
 
-	def get_ampel_unit(self, unit_data: UnitData) -> AmpelUnit:
+
+	def __init__(self, ampel_config: AmpelConfig, tier: Optional[int] = None):
+		""" 
+		For optimization purposes, please try to set the parameter tier.
+		For example, a T2 controller should spawn an AmpelUnitLoader 
+		using AmpelUnitLoader(ampel_config, 2).
+		"""
+		self.ampel_config = ampel_config
+		if tier is not None:
+			self.tiers = [f"t{tier}"] + [f"t{el}" for el in (0, 1, 2, 3) if el != tier]
+		else:
+			self.tiers = [f"t{el}" for el in (0, 1, 2, 3) if el != tier]
+
+
+	def get_unit_class(self, unit_model: UnitModel) -> Type:
 		""" """
+		return self.get_class(unit_model.class_name)
 
-		klass = self.get_class(unit_data.class_name)
 
-		return AmpelUnit(
-			unit_class = klass,
-			init_config = self.get_init_config(unit_data, klass),
-			resources = self.get_resources(unit_data, klass)
+	def get_unit_instance(
+		self, unit_model: UnitModel, logger: AmpelLogger
+	) -> AbsAmpelUnit:
+		""" """
+		klass = self.get_unit_class(unit_model)
+		return klass(
+			logger,
+			self.get_init_config(unit_model, klass),
+			self.get_resources(unit_model, klass),
 		)
 
 
+	def get_processor_class(self, unit_model: UnitModel) -> Type:
+		""" """
+		return self.get_class(unit_model.class_name, category="processor")
+
+
+	def get_processor_instance(
+		self, unit_model: UnitModel, 
+		options: Optional[Union[BaseModel, Dict[str, Any]]] = None
+	) -> AbsAmpelProcessor:
+		""" """
+		klass = self.get_unit_class(unit_model)
+		return klass(
+			self.ampel_config, 
+			self.get_init_config(unit_model, klass),
+			options
+		)
+		
+
+	def get_class(self, class_name: str, category="unit") -> Type:
+		""" """
+
+		# Load/Get unit *class* corresponding to provided unit id
+		if class_name in self.classes:
+			return self.classes[class_name]
+
+		for tier in self.tiers:
+			unit_def = self.ampel_config.get(
+				f"{tier}.{category}.{class_name}"
+			)
+			if unit_def:
+				break
+
+		if unit_def is None:
+			raise ValueError(
+				f"Ampel {category} not found: {class_name}"
+			)
+
+		# get class object
+		UnitClass = getattr(
+			# import using fully qualified name
+			importlib.import_module(unit_def['fqn']),
+			class_name
+		)
+
+		if not issubclass(UnitClass, (AbsAmpelProcessor, AbsAmpelUnit)):
+			raise ValueError("Unrecognized parent class")
+
+		# waiting for walrus operator
+		self.classes[class_name] = UnitClass
+		return UnitClass
+
+
+	def get_init_config(
+		self, unit_model: UnitModel, klass: Type
+	) -> Union[BaseModel, Dict[str, any]]:
+		""" """		
+
+		init_config = unit_model.init_config
+
+		if not init_config:
+			return None
+
+		# Load run config from alias
+		if isinstance(init_config, str):
+
+			alias = init_config
+			for tier in self.tiers:
+				init_config = self.ampel_config.get(
+					f"t{tier}.alias.{init_config}"
+				)
+				if init_config:
+					break
+
+			if not init_config:
+				raise ValueError(
+					f"Alias {alias} not found"
+				)
+
+		if unit_model.override:
+			init_config = AmpelUtils.unflatten_dict(
+				{
+					**AmpelUtils.flatten_dict(init_config), 
+					**AmpelUtils.flatten_dict(unit_model.override)
+				}
+			)
+
+		# Check for possibly defined InitConfig model
+		InnerInitConfigClass = getattr(klass, 'InitConfig', None)
+		if InnerInitConfigClass:
+			return InnerInitConfigClass(**init_config)
+
+		return init_config
+
+
 	def get_run_config(
-		self, unit_data: UnitData, klass: Type
-	) -> Union[AmpelBaseModel, Dict[str, any]]:
+		self, unit_model: UnitModel, klass: Type
+	) -> Union[BaseModel, Dict[str, any]]:
 		""" """
 
 		if not issubclass(klass, AbsT2Unit):
 			raise ValueError("Method only available for T2 units")
 
-		run_config = unit_data.run_config
+		run_config = unit_model.run_config
 
 		if not run_config:
 			return None
@@ -59,11 +174,11 @@ class AmpelUnitLoader(AmpelLoader):
 				"T2 run config {run_config} not found"
 			)
 
-		if unit_data.override:
+		if unit_model.override:
 			run_config = AmpelUtils.unflatten_dict(
 				{
 					**AmpelUtils.flatten_dict(run_config), 
-					**AmpelUtils.flatten_dict(unit_data.override)
+					**AmpelUtils.flatten_dict(unit_model.override)
 				}
 			)
 
@@ -75,7 +190,7 @@ class AmpelUnitLoader(AmpelLoader):
 		return run_config
 
 
-	def get_resources(self, unit_data: UnitData, klass: Type) -> Dict[str, Any]:
+	def get_resources(self, unit_model: UnitModel, klass: Type) -> Dict[str, Any]:
 		"""
 		Global resources are defined in the static class variable 'resources' of the corresponding unit
 		Global resource example: catsHTM.default
@@ -98,9 +213,9 @@ class AmpelUnitLoader(AmpelLoader):
 			resources[k] = resource
 
 		# Load possibly defined local resources 
-		if unit_data.resources:
+		if unit_model.resources:
 
-			for k in unit_data.resources:
+			for k in unit_model.resources:
 
 				local_resource = self.ampel_config.get('resource.' + k)
 				if not local_resource:
