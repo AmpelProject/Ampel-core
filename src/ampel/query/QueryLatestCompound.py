36c83322348d9c9ff26803c975f6ab79393f3aef
vbrinnel <valery.brinnel@gmail.com>
2019-12-10 12:08:36 +0100

    QueryLatestCompound: v0.7 compatibility update and added type hints

--- src/ampel/query/QueryLatestCompound.py
+++ src/ampel/query/QueryLatestCompound.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/db/QueryLatestCompound.py
+# File              : ampel/query/QueryLatestCompound.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 13.01.2018
-# Last Modified Date: 20.08.2019
+# Last Modified Date: 10.12.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-import collections, bson
+import collections
+from bson.int64 import Int64
+from typing import Union, List, Sequence, Dict, Optional, Any
+
 from ampel.common.AmpelUtils import AmpelUtils
+from ampel.model.operator.AnyOf import AnyOf
+from ampel.model.operator.AllOf import AllOf
+from ampel.model.operator.OneOf import OneOf
 from ampel.query.QueryMatchSchema import QueryMatchSchema
 
 class QueryLatestCompound:
@@ -16,22 +22,22 @@ class QueryLatestCompound:
 	"""
 
 	@staticmethod
-	def fast_query(tran_ids, channels=None):
+	def fast_query(
+		tran_ids: Union[int, Int64, str, Sequence[Union[int, Int64, str]]], 
+		channels: Optional[Union[int, str, Dict, AllOf, AnyOf, OneOf]] = None
+	) -> Dict:
 		"""
 		| **Must be used on transients whose compounds were solely created by T0** \
 		(i.e with no T3 compounds)
 		| Should perform faster than general_query.
 
 		:param tran_ids: transient id(s), query can/should be performed on multiple ids at once.
-		:type tran_id: str, list(str), set(str). 
 
-		:type channels: str, dict
-		:param channels: string (one channel only) or a dict schema \
-		(see :obj:`QueryMatchSchema <ampel.query.QueryMatchSchema>` \
-		for syntax details). None (no criterium) means all channels are considered. 
+		:param channels: a single channel or a dict schema \
+		(see :obj:`QueryMatchSchema <ampel.query.QueryMatchSchema>` for details. \
+		None (no criterium) means all channels are considered. 
 
 		:returns: a dict instance to be used with the mongoDB **aggregation** framework.
-		:rtype: dict
 
 		Example:\n
 		.. sourcecode:: python\n
@@ -46,25 +52,25 @@ class QueryLatestCompound:
 			In []: list(cursor)
 			Out[]: 
 			[
-				{'_id': Binary(b'T6TG\x96\x80\x1d\x86\x9f\x11\xf2G\xe7\xf4\xe0\xc3', 5), 'tranId': 'ZTF18aaayyuq'},
- 				{'_id': Binary(b'\xaaL|\x94?\xa4\xa1D\xbe\x0c[D\x9b\xc6\xe6o', 5), 'tranId': 'ZTF18aaabikt'},
- 				{'_id': Binary(b'\xaaL|\x14?\xb4\xc3D\xd2\x2a?L\x9a\xa6\xa1o', 5), 'tranId': 'ZTF17aaagvng'}
+				{'_id': Binary(b'T6TG\x96\x80\x1d\x86\x9f\x11\xf2G\xe7\xf4\xe0\xc3', 5), 'stockId': 'ZTF18aaayyuq'},
+ 				{'_id': Binary(b'\xaaL|\x94?\xa4\xa1D\xbe\x0c[D\x9b\xc6\xe6o', 5), 'stockId': 'ZTF18aaabikt'},
+ 				{'_id': Binary(b'\xaaL|\x14?\xb4\xc3D\xd2\x2a?L\x9a\xa6\xa1o', 5), 'stockId': 'ZTF17aaagvng'}
 			]
 		"""
 
 		# Robustness
-		type_tran_ids = type(tran_ids)
-		if type_tran_ids in (list, tuple, set):
-			if not AmpelUtils.check_seq_inner_type(tran_ids, (int, str, bson.int64.Int64)):
-				raise ValueError("Elements of tran_ids sequence must have type str or int")
-		elif not type_tran_ids in (int, str, bson.int64.Int64):
-			raise ValueError("tran_ids must have type str or int (or sequence of these types)")
+		if isinstance(tran_ids, Sequence):
+			if not AmpelUtils.check_seq_inner_type(tran_ids, (int, Int64, str)):
+				raise ValueError("Elements in tran_ids must be of type str or int or Int64 (bson)")
+		else:
+			if not isinstance(tran_ids, (int, Int64, str)):
+				raise ValueError("tran_ids must be of type str or int or Int64 (or sequence of these types)")
 
 		query = {
-			'tranId': tran_ids if type(tran_ids) in (str, int)
+			'stockId': tran_ids if isinstance(tran_ids, (int, Int64, str))
 			else {
-				'$in': tran_ids if type(tran_ids) is list 
-				else list(tran_ids)
+				'$in': tran_ids if isinstance(tran_ids, List)
+					else list(tran_ids)
 			}
 		}
 
@@ -79,19 +85,19 @@ class QueryLatestCompound:
 			},
 			{
 				'$project': {
-					'tranId': 1,
+					'stockId': 1,
 					'len': 1
 				}
 			},
 			{
 				'$sort': {
-					'tranId': 1, 
+					'stockId': 1, 
 					'len': -1
 				} 
 			},
 			{
 				'$group': {
-					'_id': '$tranId',
+					'_id': '$stockId',
 					'data': {
 						'$first': '$$ROOT'
 					}
@@ -111,22 +117,26 @@ class QueryLatestCompound:
 
 
 	@staticmethod
-	def general_query(tran_id, project=None, channels=None):
+	def general_query(
+		tran_id: Union[int, Int64, str], 
+		project: Optional[Dict[str, Any]] = None, 
+		channels: Optional[Union[int, str, Dict, AllOf, AnyOf, OneOf]] = None
+	) -> Dict:
 		"""
-		| Can be used on any ampel transients.
-		| There is a very detailed explanation of each step of the aggragetion \
-		documented in the python notebook "T3 get_lastest_compound"
+		| Should work with any ampel transients.
+		| A detailed explanation of each step of the aggregation \
+		is documented in the python notebook "T3 get_lastest_compound"
 
-		:param tran_id: transient id (string). Query **CANNOT** be performed on multiple ids at once.
+		:param tran_id: transient id (int, Int64 or string). \
+		Query **CANNOT** be performed on multiple ids at once.
+
 		:param dict project: optional projection stage at the end of the aggregation
 
-		:type channels: str, dict
-		:param channels: string (one channel only) or a dict schema \
-		(see :obj:`QueryMatchSchema <ampel.query.QueryMatchSchema>` \
-		for syntax details). None (no criterium) means all channels are considered. 
+		:param channels: single channel or a dict schema \
+		(see :obj:`QueryMatchSchema <ampel.query.QueryMatchSchema>` for details. \
+		None (no criterium) means all channels are considered. 
 
 		:returns: A dict instance intended to be used with the mongoDB **aggregation** framework.
-		:rtype: dict
 
 		IMPORTANT NOTE: the following two examples show the output of the aggregation framework \
 		from mongodb (i.e the output after having performed a DB query **using the output \
@@ -161,7 +171,7 @@ class QueryLatestCompound:
 				   {'pp': 378334946315010000},
 				   {'pp': 404270856315015007}],
 				  'tier': 0,
-				  'tranId': 'ZTF18aaayyuq'
+				  'stockId': 'ZTF18aaayyuq'
 				}
 			]
 
@@ -170,21 +180,22 @@ class QueryLatestCompound:
 			In []: list(
 				col.aggregate(
 					QueryLatestCompound.general_query(
-						'ZTF18aaayyuq', project={'$project': {'tranId':1}}
+						'ZTF18aaayyuq', project={'$project': {'stockId':1}}
 					)
 				)
 			)
 			Out[]: 
-				[{'_id': '5de2480f28bfca0bd3baae890cb2d2ae', 'tranId': 'ZTF18aaayyuq'}]
+				[{'_id': '5de2480f28bfca0bd3baae890cb2d2ae', 'stockId': 'ZTF18aaayyuq'}]
 		"""
 
 		# Robustness
-		if isinstance(tran_id, collections.Sequence) or not type(tran_id) in (str, int):
+		if isinstance(tran_id, collections.Sequence) or \
+		not isinstance(tran_id, (int, Int64, str)):
 			raise ValueError(
 				"Type of tran_id must be a string or an int (multi tran_id queries not supported)"
 			)
 
-		query = {'tranId': tran_id}
+		query = {'stockId': tran_id}
 
 		if channels is not None:
 			QueryMatchSchema.apply_schema(
