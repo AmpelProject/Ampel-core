31fa1b0836433a6f43bfe19be01c22b1e95f404f
vbrinnel <valery.brinnel@gmail.com>
2019-12-10 12:54:36 +0100

    ContentModel: fix pylint warnings

--- src/ampel/model/legacy/ContentModel.py
+++ src/ampel/model/legacy/ContentModel.py
@@ -4,11 +4,12 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 29.09.2018
-# Last Modified Date: 11.10.2019
+# Last Modified Date: 10.12.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel, validator
-from typing import Dict, Union, List, Any
+from typing import List
+from pydantic import validator
+
 from ampel.common.AmpelUtils import AmpelUtils
 from ampel.common.docstringutils import gendocstring
 from ampel.model.AmpelBaseModel import AmpelBaseModel
@@ -29,12 +30,12 @@ class ContentModel(AmpelBaseModel):
 		}
 	"""
 
-
 	docs: List[AlDocType]
 	t2SubSelection: List[str] = None
 
 
 	@validator('docs', whole=True, pre=True, always=True)
+	#pylint: disable=no-self-argument,no-self-use,unused-argument
 	def convert_to_enum(cls, v, values, **kwargs):
 		""" """
 
@@ -47,22 +48,22 @@ class ContentModel(AmpelBaseModel):
 		# Due to pydantic bug, validators can be called twice
 		if AmpelUtils.check_seq_inner_type(v, (int, AlDocType)):
 			return v
-		else:
-			# For convenience and syntax consistency, we accept dicts
-			if isinstance(v, dict):
-				return cls.logic_dict_to_list("docs", v)
 
-			if not isinstance(v, str) and not AmpelUtils.check_seq_inner_type(v, str):
-				raise ValueError(
-					"transients->content->docs model error\n" +
-					'List values must be string'
-				)
+		# For convenience and syntax consistency, we accept dicts
+		if isinstance(v, dict):
+			return cls.logic_dict_to_list("docs", v)
+
+		if not isinstance(v, str) and not AmpelUtils.check_seq_inner_type(v, str):
+			raise ValueError(
+				"transients->content->docs model error\n" +
+				'List values must be string'
+			)
 
 		ret = []
 
 		for el in AmpelUtils.iter(v):
 
-			if type(el) is str:
+			if isinstance(el, str):
 				try:
 					ret.append(AlDocType[el])
 				except KeyError:
@@ -77,27 +78,27 @@ class ContentModel(AmpelBaseModel):
 
 
 	@validator('t2SubSelection', pre=True, whole=True)
+	#pylint: disable=no-self-argument,no-self-use,unused-argument
 	def to_seq(cls, v, values, **kwargs):
 		""" """
 
 		if AmpelUtils.is_sequence(v):
 		
-			# Due to pydantic bug, validators can be called twice
+			# Due to pydantic bug (validators can be called twice)
 			if AmpelUtils.check_seq_inner_type(v, str):
 				return v
-			else:
-				# For convenience and syntax consistency, we accept dicts
-				if isinstance(v, dict):
-					return cls.logic_dict_to_list("t2SubSelection", v)
 
-				raise ValueError(
-					"transients->content->t2SubSelection model error\n" +
-					'List values must be string'
-				)
+			# For convenience and syntax consistency, we accept dicts
+			if isinstance(v, dict):
+				return cls.logic_dict_to_list("t2SubSelection", v)
 
-		if type(v) is str:
+			raise ValueError(
+				"transients->content->t2SubSelection model error\n" +
+				'List values must be string'
+			)
+
+		if isinstance(v, str):
 			return [v]
-
 
 		raise ValueError(
 			"transients->content->t2SubSelection unknown format\n" +
@@ -121,6 +122,7 @@ class ContentModel(AmpelBaseModel):
 
 
 	@validator('t2SubSelection')
+	#pylint: disable=no-self-argument,no-self-use,unused-argument
 	def validate_subselection(cls, t2SubSelection, values, **kwargs):
 		"""
 		Check transients->content->t2SubSelection
