62d9bc31c20a417d98c35972271bb1022c1f160d
vbrinnel <valery.brinnel@gmail.com>
2019-12-10 11:33:06 +0100

    QueryEventsCol: added type hints

--- src/ampel/query/QueryEventsCol.py
+++ src/ampel/query/QueryEventsCol.py
@@ -4,9 +4,10 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 11.07.2018
-# Last Modified Date: 05.10.2018
+# Last Modified Date: 10.12.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
+from typing import Dict, Union, Optional
 from time import time, strftime, gmtime
 
 class QueryEventsCol:
@@ -14,14 +15,13 @@ class QueryEventsCol:
 	"""
 
 	@staticmethod
-	def get_last_run(event_name, days_back=10):
+	def get_last_run(process_name: str, days_back: Optional[int] = 10) -> Dict:
 		"""
-		:param str event_name:
-		:param int days_back:
 		"""
 
 		ret = QueryEventsCol.get(
-			tier=3, event_name=event_name, days_back=days_back
+			tier=3, process_name=process_name, 
+			days_back=days_back
 		)
 
 		ret.append(
@@ -32,10 +32,9 @@ class QueryEventsCol:
 
 
 	@staticmethod
-	def get_t0_stats(timestamp):
+	def get_t0_stats(timestamp: Union[float, int]) -> Dict:
 		"""
 		:param timestamp: unix timestamp
-		:type timestamp: int, float
 		"""
 
 		ret = QueryEventsCol.get(
@@ -59,13 +58,15 @@ class QueryEventsCol:
 
 
 	@staticmethod
-	def get(tier=0, event_name=None, days_back=10, timestamp=None):
+	def get(
+		tier: int = 0, process_name: Optional[str] = None, 
+		days_back: Optional[int] = 10, 
+		timestamp: Optional[Union[int, float]] = None
+	) -> Dict:
 		"""
-		:param int tier: positive integer between 0 and 3
-		:param str event_name: Name of event (job id or task name)
-		:param int days_back: positive integer
+		:param tier: positive integer between 0 and 3
+		:param int days_back: positive integer or None
 		:param timestamp: unix time
-		:type timestamp: int, float
 		"""
 
 		# Array returned by this method
@@ -102,18 +103,18 @@ class QueryEventsCol:
 
 		second_match_stage = {'events.tier': tier}
 
-		if event_name is not None:
-			second_match_stage['events.event'] = event_name
+		if process_name:
+			second_match_stage['events.process'] = process_name
 
 		if timestamp is not None:
-			second_match_stage['events.dt'] = {'$gt': timestamp}
+			second_match_stage['events.ts'] = {'$gt': timestamp}
 
 		ret.extend(
 			[
 				{'$unwind': '$events'},
 				{'$match': second_match_stage},
 				# sort events by descending date-time
-				{'$sort': {'events.dt': -1}},
+				{'$sort': {'events.ts': -1}},
 			]
 		)
 
