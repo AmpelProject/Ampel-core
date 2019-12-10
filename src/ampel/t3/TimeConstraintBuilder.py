41e1bbcc1e9b4a614c82237b72a9abbdbacc7941
vbrinnel <valery.brinnel@gmail.com>
2019-12-10 08:34:05 +0100

    TimeConstraintBuilder: added type hints

--- src/ampel/t3/TimeConstraintBuilder.py
+++ src/ampel/t3/TimeConstraintBuilder.py
@@ -1,78 +1,85 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/t3/TimeConstraint.py
+# File              : ampel/t3/TimeConstraintBuilder.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 06.06.2018
 # Last Modified Date: 16.10.2018
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
+from typing import Union
 from datetime import datetime, timedelta
-from ampel.config.AmpelConfig import AmpelConfig
+
 from ampel.db.AmpelDB import AmpelDB
-from ampel.config.time.TimeDeltaConfig import TimeDeltaConfig
-from ampel.config.time.TimeLastRunConfig import TimeLastRunConfig
-from ampel.config.time.TimeStringConfig import TimeStringConfig
-from ampel.config.time.UnixTimeConfig import UnixTimeConfig
+from ampel.model.time.TimeDeltaModel import TimeDeltaModel
+from ampel.model.time.TimeLastRunModel import TimeLastRunModel
+from ampel.model.time.TimeStringModel import TimeStringModel
+from ampel.model.time.UnixTimeModel import UnixTimeModel
+from ampel.model.time.TimeConstraintModel import TimeConstraintModel
 
 
-class TimeConstraint:
+class TimeConstraintBuilder:
 	"""
 	"""
 
-	def __init__(self, tc_config=None):
+	def __init__(self, ampel_db: AmpelDB, tc_config: TimeConstraintModel = None):
 		"""
-		:param TimeConstraintConfig tc_config: TimeConstraintConfig instance (see docstring) \
-		if not provided, please make sure to call method :func:`set <ampel.t3.TimeConstraint.set>` 
+		:param tc_config: if not provided in constructor, 
+		please make sure to call method :func:`set <ampel.t3.TimeConstraintBuilder.set>` 
 		"""
 
 		self.constraints = {}
+		self._ampel_db = ampel_db
 
 		if tc_config is not None:
 			self.set("before", tc_config.before)
 			self.set("after", tc_config.after)
 
 
-	def has_constraint(self):
+	def has_constraint(self) -> bool:
 		""" """ 
 		return len(self.constraints) > 0
 
 
-	def get_after(self):
+	def get_after(self) -> datetime:
 		""" """ 
 		return self.get('after')
 	
 
-	def get_before(self):
+	def get_before(self) -> datetime:
 		""" """ 
 		return self.get('before')
 
 
-	def set(self, constraint_name, value):
+	def set(self, 
+		constraint_name: str, 
+		value: Union[
+			type(None), datetime, timedelta, TimeDeltaModel, 
+			TimeLastRunModel, UnixTimeModel, TimeStringModel
+		]
+	) -> None:
 		""" 
-		:param str constraint_name: "before" or "after"
-		:param value: "before" or "after"
-		:type value: datetime, timedelta, TimeDeltaConfig, \
-		TimeLastRunConfig, UnixTimeConfig, TimeStringConfig
+		:param constraint_name: "before" or "after"
+		:type value: datetime, timedelta, TimeDeltaModel, \
+		TimeLastRunModel, UnixTimeModel, TimeStringModel
 		""" 
 
 		if constraint_name not in ["before", "after"]:
 			raise ValueError("constraint_name must be 'before' or 'after'")
 
-		if type(value) not in [
-			type(None), datetime, timedelta, TimeDeltaConfig, TimeLastRunConfig, 
-			UnixTimeConfig, TimeStringConfig
-		]: 
-			raise ValueError("Unsupported type (%s)" % type(value))
+		if not isinstance(value, [
+			type(None), datetime, timedelta, TimeDeltaModel, 
+			TimeLastRunModel, UnixTimeModel, TimeStringModel
+		]): 
+			raise ValueError(f"Unsupported type ({type(value)})")
 
 		self.constraints[constraint_name] = value
 
 
-	def get(self, param):
+	def get(self, param: str) -> datetime:
 		""" 
 		Schema validation ensures value can be only either None, dict, datetime or timedelta
-
-		:param str param: either 'after' or 'before'
+		:param param: either 'after' or 'before'
 		"""
 
 		tc = self.constraints.get(param, None)
@@ -80,21 +87,19 @@ class TimeConstraint:
 		if tc is None:
 			return None
 
-		tc_type = type(tc)
-
-		if tc_type is datetime:
+		if isinstance(tc, datetime):
 			return tc
 
-		elif tc_type is timedelta:
+		if isinstance(tc, timedelta):
 			return datetime.today() + tc
 
-		elif tc_type is TimeDeltaConfig:
+		if isinstance(tc, TimeDeltaModel):
 			return datetime.today() + timedelta(**tc.arguments)
 
-		elif tc_type is TimeLastRunConfig:
+		if isinstance(tc, TimeLastRunModel):
 
-			from ampel.db.query.QueryEventsCol import QueryEventsCol
-			col = AmpelDB.get_collection('events')
+			from ampel.query.QueryEventsCol import QueryEventsCol
+			col = self._ampel_db.get_collection('events')
 			res = next(
 				col.aggregate(
 					QueryEventsCol.get_last_run(tc.event)
@@ -114,12 +119,14 @@ class TimeConstraint:
 				if res is None:
 					return None
 
-			return datetime.fromtimestamp(res['events']['dt'])
+			return datetime.fromtimestamp(
+				res['events']['dt']
+			)
 
-		elif tc_type is UnixTimeConfig:
+		if isinstance(tc, UnixTimeModel):
 			return datetime.fromtimestamp(tc.value)
 
-		elif tc_type is TimeStringConfig:
+		if isinstance(tc, TimeStringModel):
 			return datetime.strptime(tc.dateTimeStr, tc.dateTimeFormat)
 
-		raise ValueError("Illegal argument (type: %s)" % tc_type)
+		raise ValueError(f"Illegal argument (type: {type(tc)})")
