9310cff2d1b83599571e0dd14badfb7c28068726
vbrinnel <valery.brinnel@gmail.com>
2019-12-11 20:38:41 +0100

    QueryLoadT2Info: added type hints

--- src/ampel/query/QueryLoadT2Info.py
+++ src/ampel/query/QueryLoadT2Info.py
@@ -4,56 +4,57 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.02.2018
-# Last Modified Date: 06.12.2019
+# Last Modified Date: 11.12.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from bson.binary import Binary
+from typing import Iterable, Union, Dict, Any
+from ampel.typing import StrictIterable
+from ampel.model.operator.AnyOf import AnyOf
+from ampel.model.operator.AllOf import AllOf
+from ampel.model.operator.OneOf import OneOf
 from ampel.common.AmpelUtils import AmpelUtils
-from ampel.query.QueryMatchSchema import QueryMatchSchema
 from ampel.query.QueryUtils import QueryUtils
+from ampel.query.QueryMatchSchema import QueryMatchSchema
+
 
 class QueryLoadT2Info:
-	"""
-	"""
+	""" """
 
 	@classmethod
-	def build_stateless_query(cls, tran_ids, channels=None, t2_subsel=None):
+	def build_stateless_query(cls, 
+		stock_ids: Union[int, str, Iterable[Union[int, str]]], 
+		channels: Union[int, str, Dict, AllOf, AnyOf, OneOf],
+		t2_subsel: Union[int, str, Iterable[Union[int, str]]] = None
+	) -> Dict[str, Any]:
 		"""
-		| Builds a pymongo query aiming at loading transient t2 or compounds docs \
+		| Builds a pymongo query dict aiming at loading transient t2 or compounds docs \
 		| Stateless query: all avail compounds and t2docs (although possibly \
 		constrained by parameter t2_subsel) are targeted.
 
-		:type tran_id: int, List[int], set[int]
-		:param tran_id: transient id(s) (int, list of ints, set of ints). \
-		Query is generated to be performed on multiple ids at once.
+		:param stock_ids: (query can be performed on multiple ids at once)
 
-		:type channels: str, dict
-		:param channels: string (one channel only) or a dict \
-		(see :obj:`QueryMatchSchema <ampel.query.QueryMatchSchema>` \
-		for syntax details). None (no criterium) means all channels are considered. 
+		:param channels: see :obj:`QueryMatchSchema <ampel.query.QueryMatchSchema>` for details. \
+		None (no criterium) means all channels are considered. 
 
-		:type t2_subsel: str or List[str]
-		:param t2_subsel: optional sub-selection of t2 results based on t2 unit id(s). \
-		t2_subsel will *include* the provided results of t2s with the given ids \
-		and thus exclude other t2 results. \
-		If None or an empty list: all t2 docs associated with the matched transients will be loaded. \
-		A single t2 unit id (string) or a list of t2 unit ids can be provided.
-
-		:returns: pymongo query matching criteria in form of a dict
-		:rtype: dict
+		:param t2_subsel: optional sub-selection of t2 records based on t2 class names. \
+		-> only t2 records matching with the provided t2 class names will be returned. \
+		If None (or empty iterable): all t2 docs associated with the matched transients will be targeted. \
 		"""
 
-		query = cls.create_broad_query(tran_ids, channels)
+		query = cls.create_broad_query(stock_ids, channels)
 
 		if t2_subsel:
-			query['t2UnitId'] = t2_subsel if isinstance(t2_subsel, str) \
-			else QueryUtils.match_array(t2_subsel)
+			query['t2Id'] = t2_subsel if isinstance(t2_subsel, str) \
+				else QueryUtils.match_array(t2_subsel)
 
 		return query
 
 
 	@classmethod
-	def build_statebound_t1_query(cls, states):
+	def build_statebound_t1_query(cls,
+		states: Union[str, bytes, Binary, Iterable[str], Iterable[bytes], Iterable[Binary]],
+	) -> Dict[str, Any]:
 		"""
 		"""
 		return {
@@ -62,53 +63,54 @@ class QueryLoadT2Info:
 
 
 	@classmethod
-	def build_statebound_t2_query(cls, tran_ids, states, channels=None, t2_subsel=None):
+	def build_statebound_t2_query(cls, 
+		stock_ids: Union[int, str, Iterable[Union[int, str]]], 
+		states: Union[str, bytes, Binary, Iterable[str], Iterable[bytes], Iterable[Binary]],
+		channels: Union[int, str, Dict, AllOf, AnyOf, OneOf],
+		t2_subsel: Union[int, str, Iterable[Union[int, str]]] = None
+	) -> Dict[str, Any]:
 		"""
 		See :func:`build_stateless_query <build_stateless_query>` docstring
 		"""
 
-		query = cls.create_broad_query(tran_ids, channels)
-
-		t2_match = {
-			'docId': cls.get_compound_match(states)
-		}
+		query = cls.create_broad_query(stock_ids, channels)
+		query['docId'] = cls.get_compound_match(states)
 
 		if t2_subsel:
-			t2_match['t2UnitId'] = t2_subsel if isinstance(t2_subsel, str) \
+			query['t2Id'] = t2_subsel if isinstance(t2_subsel, str) \
 			else QueryUtils.match_array(t2_subsel)
 
 		return query
 
 
 	@staticmethod
-	def get_compound_match(states):
+	def get_compound_match(
+		states: Union[str, bytes, Binary, Iterable[str], Iterable[bytes], Iterable[Binary]]
+	) -> Dict[str, Any]:
 		"""
-		:returns: dict
+		:raises ValueError: if provided states parameter is invalid
 		"""
-
-		# Check if correct state(s) was provided
-		type_state = type(states)
 
 		# Single state was provided as string
-		if type_state is str:
+		if isinstance(states, str):
 			if len(states) != 32:
 				raise ValueError("Provided state string must have 32 characters")
 			match_comp_ids = Binary(bytes.fromhex(states), 5) # convert to bson Binary
 
 		# Single state was provided as bytes
-		elif type_state is bytes:
+		elif isinstance(states, bytes):
 			if len(states) != 16:
 				raise ValueError("Provided state bytes must have a length of 16")
 			match_comp_ids = Binary(states, 5) # convert to bson Binary
 
 		# Single state was provided as bson Binary
-		elif type_state is Binary:
+		elif isinstance(states, Binary):
 			if states.subtype != 5:
 				raise ValueError("Provided bson Binary state must have subtype 5")
 			match_comp_ids = states
 
 		# Multiple states were provided
-		elif type_state in (list, tuple, set):
+		elif isinstance(states, StrictIterable):
 
 			# check_seq_inner_type makes sure the sequence is monotype
 			if not AmpelUtils.check_seq_inner_type(states, (str, bytes, Binary)):
@@ -137,31 +139,28 @@ class QueryLoadT2Info:
 				if not all(st.subtype == 5 for st in states):
 					raise ValueError("Bson Binary states must have subtype 5")
 				match_comp_ids = {'$in': states if isinstance(states, list) else list(states)}
+
 		else:
 			raise ValueError(
-				"Type of provided state (%s) must be bytes, str, or sequences of these " % type(states)
+				f"Type of provided state ({type(states)}) must be "
+				f"bytes, str, bson.Binary or sequences of these"
 			)
 
 		return match_comp_ids
 
 
 	@staticmethod
-	def create_broad_query(tran_ids, channels):
+	def create_broad_query(
+		stock_ids: Union[int, str, Iterable[Union[int, str]]], 
+		channels: Union[int, str, Dict, AllOf, AnyOf, OneOf]
+	) -> Dict[str, Any]:
 		"""
-		Creates a broad matching query
-
-		:type tran_id: int, list(int), set(int)
-		:param tran_id: transient id(s) (int, list of ints, set of ints). \
-
 		:param channels: see :func:`build_stateless_query <build_stateless_query>` docstring
-
-		:returns: updated match dict
-		:rtype: dict
 		"""
 
 		query = {
-			'tranId': tran_ids if isinstance(tran_ids, int)
-			else QueryUtils.match_array(tran_ids)
+			'stockId': QueryUtils.match_array(stock_ids) \
+				if isinstance(stock_ids, StrictIterable) else stock_ids
 		}
 
 		if channels is not None:
