971169f485e63d6c4462d614eb3e0212ef8898d2
vbrinnel <valery.brinnel@gmail.com>
2019-12-25 21:34:49 +0100

    T2RunState: updated flag values

--- src/ampel/flags/T2RunState.py
+++ src/ampel/flags/T2RunState.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/flags/T2RunStates.py
+# File              : ampel/flags/T2RunState.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 12.05.2019
+# Last Modified Date: 18.12.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from enum import Flag
 
-class T2RunStates(Flag):
-	"""
-	"""
+class T2RunState(Flag):
 
-	TO_RUN                      = 1
-	TO_RUN_PRIO                 = 2
-	TO_RUN_DEPENDENCY           = 4
+	TO_RUN               = 0
+	TO_RUN_PRIO          = 1
+	TO_RUN_DEPENDENCY    = 2
 
-	COMPLETED                   = 8
-	QUEUED                      = 16
-	RUNNING                     = 32
-	EXPORTED                    = 64 
+	COMPLETED            = 4
+	QUEUED               = 8
+	RUNNING              = 16
+	EXPORTED             = 32
 
-	MISSING_INFO                = 128
-	BAD_CONFIG                  = 256
-	ERROR                       = 512
-	EXCEPTION                   = 1024
+	MISSING_INFO         = 64
+	BAD_CONFIG           = 128
+	ERROR                = 256
+	EXCEPTION            = 512
