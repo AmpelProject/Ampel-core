f7451f5be50d73ecf92a0b4eca950dc4e05ec1b1
vbrinnel <valery.brinnel@gmail.com>
2020-01-29 20:58:50 +0100

    GraphiteFeeder: added type hints

--- ampel/metrics/GraphiteFeeder.py
+++ ampel/metrics/GraphiteFeeder.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/common/GraphiteFeeder.py
+# File              : Ampel-core/ampel/metrics/GraphiteFeeder.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 26.04.2018
-# Last Modified Date: 12.07.2018
+# Last Modified Date: 29.01.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 import socket
+from typing import Dict, Any
 from urllib.parse import urlparse
 from graphitesend import GraphiteClient
-from ampel.common.AmpelUtils import AmpelUtils
+from ampel.utils.AmpelUtils import AmpelUtils
+
 
 class GraphiteFeeder:
 	"""
 	"""
 
-	def __init__(self, uri, autoreconnect=True):
+	def __init__(self, uri: str, autoreconnect: bool = True) -> None:
 		"""
 		"""
 
@@ -26,50 +28,54 @@ class GraphiteFeeder:
 		# Generate sytem name if not provided in URI
 		if parts.path == '/':
 			hostname = socket.gethostname().split('.')[0]
-			system_name = "ampel.{}".format(hostname)
+			system_name = f"ampel.{hostname}"
 		else:
 			system_name = parts.path[1:]
 
 		# Instanciate GraphiteClient
 		self.gclient = GraphiteClient(
-			graphite_server=getattr(parts, 'hostname'), 
-			graphite_port=getattr(parts, 'port'), 
+			graphite_server=getattr(parts, 'hostname'),
+			graphite_port=getattr(parts, 'port'),
 			system_name=system_name,
 			autoreconnect=autoreconnect
 		)
 
-		self.stats = {}
+		self.stats: Dict[str, Any] = {}
 
 
-	def add_stat(self, key, value, prefix=""):
+	def add_stat(self, key: str, value: Any, prefix: str = "") -> None:
 		"""
 		"""
-		if len(prefix) > 0 and prefix[-1] != ".": prefix += "."
+		if len(prefix) > 0 and prefix[-1] != ".":
+			prefix += "."
 		self.stats[prefix + key] = value
 
 
-	def add_stats(self, in_dict, prefix=""):
+	def add_stats(self, in_dict: Dict, prefix: str = "") -> None:
 		"""
 		"""
-		if len(prefix) > 0 and prefix[-1] != ".": prefix += "."
+		if len(prefix) > 0 and prefix[-1] != ".":
+			prefix += "."
 
 		fdict = AmpelUtils.flatten_dict(in_dict)
+
 		for key in fdict:
 			self.stats[prefix + key] = fdict[key]
 
 
-	def add_stats_with_mean_std(self, in_dict, prefix=""):
+	def add_stats_with_mean_std(self, in_dict, prefix="") -> None:
 		"""
 		"""
-		if len(prefix) > 0 and prefix[-1] != ".": prefix += "."
+		if len(prefix) > 0 and prefix[-1] != ".":
+			prefix += "."
 
 		fdict = AmpelUtils.flatten_dict(in_dict)
 		key_to_delete = []
 
 		for key in fdict:
 			val = fdict[key]
-			if type(val) in [tuple, list]:
-				if len(val) == 2: 
+			if isinstance(val, (tuple, list)):
+				if len(val) == 2:
 					self.stats[prefix + key + ".mean"] = val[0]
 					self.stats[prefix + key + ".std"] = val[1]
 				elif len(val) == 0: # empty list
@@ -79,9 +85,9 @@ class GraphiteFeeder:
 
 		for key in key_to_delete:
 			del fdict[key] # remove empty lists
-			
 
-	def send(self):
+
+	def send(self) -> None:
 		"""
 		"""
 		self.gclient.send_dict(self.stats)
