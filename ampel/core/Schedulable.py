691156bfdbffb8590a14f44b2ded6df8f554f717
vbrinnel <valery.brinnel@gmail.com>
2020-01-30 00:04:28 +0100

    Schedulable: add type hints

--- ampel/core/Schedulable.py
+++ ampel/core/Schedulable.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/common/Schedulable.py
+# File              : Ampel-core/ampel/common/Schedulable.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 26.05.2018
-# Last Modified Date: 18.12.2018
-# Last Modified By  : Jakob van Santen <jakob.van.santen@desy.de>
+# Last Modified Date: 29.01.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-import schedule, logging, time, threading, signal
+import schedule, logging, threading, signal
+
+from logging import Logger
+from schedule import Scheduler
+from typing import Callable, Optional
 from contextlib import contextmanager
+
 from ampel.logging.AmpelLogger import AmpelLogger
 
-class Schedulable():
-	""" 
-	"""
-	logger = None
 
-	def __init__(self, start_callback=None, stop_callback=None):
+class Schedulable():
+	""" """
+
+	def __init__(self,
+		start_callback: Optional[Callable] = None,
+		stop_callback: Optional[Callable] = None
+	) -> None:
 		"""
 		Optional arguments:
 		:param start_callback: method to be executed before starting the run thread.
@@ -32,7 +39,7 @@ class Schedulable():
 
 		# run() can be used rather that start() stop()
 		self._stop = threading.Event()
-	
+
 		# Callback functions for start and stop methods
 		self.start_callback = start_callback
 		self.stop_callback = stop_callback
@@ -41,7 +48,7 @@ class Schedulable():
 		self.scheduler = schedule.Scheduler()
 
 
-	def get_scheduler(self):
+	def get_scheduler(self) -> Scheduler:
 		"""
 		Returns instance of schedule.Scheduler associated with this class instance
 		"""
@@ -50,7 +57,7 @@ class Schedulable():
 
 
 	@contextmanager
-	def run_in_thread(self, logger=None):
+	def run_in_thread(self, logger: Optional[Logger] = None):
 		"""
 		Executes method 'run()' in its own thread.
 		If start_callback was provided as argument in constructor,
@@ -62,7 +69,7 @@ class Schedulable():
 			self.start_callback()
 
 		self.run_thread = threading.Thread(
-			target=self.run, args=(logger,)
+			target=self.run, args=(logger, )
 		)
 
 		self.run_thread.start()
@@ -71,6 +78,7 @@ class Schedulable():
 			yield self
 
 		finally:
+
 			self._stop.set()
 
 			if hasattr(self, 'run_thread'):
@@ -80,26 +88,23 @@ class Schedulable():
 				self.stop_callback()
 
 
-	def sig_exit(self, signum, frame):
+	# pylint: disable=unused-argument
+	def sig_exit(self, signum, frame) -> None:
 		"""
-		Calls method stop(). 
+		Calls method stop().
 		sig_exit() is executed when SIGTERM/SIGINT is caught.
 		"""
 		self._stop.set()
 
 
-	def run(self, logger=None):
+	def run(self, logger: Logger = None) -> None:
 		"""
 		Runs scheduler main loop.
 		See https://schedule.readthedocs.io
 		"""
 
-		# Setup logger
 		if logger is None:
-			if hasattr(self, 'logger') and isinstance(self.logger, logging.Logger):
-				logger = self.logger
-			else:
-				logger = AmpelLogger.get_unique_logger()
+			logger = AmpelLogger.get_unique_logger()
 
 		logger.info("Starting scheduler loop")
 
