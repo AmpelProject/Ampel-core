ef354ab07e7122e3d80cf3e08f47c78c7c1d6b89
vbrinnel <valery.brinnel@gmail.com>
2020-03-11 14:32:43 +0100

    T2RunState: new flags for the new chained T2s

--- ampel/flags/T2RunState.py
+++ ampel/flags/T2RunState.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : ampel/flags/T2RunState.py
+# File              : Ampel-core/ampel/flags/T2RunState.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 18.12.2019
+# Last Modified Date: 11.03.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from enum import Flag
 
+# flake8: noqa: E221
 class T2RunState(Flag):
 
 	TO_RUN               = 0
 	TO_RUN_PRIO          = 1
-	TO_RUN_DEPENDENCY    = 2
 
-	COMPLETED            = 4
-	QUEUED               = 8
-	RUNNING              = 16
-	EXPORTED             = 32
+	COMPLETED            = 2
+	QUEUED               = 4
+	RUNNING              = 8
+	EXPORTED             = 16
+	ERROR                = 32
+	EXCEPTION            = 64
 
-	MISSING_INFO         = 64
-	BAD_CONFIG           = 128
-	ERROR                = 256
-	EXCEPTION            = 512
+	TOO_MANY_TRIALS      = 128
+	# For now, I only see a 'purge' bug being capable of
+	# indirectly triggering a UNKNOWN_LINK error
+	UNKNOWN_LINK         = 256
+	UNKNOWN_CONFIG       = 512
+	MISSING_DEPENDENCY   = 1024
+
+
