ce4fef40a81d015113312482285411e28ec1485d
vbrinnel <valery.brinnel@gmail.com>
2020-06-05 18:46:51 +0200

    New Model: T2ComputeModel

---
+++ ampel/model/ingest/T2ComputeModel.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-core/ampel/model/ingest/T2ComputeModel.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 10.03.2020
+# Last Modified Date: 05.06.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from pydantic import Field
+from typing import Sequence
+from ampel.model.ingest.T2IngestModel import T2IngestModel
+from ampel.model.PlainUnitModel import PlainUnitModel
+
+
+class T2ComputeModel(PlainUnitModel):
+	# Override of 'unit' to enable alias
+	unit: str = Field(..., alias='ingester')
+	# config (t2 ingester config [from PlainUnitModel])
+	units: Sequence[T2IngestModel]
