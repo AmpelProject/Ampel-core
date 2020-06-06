2097cfc8da1ba107c8e0a4ded3e31f7ab598d04f
vbrinnel <valery.brinnel@gmail.com>
2020-06-06 13:12:36 +0200

    DemoPointT2Unit: include datapoint id in result

--- ampel/test/unit/DemoPointT2Unit.py
+++ ampel/test/unit/DemoPointT2Unit.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 25.03.2020
-# Last Modified Date: 25.03.2020
+# Last Modified Date: 02.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from time import time
@@ -16,4 +16,4 @@ from ampel.abstract.AbsPointT2Unit import AbsPointT2Unit
 class DemoPointT2Unit(AbsPointT2Unit):
 
 	def run(self, datapoint: DataPoint) -> T2UnitResult:
-		return {'result': {"time": time()}}
+		return {'result': {"id": datapoint['_id'], "time": time()}}
