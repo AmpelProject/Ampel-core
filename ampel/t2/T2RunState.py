91d9291f4ac802f2702438e17ac740ddfd55e3b1
vbrinnel <valery.brinnel@gmail.com>
2020-06-09 23:55:27 +0200

    T2RunState: inherit IntEnum, negative members

--- ampel/t2/T2RunState.py
+++ ampel/t2/T2RunState.py
@@ -4,29 +4,26 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.12.2017
-# Last Modified Date: 11.03.2020
+# Last Modified Date: 08.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from enum import Flag
+from enum import IntEnum
 
 # flake8: noqa: E221
-class T2RunState(Flag):
+class T2RunState(IntEnum):
 
-	TO_RUN               = 0
-	TO_RUN_PRIO          = 1
+	COMPLETED            = 0
+	TO_RUN               = -1
+	TO_RUN_PRIO          = -2
+	QUEUED               = -3
+	RUNNING              = -4
+	EXPORTED             = -5
+	ERROR                = -6
+	EXCEPTION            = -7
 
-	COMPLETED            = 2
-	QUEUED               = 4
-	RUNNING              = 8
-	EXPORTED             = 16
-	ERROR                = 32
-	EXCEPTION            = 64
+	TOO_MANY_TRIALS      = -8
 
-	TOO_MANY_TRIALS      = 128
-	# For now, I only see a 'purge' bug being capable of
-	# indirectly triggering a UNKNOWN_LINK error
-	UNKNOWN_LINK         = 256
-	UNKNOWN_CONFIG       = 512
-	MISSING_DEPENDENCY   = 1024
-
-
+	# UNKNOWN_* may be caused by a bug of the ingester or purge
+	UNKNOWN_LINK         = -9
+	UNKNOWN_CONFIG       = -10
+	MISSING_DEPENDENCY   = -11
