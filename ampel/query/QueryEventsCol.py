4510c676d0f4c3b26d2f5a905aae4bcde364f99e
vbrinnel <valery.brinnel@gmail.com>
2020-06-13 20:56:37 +0200

    QueryEventsCol: v0.7 structure update

--- ampel/query/QueryEventsCol.py
+++ ampel/query/QueryEventsCol.py
@@ -4,66 +4,64 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 11.07.2018
-# Last Modified Date: 06.01.2020
+# Last Modified Date: 10.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Dict, Union, Optional, List, Literal, Any
-from time import time, strftime, gmtime
+from bson import ObjectId
+from pymongo.collection import Collection
+from datetime import datetime, timedelta
+from typing import Dict, Optional, List, Literal, Any, Union
+
 
 class QueryEventsCol:
-	"""
-	"""
-
-	@staticmethod
-	def get_last_run(process_name: str, days_back: Optional[int] = 10) -> List[Dict]:
-		"""
-		"""
-
-		ret = QueryEventsCol.get(
-			tier=3, process_name=process_name,
-			days_back=days_back
-		)
-
-		ret.append(
-			{'$limit': 1} # take only the first entry
-		)
-
-		return ret
 
 
 	@staticmethod
-	def get_t0_stats(timestamp: Union[float, int]) -> List[Dict]:
+	def get_last_run(
+		col: Collection, process_name: str,
+		gte_time: Optional[Union[dict, float]] = None
+	) -> Optional[float]:
+
+		query = QueryEventsCol.get(tier=3, process_name=process_name, gte_time=gte_time)
+		if ret := list(col.find(query).sort('_id', -1).limit(2)):
+			if len(ret) > 1:
+				return ret[1]['_id'].generation_time.timestamp()
+		return None
+
+
+	@staticmethod
+	def get_t0_stats(
+		gte_time: Optional[Union[dict, int, float]] = None,
+		lte_time: Optional[Union[dict, int, float]] = None
+	) -> List[Dict]:
 		"""
-		:param timestamp: unix timestamp
+		:param gte_time: unix timestamp or timedelta argument
+		:param lte_time: unix timestamp or timedelta argument
 		"""
 
-		ret = QueryEventsCol.get(
-			tier=0,
-			timestamp=timestamp,
-			days_back=int((time() - timestamp) / 86400) + 1
-		)
+		ret = QueryEventsCol.get(tier=0, gte_time=gte_time, lte_time=lte_time)
+		ret['metrics.count.alerts'] = {"$exists": True}
 
-		ret.append(
+		return [
+			{'$match': ret},
 			{
 				"$group": {
 					"_id": 1,
 					"alerts": {
-						"$sum": "$events.metrics.count.alerts"
+						"$sum": "$metrics.count.alerts"
 					}
 				}
 			}
-		)
-
-		return ret
+		]
 
 
 	@staticmethod
 	def get(
 		tier: Literal[0, 1, 2, 3] = 0,
 		process_name: Optional[str] = None,
-		days_back: Optional[int] = 10,
-		timestamp: Optional[Union[int, float]] = None
-	) -> List[Dict]:
+		lte_time: Optional[Union[dict, int, float]] = None,
+		gte_time: Optional[Union[dict, int, float]] = None
+	) -> Dict[str, Any]:
 		"""
 		:param tier: positive integer between 0 and 3
 		:param days_back: positive integer or None
@@ -71,53 +69,35 @@ class QueryEventsCol:
 		:returns: list of dict to be used as aggregation pipeline query parameters
 		"""
 
-		# Array returned by this method
-		ret: List[Dict] = []
+		match: Dict[str, Any] = {}
 
-		# restrict match criteria
-		if days_back is not None:
-
-			# Matching db doc ids. Example: [20180711, 20180710]
-			match = []
-
-			# add today. Example: 20180711
-			match.append(
-				int(strftime('%Y%m%d'))
-			)
-
-			# add docs from previous days. Example: 20180710, 20180709
-			for i in range(1, days_back + 1):
-				match.append(
-					int(
-						strftime('%Y%m%d', gmtime(time() - 86400 * i))
-					)
+		if gte_time:
+			match['_id'] = {
+				"$gte": ObjectId.from_datetime(
+					QueryEventsCol.get_datetime(gte_time)
 				)
+			}
 
-			ret.append(
-				{
-					'$match': {
-						'_id': {
-							'$in': match
-						}
-					}
-				}
+		if lte_time:
+			if '_id' not in match:
+				match['_id'] = {}
+			match['_id']['$lte'] = ObjectId.from_datetime(
+				QueryEventsCol.get_datetime(lte_time)
 			)
 
-		second_match_stage: Dict[str, Any] = {'events.tier': tier}
+		if tier:
+			match['tier'] = tier
 
 		if process_name:
-			second_match_stage['events.process'] = process_name
+			match['process'] = process_name
 
-		if timestamp is not None:
-			second_match_stage['events.ts'] = {'$gt': timestamp}
+		return match
 
-		ret.extend(
-			[
-				{'$unwind': '$events'},
-				{'$match': second_match_stage},
-				# sort events by descending date-time
-				{'$sort': {'events.ts': -1}},
-			]
-		)
 
-		return ret
+	@staticmethod
+	def get_datetime(t: Union[int, float, dict]) -> datetime:
+		if isinstance(t, (int, float)):
+			return datetime.fromtimestamp(t)
+		elif isinstance(t, dict):
+			return datetime.today() + timedelta(**t)
+		return None
