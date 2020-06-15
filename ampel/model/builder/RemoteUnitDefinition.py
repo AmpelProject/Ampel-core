33b2177f4c3d1f30f414bfba3a719d3b6b8f58d7
vbrinnel <valery.brinnel@gmail.com>
2020-06-15 20:36:45 +0200

    Move UnitDefinitionModel -> RemoteUnitDefinition

--- ampel/model/builder/RemoteUnitDefinition.py
+++ ampel/model/builder/RemoteUnitDefinition.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-core/ampel/model/builder/UnitDefinitionModel.py
+# File              : Ampel-core/ampel/model/builder/RemoteUnitDefinition.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 06.11.2019
-# Last Modified Date: 09.04.2020
+# Last Modified Date: 15.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import List
 from ampel.model.AmpelStrictModel import AmpelStrictModel
 
-class UnitDefinitionModel(AmpelStrictModel):
+class RemoteUnitDefinition(AmpelStrictModel):
 
 	class_name: str
-	abc: List['str']
+	base: List['str']
