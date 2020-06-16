109a4fbad319d7b968ad72d941f9a0694446f1ed
vbrinnel <valery.brinnel@gmail.com>
2020-06-16 23:34:49 +0200

    New model: T3Directive

---
+++ ampel/model/t3/T3Directive.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-core/ampel/model/t3/T3Directive.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 04.06.2020
+# Last Modified Date: 04.06.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Optional, Sequence
+from ampel.model.UnitModel import UnitModel
+from ampel.model.StrictModel import StrictModel
+
+class T3Directive(StrictModel):
+
+	context: Optional[Sequence[UnitModel]]
+	select: UnitModel
+	load: UnitModel
+	complement: Optional[Sequence[UnitModel]]
+	run: UnitModel
