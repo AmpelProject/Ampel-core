dc7c4fac2a8d5e270c7c53c9e1a005fd88d74c97
vbrinnel <valery.brinnel@gmail.com>
2020-06-18 11:39:17 +0200

    Move AbsRunnable -> AbsProcessorUnit, update docstring

--- ampel/abstract/AbsProcessorUnit.py
+++ ampel/abstract/AbsProcessorUnit.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-core/ampel/abstract/AbsRunnable.py
+# File              : Ampel-core/ampel/abstract/AbsProcessorUnit.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 04.02.2020
-# Last Modified Date: 08.06.2020
+# Last Modified Date: 18.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 from ampel.base import abstractmethod
-from ampel.abstract.AbsAdminUnit import AbsAdminUnit
+from ampel.core.AdminUnit import AdminUnit
 from ampel.log.LogRecordFlag import LogRecordFlag
 
 
-class AbsRunnable(AbsAdminUnit, abstract=True):
+class AbsProcessorUnit(AdminUnit, abstract=True):
 	"""
-	Ampel admin unit featuring method run()
+	Ampel admin unit featuring methods run (abstract), new_run_id and dedicated attributes
 
-	:param log_flag: Any additional LogRecordFlag (default: SCHEDULED_RUN, possible alternative: MANUAL_RUN)
-	:param logger_profile: logger profile key as defined in the ampel conf (ex: 'standard', 'quiet', 'silent')
-	:param db_logging_handler_kwargs: optional kwargs to be passed to the DBLoggingHandler constructor.
+	:param base_log_flag: Any additional LogRecordFlag (default: SCHEDULED_RUN, possible alternative: MANUAL_RUN)
+	:param log_profile: logger profile key as defined in the ampel conf (ex: 'standard', 'quiet', 'silent')
+	:param db_handler_kwargs: optional override arguments to be passed to the DBLoggingHandler constructor
 	:param raise_exc: whether this class should raise Exceptions rather than catching them (default False)
 	"""
 
 	process_name: str
-	logger_profile: str = "default"
-	db_logging_handler_kwargs: Dict[str, Any] = {}
-
 	raise_exc: bool = False
-	log_flag: LogRecordFlag = LogRecordFlag.SCHEDULED_RUN
+	log_profile: str = "default"
+	base_log_flag: LogRecordFlag = LogRecordFlag.SCHEDULED_RUN
+	db_handler_kwargs: Optional[Dict[str, Any]] = None
 
 
 	@abstractmethod(var_args=True)
