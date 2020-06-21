ba36e34fd29c81f0c5f4dbe20db1196829975ba0
vbrinnel <valery.brinnel@gmail.com>
2020-06-21 16:04:51 +0200

    New abstract class: AbsT3DataAppender

---
+++ ampel/t3/complement/AbsT3DataAppender.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-core/ampel/t3/complement/AbsT3DataAppender.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 16.01.2020
+# Last Modified Date: 14.06.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Iterable
+from ampel.log import AmpelLogger
+from ampel.base import abstractmethod
+from ampel.core.AmpelBuffer import AmpelBuffer
+from ampel.core.AdminUnit import AdminUnit
+
+
+class AbsT3DataAppender(AdminUnit, abstract=True):
+	"""
+	Inherits AdminUnit because implementing classes might need access to
+	an AmpelConfig instance (foremost to the contained resource definitions)
+	"""
+
+	logger: AmpelLogger
+
+	@abstractmethod
+	def complement(self, it: Iterable[AmpelBuffer]) -> None:
+		...
