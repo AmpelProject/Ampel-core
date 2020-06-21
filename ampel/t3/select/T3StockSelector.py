a71ce93c163c0242bbf8b332dd588d74f016e859
vbrinnel <valery.brinnel@gmail.com>
2020-06-21 16:17:58 +0200

    T3StockSelector updates

--- ampel/t3/select/T3StockSelector.py
+++ ampel/t3/select/T3StockSelector.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-core/ampel/t3/select/T3DefaultStockSelector.py
+# File              : Ampel-core/ampel/t3/select/T3StockSelector.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 06.12.2019
-# Last Modified Date: 13.06.2020
+# Last Modified Date: 20.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import validator
 from pymongo.cursor import Cursor
-from typing import Union, Optional
+from typing import Union, Optional, Dict, Literal
 
 from ampel.type import ChannelId, Tag
-from ampel.query.QueryMatchStock import QueryMatchStock
-from ampel.log.AmpelLogger import AmpelLogger
+from ampel.db.query.stock import build_stock_query
+from ampel.log.AmpelLogger import AmpelLogger, VERBOSE
 from ampel.log.utils import safe_query_dict
-from ampel.t3.select.AbsStockSelector import AbsStockSelector
+from ampel.t3.select.AbsT3Selector import AbsT3Selector
 from ampel.model.operator.AllOf import AllOf
 from ampel.model.operator.AnyOf import AnyOf
 from ampel.model.operator.OneOf import OneOf
@@ -23,7 +22,7 @@ from ampel.model.time.TimeConstraintModel import TimeConstraintModel
 from ampel.config.LogicSchemaUtils import LogicSchemaUtils
 
 
-class T3DefaultStockSelector(AbsStockSelector):
+class T3StockSelector(AbsT3Selector):
 	"""
 	Default stock/transient selector used by T3Processor
 	Example:
@@ -32,28 +31,27 @@ class T3DefaultStockSelector(AbsStockSelector):
 		"created": {"after": {"use": "$timeDelta", "arguments": {"days": -40}}},
 		"modified": {"after": {"use": "$timeDelta", "arguments": {"days": -1}}},
 		"channel": "HU_GP_CLEAN",
-		"with_tags": "ZTF",
-		"without_tags": "HAS_ERROR"
+		"tags": {"with": "ZTF", "without": "HAS_ERROR"}
 	}
 	"""
 
 	logger: AmpelLogger
-	created: Optional[TimeConstraintModel]
-	modified: Optional[TimeConstraintModel]
-	channel: Optional[Union[ChannelId, AnyOf[ChannelId], AllOf[ChannelId], OneOf[ChannelId]]]
-	with_tags: Optional[Union[Tag, AnyOf[Tag], AllOf[Tag], OneOf[Tag]]]
-	without_tags: Optional[Union[Tag, AnyOf[Tag], AllOf[Tag], OneOf[Tag]]]
-
-
-	@validator('channel', 'with_tags', 'without_tags', pre=True, whole=True)
-	def cast(cls, v, values, **kwargs):
-		""" """
-		return LogicSchemaUtils.to_logical_struct(
-			v, kwargs['field'].name.split("_")[0]
-		)
+	created: Optional[TimeConstraintModel] = None
+	modified: Optional[TimeConstraintModel] = None
+	channel: Optional[Union[ChannelId, AnyOf[ChannelId], AllOf[ChannelId], OneOf[ChannelId]]] = None
+	tag: Optional[Dict[Literal['with', 'without'], Union[Tag, Dict, AllOf[Tag], AnyOf[Tag], OneOf[Tag]]]] = None
 
 
 	def __init__(self, **kwargs):
+
+		if 'channel' in kwargs:
+			kwargs['channel'] = LogicSchemaUtils.to_logical_struct(kwargs['channel'], 'channel')
+
+		if 'tag' in kwargs:
+			for k in ('with', 'without'):
+				if k in kwargs['tag']:
+					kwargs['tag'][k] = LogicSchemaUtils.to_logical_struct(kwargs['tag'][k], 'tag')
+
 		super().__init__(**kwargs)
 		if self.logger is None:
 			raise ValueError("Parameter logger cannot be None")
@@ -61,22 +59,20 @@ class T3DefaultStockSelector(AbsStockSelector):
 
 	# Override/Implement
 	def fetch(self) -> Cursor:
-		"""
-		The returned Iterator is a pymongo Cursor
-		"""
+		""" The returned Iterator is a pymongo Cursor """
 
 		# Build query for matching transients using criteria defined in config
-		match_query = QueryMatchStock.build_query(
-			channels = self.channel,
+		match_query = build_stock_query(
+			channel = self.channel,
+			tag = self.tag,
 			time_created = self.created.get_query_model(db=self.context.db) \
 				if self.created else None,
 			time_modified = self.modified.get_query_model(db=self.context.db) \
 				if self.modified else None,
-			with_tags = self.with_tags,
-			without_tags = self.without_tags
 		)
 
-		self.logger.info("Executing search query", extra=safe_query_dict(match_query))
+		if self.logger.verbose:
+			self.logger.log(VERBOSE, "Executing search query", extra=safe_query_dict(match_query))
 
 		# Execute 'find transients' query
 		cursor = self.context.db \
