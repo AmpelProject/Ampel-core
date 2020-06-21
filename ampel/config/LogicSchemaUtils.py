57a4b18e425ad6bd11987e6330a963eb22da2154
vbrinnel <valery.brinnel@gmail.com>
2020-06-21 14:59:01 +0200

    LogicSchemaUtils: go for snake_case

--- ampel/config/LogicSchemaUtils.py
+++ ampel/config/LogicSchemaUtils.py
@@ -34,15 +34,15 @@ class LogicSchemaUtils:
 				"transients->select->%s config error\n" % field_name +
 				"'%s' parameter cannot be a list. " % field_name +
 				"Please use the following syntax:\n" +
-				" -> {'anyOf': ['Ab', 'Cd']} or\n" +
-				" -> {'allOf': ['Ab', 'Cd']} or\n" +
-				" -> {'oneOf': ['Ab', 'Cd']} or\n" +
+				" -> {'any_of': ['Ab', 'Cd']} or\n" +
+				" -> {'all_of': ['Ab', 'Cd']} or\n" +
+				" -> {'one_of': ['Ab', 'Cd']} or\n" +
 				"One level nesting is allowed, please see\n" +
 				"ConfigUtils.conditional_expr_converter(..) docstring for more info"
 			)
 
 		if isinstance(v, (str, int)):
-			return {'anyOf': [v]}
+			return {'any_of': [v]}
 
 		if isinstance(v, dict):
 
@@ -52,99 +52,99 @@ class LogicSchemaUtils:
 					"Unsupported dict format %s" % v
 				)
 
-			if 'anyOf' in v:
+			if 'any_of' in v:
 
-				if not isinstance(v['anyOf'], strict_iterable):
+				if not isinstance(v['any_of'], strict_iterable):
 					raise ValueError(
-						"transients->select->%s:anyOf config error\n" % field_name +
-						"Invalid dict value type: %s. Must be a sequence" % type(v['anyOf'])
+						"transients->select->%s:any_of config error\n" % field_name +
+						"Invalid dict value type: %s. Must be a sequence" % type(v['any_of'])
 					)
 
-				# 'anyOf' supports only a list of dicts and str/int
-				if not check_seq_inner_type(v['anyOf'], (str, int, dict), multi_type=True):
+				# 'any_of' supports only a list of dicts and str/int
+				if not check_seq_inner_type(v['any_of'], (str, int, dict), multi_type=True):
 					raise ValueError(
-						"transients->select->%s:anyOf config error\n" % field_name +
+						"transients->select->%s:any_of config error\n" % field_name +
 						"Unsupported nesting (err 2)"
 					)
 
-				if not check_seq_inner_type(v['anyOf'], (int, str)) and len(v['anyOf']) < 2:
+				if not check_seq_inner_type(v['any_of'], (int, str)) and len(v['any_of']) < 2:
 					raise ValueError(
-						"transients->select->%s:anyOf config error\n" % field_name +
-						"anyOf list must contain more than one element when containing allOf\n" +
+						"transients->select->%s:any_of config error\n" % field_name +
+						"any_of list must contain more than one element when containing all_of\n" +
 						"Offending value: %s" % v
 					)
 
-				for el in v['anyOf']:
+				for el in v['any_of']:
 
 					if isinstance(el, dict):
 
-						if 'anyOf' in el:
+						if 'any_of' in el:
 							raise ValueError(
-								"transients->select->%s:anyOf.anyOf config error\n" % field_name +
-								"Unsupported nesting (anyOf in anyOf)"
+								"transients->select->%s:any_of.any_of config error\n" % field_name +
+								"Unsupported nesting (any_of in any_of)"
 							)
 
-						if 'allOf' in el:
+						if 'all_of' in el:
 
-							# 'allOf' closes nesting
-							if not check_seq_inner_type(el['allOf'], (int, str)):
+							# 'all_of' closes nesting
+							if not check_seq_inner_type(el['all_of'], (int, str)):
 								raise ValueError(
-									"transients->select->%s:anyOf.allOf config error\n" % field_name +
-									"Unsupported nesting (allOf list content must be str/int)"
+									"transients->select->%s:any_of.all_of config error\n" % field_name +
+									"Unsupported nesting (all_of list content must be str/int)"
 								)
 
-							if len(set(el['allOf'])) < 2:
+							if len(set(el['all_of'])) < 2:
 								raise ValueError(
-									"transients->select->%s:allOf config error\n" % field_name +
-									"Please do not use allOf with just one element\n" +
+									"transients->select->%s:all_of config error\n" % field_name +
+									"Please do not use all_of with just one element\n" +
 									"Offending value: %s" % el
 								)
 
 						else:
 							raise ValueError(
-								"transients->select->%s:anyOf config error\n" % field_name +
+								"transients->select->%s:any_of config error\n" % field_name +
 								"Unsupported nested dict: %s" % el
 							)
 
-			elif 'allOf' in v:
+			elif 'all_of' in v:
 
-				# 'allOf' closes nesting
+				# 'all_of' closes nesting
 				if (
-					not isinstance(v['allOf'], strict_iterable) or
-					not check_seq_inner_type(v['allOf'], (int, str))
+					not isinstance(v['all_of'], strict_iterable) or
+					not check_seq_inner_type(v['all_of'], (int, str))
 				):
 					raise ValueError(
-						"transients->select->%s:allOf config error\n" % field_name +
+						"transients->select->%s:all_of config error\n" % field_name +
 						"Invalid type for value %s\n(must be a sequence, is: %s)\n" %
-						(v['allOf'], type(v['allOf'])) +
-						"Note: no nesting is allowed below 'allOf'"
+						(v['all_of'], type(v['all_of'])) +
+						"Note: no nesting is allowed below 'all_of'"
 					)
 
-				if len(set(v['allOf'])) < 2:
+				if len(set(v['all_of'])) < 2:
 					raise ValueError(
-						"transients->select->%s:allOf config error\n" % field_name +
-						"Please do not use allOf with just one element\n" +
+						"transients->select->%s:all_of config error\n" % field_name +
+						"Please do not use all_of with just one element\n" +
 						"Offending value: %s" % v
 					)
 
-			elif 'oneOf' in v:
+			elif 'one_of' in v:
 
-				# 'oneOf' closes nesting
+				# 'one_of' closes nesting
 				if (
-					not isinstance(v['oneOf'], strict_iterable) or
-					not check_seq_inner_type(v['oneOf'], (int, str))
+					not isinstance(v['one_of'], strict_iterable) or
+					not check_seq_inner_type(v['one_of'], (int, str))
 				):
 					raise ValueError(
-						"transients->select->%s:oneOf config error\n" % field_name +
+						"transients->select->%s:one_of config error\n" % field_name +
 						"Invalid type for value %s\n(must be a sequence, is: %s)\n" %
-						(v['oneOf'], type(v['oneOf'])) +
-						"Note: no nesting is allowed below 'oneOf'"
+						(v['one_of'], type(v['one_of'])) +
+						"Note: no nesting is allowed below 'one_of'"
 					)
 
 			else:
 				raise ValueError(
 					"transients->select->%s config error\n" % field_name +
-					"Invalid dict key (only 'anyOf', 'allOf', 'oneOf' are allowed)"
+					"Invalid dict key (only 'any_of', 'all_of', 'one_of' are allowed)"
 				)
 
 		return v
@@ -160,13 +160,13 @@ class LogicSchemaUtils:
 
 			Schema: 'a'
 			Reduced set: {'a'}
-			Schema: {'anyOf': ['a', 'b', 'c']}
+			Schema: {'any_of': ['a', 'b', 'c']}
 			Reduced set: {'b', 'a', 'c'}
-			Schema: {'allOf': ['a', 'b', 'c']}
+			Schema: {'all_of': ['a', 'b', 'c']}
 			Reduced set: {'b', 'a', 'c'}
-			Schema: {'anyOf': [{'allOf': ['a', 'b']}, 'c']}
+			Schema: {'any_of': [{'all_of': ['a', 'b']}, 'c']}
 			Reduced set: {'b', 'a', 'c'}
-			Schema: {'anyOf': [{'allOf': ['a', 'b']}, {'allOf': ['a', 'c']}, 'd']}
+			Schema: {'any_of': [{'all_of': ['a', 'b']}, {'all_of': ['a', 'c']}, 'd']}
 			Reduced set: {'d', 'b', 'a', 'c'}
 		"""
 
@@ -178,9 +178,9 @@ class LogicSchemaUtils:
 
 		if isinstance(arg, dict):
 
-			if "anyOf" in arg:
+			if "any_of" in arg:
 				s = set()
-				for el in arg['anyOf']:
+				for el in arg['any_of']:
 					if isinstance(el, in_type):
 						s.add(el)
 					elif isinstance(el, dict):
@@ -190,11 +190,11 @@ class LogicSchemaUtils:
 						raise ValueError("LogicSchemaUtils.reduce_to_set: unsupported format (1)")
 				return s
 
-			if 'allOf' in arg:
-				return set(arg['allOf'])
+			if 'all_of' in arg:
+				return set(arg['all_of'])
 
-			if 'oneOf' in arg:
-				return set(arg['oneOf'])
+			if 'one_of' in arg:
+				return set(arg['one_of'])
 
 			raise ValueError("LogicSchemaUtils.reduce_to_set: unsupported format (2)")
 
@@ -206,29 +206,29 @@ class LogicSchemaUtils:
 		"""
 		Converts JSON encoded conditional statements from Ampel config file
 		into arrays with dimension up to two.
-		'anyOf' -> or operator -> encoded in a array elements of depth=1
-		'allOf' -> and operator -> encoded in array elements of depth=2
+		'any_of' -> or operator -> encoded in a array elements of depth=1
+		'all_of' -> and operator -> encoded in array elements of depth=2
 
 		Accepted input:
 		---------------
 
 		atomar values str, int float: "a" / 1 / 1.2
 
-		1d sequences of atomar values (automaticallOfy treated as 'anyOf' sequence):
+		1d sequences of atomar values (automaticall_ofy treated as 'any_of' sequence):
 		[1, 2, 3]  / [1, "a", 3.4]
 
-		'anyOf' dict containing 1d list of atomar values (explicit 'anyOf' sequence):
-		{'anyOf': [1, 2, 3]} / {'anyOf': [1, "a", 3.4]}
+		'any_of' dict containing 1d list of atomar values (explicit 'any_of' sequence):
+		{'any_of': [1, 2, 3]} / {'any_of': [1, "a", 3.4]}
 
-		'allOf' dict containing 1d list of atomar values
-		{'allOf': [1, 2, 3]} / {'anyOf': [1, "a", 3.4]}
+		'all_of' dict containing 1d list of atomar values
+		{'all_of': [1, 2, 3]} / {'any_of': [1, "a", 3.4]}
 
-		Nested structure whereby 'allOf' closes the nesting (can contain only a sequence of atomar values)
+		Nested structure whereby 'all_of' closes the nesting (can contain only a sequence of atomar values)
 		{
-			'anyOf': [
-				{'allOf': ["HUSN1", "HUSN2"]},
+			'any_of': [
+				{'all_of': ["HUSN1", "HUSN2"]},
 				"HUBH1",
-				{'allOf': ["HUSN1", "HUSN3"]}
+				{'all_of': ["HUSN1", "HUSN3"]}
 			]
 		}
 
@@ -242,37 +242,37 @@ class LogicSchemaUtils:
 		In []: conditional_expr_converter(["1","2","3"])
 		Out[]: ["1", "2", "3"]
 
-		In []: conditional_expr_converter({'allOf': ["3", "1", "2"]})
+		In []: conditional_expr_converter({'all_of': ["3", "1", "2"]})
 		Out[]: [["3", "1", "2"]]
 
-		In []: conditional_expr_converter({'anyOf': ["3", "1", "2"]})
+		In []: conditional_expr_converter({'any_of': ["3", "1", "2"]})
 		Out[]: ["3", "1", "2"]
 
-		In []: conditional_expr_converter({'anyOf': [{'allOf': ["1","2"]}, "3", "1", "2"]})
+		In []: conditional_expr_converter({'any_of': [{'all_of': ["1","2"]}, "3", "1", "2"]})
 		Out[]: [["1", "2"], "3", "1", "2"]
 
-		In []: conditional_expr_converter({'anyOf': [{'allOf': ["1","2"]}, "3", {'allOf': ["1","3"]}]})
+		In []: conditional_expr_converter({'any_of': [{'all_of': ["1","2"]}, "3", {'all_of': ["1","3"]}]})
 		Out[]: [["1", "2"], "3", ["1", "3"]]
 
 		In []: conditional_expr_converter(["1", "2", ["1", "2""3"]])
 		ValueError: Unsupported format (0)
 
-		In []: conditional_expr_converter({'allOf': ["1", "2", ["1","2"]]})
+		In []: conditional_expr_converter({'all_of': ["1", "2", ["1","2"]]})
 		ValueError: Unsupported nesting
 
-		In []: conditional_expr_converter({'allOf': ["1", "2"], 'abc': "2"})
+		In []: conditional_expr_converter({'all_of': ["1", "2"], 'abc': "2"})
 		ValueError: Unsupported format ("1")
 
-		In []: conditional_expr_converter({'anyOf': [{'anyOf': ["1","2"]}, "2"]})
+		In []: conditional_expr_converter({'any_of': [{'any_of': ["1","2"]}, "2"]})
 		ValueError: Unsupported nesting
 
-		In []: conditional_expr_converter({'anyOf': [{'allOf': ["1","2"]}, "3", {'anyOf': ["1","2"]}]})
+		In []: conditional_expr_converter({'any_of': [{'all_of': ["1","2"]}, "3", {'any_of': ["1","2"]}]})
 		ValueError: Unsupported nesting
 
-		In []: conditional_expr_converter({'allOf': [{'allOf': ["1","2"]}, "3", "1", "2"]})
+		In []: conditional_expr_converter({'all_of': [{'all_of': ["1","2"]}, "3", "1", "2"]})
 		ValueError: Unsupported nesting
 
-		In []: conditional_expr_converter({'anyOf': [{'allOf': ["1","2"]}, "3", {'allOf': ["1",{'allOf':["1","2"]}]}]})
+		In []: conditional_expr_converter({'any_of': [{'all_of': ["1","2"]}, "3", {'all_of': ["1",{'all_of':["1","2"]}]}]})
 		ValueError: Unsupported nesting
 		```
 		"""
@@ -298,19 +298,19 @@ class LogicSchemaUtils:
 
 			key = next(iter(arg.keys()))
 
-			if key == "allOf":
+			if key == "all_of":
 
 				# Value must be a sequence
 				if not isinstance(arg[key], sequences):
 					raise ValueError("Unsupported format (3)")
 
-				# 'allOf' closes nesting (content must be atomar elements of type 'ok')
+				# 'all_of' closes nesting (content must be atomar elements of type 'ok')
 				if not check_seq_inner_type(arg[key], ok, multi_type=True):
 					raise ValueError("Unsupported nesting")
 
 				return [arg[key]] if level == 1 else arg[key]
 
-			if key == "anyOf":
+			if key == "any_of":
 
 				if level > 1:
 					raise ValueError("Unsupported nesting")
