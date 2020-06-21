660cb0394899ce9db127bf09fd0f636fc3a8cf66
vbrinnel <valery.brinnel@gmail.com>
2020-06-21 14:55:14 +0200

    AmpelBuffer: use TypedDicts from ampel.content

--- ampel/core/AmpelBuffer.py
+++ ampel/core/AmpelBuffer.py
@@ -4,23 +4,28 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 31.05.2018
-# Last Modified Date: 14.06.2020
+# Last Modified Date: 17.06.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Optional, Dict, List, Any, TypedDict, Literal
 from ampel.type import StockId
+from ampel.content.StockRecord import StockRecord
+from ampel.content.DataPoint import DataPoint
+from ampel.content.Compound import Compound
+from ampel.content.T2Record import T2Record
+from ampel.content.LogRecord import LogRecord
 
 # Please update BufferKey on AmpelBuffer udpates
 # There is currently unfortunately no way of extracting a Literal out of a TypedDict
-BufferKey = Literal['id', 'stock', 't0', 't1', 't2', 'logs', 'extra']
+BufferKey = Literal['id', 'stock', 't0', 't1', 't2', 'log', 'extra']
 
 class AmpelBuffer(TypedDict, total=False):
 
-	# could stock be of type List[Dict[str, Any]] for hybrid/dual transients ?
+	# Could stock be of type List[StockRecord] to enable hybrid/dual transients ?
 	id: StockId
-	stock: Optional[Dict[str, Any]]
-	t0: Optional[List[Dict[str, Any]]]
-	t1: Optional[List[Dict[str, Any]]]
-	t2: Optional[List[Dict[str, Any]]]
-	logs: Optional[List[Dict[str, Any]]]
+	stock: Optional[StockRecord]
+	t0: Optional[List[DataPoint]]
+	t1: Optional[List[Compound]]
+	t2: Optional[List[T2Record]]
+	log: Optional[List[LogRecord]]
 	extra: Optional[Dict[str, Any]]
