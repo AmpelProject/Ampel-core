bb8bb0d2e580edf0f7bac3941ed7e0ec240067b6
vbrinnel <valery.brinnel@gmail.com>
2020-06-22 20:33:37 +0200

    Add __init__.py in package ampel.core

---
+++ ampel/core/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-core/ampel/core/__init__.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 16.06.2020
+# Last Modified Date: 16.06.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+# flake8: noqa: F401
+from .AmpelContext import AmpelContext
+from .AdminUnit import AdminUnit
+from .UnitLoader import UnitLoader
+from .JournalUpdater import JournalUpdater
