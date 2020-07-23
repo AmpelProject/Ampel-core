03bbebc82e755d69f6c9494bbb3b35f4c278be6d
vbrinnel <valery.brinnel@gmail.com>
2020-07-23 12:28:08 +0200

    LoggingErrorReporter: import update

--- ampel/log/LoggingErrorReporter.py
+++ ampel/log/LoggingErrorReporter.py
@@ -7,46 +7,44 @@
 # Last Modified Date: 18.01.2019
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.logging.AmpelLogger import AmpelLogger
-from ampel.logging.LoggingUtils import LoggingUtils
+from ampel.log.AmpelLogger import AmpelLogger
+from ampel.log.utils import log_exception, report_exception
 
 class LoggingErrorReporter:
-	"""
-	"""
 
 	@staticmethod
 	def report(handler, e, bwe_details=None):
 		"""
-		:param DBLoggingHandler handler: 
-		:param Exception e: 
-		:param BulkWriteError bwe_details: 
+		:param DBLoggingHandler handler:
+		:param Exception e:
+		:param BulkWriteError bwe_details:
 		"""
 
-		# Print log stack using std logging 
-		logger = AmpelLogger.get_unique_logger()
+		# Print log stack using std logging
+		logger = AmpelLogger.get_logger()
 
-		LoggingUtils.log_exception(logger, e, msg="Primary exception:")
+		log_exception(logger, e, msg="Primary exception:")
 
 		if bwe_details:
 			logger.error("BulkWriteError details:")
 			logger.error(bwe_details)
-			logger.error("#"*52)
+			logger.error("#" * 52)
 
 		logger.error("DB log flushing error, un-flushed (json) logs below.")
-		logger.error("*"*52)
+		logger.error("*" * 52)
 
 		for d in handler.log_dicts:
 			logger.error(str(d))
-		logger.error("#"*52)
+		logger.error("#" * 52)
 
-		try: 
+		try:
 			# This will fail as well if we have DB connectivity issues
-			LoggingUtils.report_exception(
-				logger, e, tier=0, run_id=handler.get_run_id(),
+			report_exception(
+				logger, e, run_id=handler.get_run_id(),
 				info = None if bwe_details is None else {'BulkWriteError': str(bwe_details)}
 			)
 		except Exception as ee:
-			LoggingUtils.log_exception(
+			log_exception(
 				logger, ee, last=True,
 				msg="Could not update troubles collection as well (DB offline?)"
 			)
