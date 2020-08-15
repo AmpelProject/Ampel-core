bf47444eab15d5a5964eab579e94c8f376a92708
vbrinnel <valery.brinnel@gmail.com>
2020-08-15 11:46:50 +0200

    New module: ampel.util.t1

---
+++ ampel/util/t1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-core/ampel/util/t1.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 15.08.2020
+# Last Modified Date: 15.08.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Any, List, Union, Optional, Set, Dict, Sequence, Callable, List
+from ampel.type import ChannelId, StockId, DataPointId, datapoint_id
+from ampel.content.Compound import Compound
+from ampel.log.AmpelLogger import AmpelLogger
+
+
+def get_datapoint_ids(compound: Compound, logger: Optional[AmpelLogger] = None) -> List[DataPointId]:
+
+	dps_ids: List[DataPointId] = []
+
+	# Load each datapoint referenced by the loaded compound
+	for el in compound['body']:
+
+		# Dict means custom policy/exclusion is set for this datapoint
+		if isinstance(el, dict):
+			if 'excl' in el:
+				if logger:
+					logger.debug("Ignoring excluded datapoint", extra={'id': el['id']})
+				continue
+			dpid = el['id']
+		elif isinstance(el, datapoint_id):
+			dpid = el
+		else:
+			raise StopIteration('Invalid compound doc')
+
+		dps_ids.append(dpid)
+
+	return dps_ids
