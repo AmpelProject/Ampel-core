560827e49ec942cdf7300c15de77ce67921e68ce
Jakob van Santen <jvansanten@gmail.com>
2020-08-27 10:52:29 +0200

    DictSecretProvider: silence mypy warnings
    I obviously haven't understood PEP 484 yet. It seems to provide for the equivalent of template argument deduction, whereas what I really want here is the equivalent of an explicit template argument.

--- ampel/dev/DictSecretProvider.py
+++ ampel/dev/DictSecretProvider.py
@@ -7,8 +7,8 @@
 # Last Modified Date: 14.08.2020
 # Last Modified By  : Jakob van Santen <jakob.van.santen@desy.de>
 
-from typing import Any, Tuple
-import yaml, json
+from typing import Any, Dict, get_args, get_origin
+import yaml
 
 from ampel.abstract.AbsSecretProvider import AbsSecretProvider
 from ampel.model.Secret import Secret, T
@@ -22,25 +22,28 @@ class SecretWrapper(Secret[T]):
         return self.value
 
 
-class DictSecretProvider(AbsSecretProvider, dict):
+class DictSecretProvider(AbsSecretProvider):
 
     @classmethod
     def load(cls, path) -> 'DictSecretProvider':
         with open(path) as f:
             return cls(yaml.safe_load(f))
 
+    def __init__(self, dictlike: Dict[str,Any]) -> None:
+        self.store : Dict[str,Any] = dict(dictlike)
+
+    # FIXME: find a way to express the desired return type statically
     def get(self, key: str, type_: T) -> SecretWrapper[T]:
         try:
-            return SecretWrapper[type_](key=key, value=self[key])
+            return SecretWrapper[T](key=key, value=self.store[key])
         except KeyError:
             raise KeyError(f"Unknown secret '{key}'")
 
 
 class PotemkinSecretProvider(AbsSecretProvider):
     def get(self, key: str, type_: T) -> SecretWrapper[T]:
-        origin = getattr(type_, '__origin__', None)
-        if origin is tuple:
-            value = tuple(t() for t in type_.__args__)
+        if get_origin(type_) is tuple:
+            value = tuple(t() for t in get_args(type_))
         else:
-            value = type_()
-        return SecretWrapper[T](key=key, value=value)
+            value = type_() # type: ignore[operator]
+        return SecretWrapper[T](key=key, value=value) # type: ignore[arg-type]
