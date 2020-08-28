bf393c06a10563a7549f4770b439e927c5b62389
Jakob van Santen <jvansanten@gmail.com>
2020-08-28 13:25:47 +0200

    AbsStateT2Compiler: T2IngestModel.config is optional

--- ampel/abstract/ingest/AbsStateT2Compiler.py
+++ ampel/abstract/ingest/AbsStateT2Compiler.py
@@ -7,7 +7,7 @@
 # Last Modified Date: 08.05.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Dict, Union, Set, Tuple, List, TypeVar, Generic
+from typing import Dict, Union, Set, Tuple, List, TypeVar, Generic, Optional
 from ampel.base import abstractmethod
 from ampel.type import ChannelId
 from ampel.ingest.compile.CompilerBase import CompilerBase
@@ -21,5 +21,5 @@ class AbsStateT2Compiler(Generic[T], CompilerBase, abstract=True):
 	def compile(self,
 		chan_selection: List[Tuple[ChannelId, Union[bool, int]]],
 		compound_blueprint: T
-	) -> Dict[Tuple[str, int, Union[bytes, Tuple[bytes, ...]]], Set[ChannelId]]:
+	) -> Dict[Tuple[str, Optional[int], Union[bytes, Tuple[bytes, ...]]], Set[ChannelId]]:
 		...
