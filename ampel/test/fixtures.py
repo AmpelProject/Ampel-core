dc95eadbc0f6f6aafbdde32cbd6ae03bf9cde455
Jakob van Santen <jvansanten@gmail.com>
2020-08-30 20:16:45 +0200

    test/fixtures: remove docker-based setup

--- ampel/test/fixtures.py
+++ ampel/test/fixtures.py
@@ -2,106 +2,20 @@ from os.path import abspath, join, dirname
 from os import environ
 import pytest, json, subprocess, socket
 
-def docker_service(image, port, environ={}, mounts=[], healthcheck=None, port_mapping=None):
-	container = None
-	try:
-		cmd = ['docker', 'run', '-d', '--restart', 'always']
-		if port_mapping is None:
-			cmd += ['-P']
-		else:
-			for item in port_mapping.items():
-				cmd += ['-p', '{}:{}'.format(*item)]
-		for k, v in environ.items():
-			cmd += ['-e', '{}={}'.format(k,v)]
-		if healthcheck is not None:
-			cmd += ['--health-start-period', '1s', '--health-interval', '1s','--health-cmd', healthcheck]
-		for source, dest in mounts:
-			cmd += ['-v', '{}:{}'.format(source, dest)]
-		container = subprocess.check_output(cmd + [image]).strip()
-		
-		import time
-		if healthcheck is not None:
-			def up():
-				 status = json.loads(subprocess.check_output(['docker', 'container', 'inspect', '-f', '{{json .State.Health.Status}}', container]))
-				 return status == "healthy"
-			for i in range(120*2):
-				if up():
-					break
-				time.sleep(0.5)
-		ports = json.loads(subprocess.check_output(['docker', 'container', 'inspect', '-f', '{{json .NetworkSettings.Ports}}', container]))
-		yield int(ports['{}/tcp'.format(port)][0]['HostPort'])
-	except FileNotFoundError:
-		return pytest.skip("Docker fixture requires Docker")
-	finally:
-		if container is not None:
-			subprocess.check_call(['docker', 'container', 'stop', container],
-				stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-			subprocess.check_call(['docker', 'container', 'rm', '--volumes', container],
-				stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 @pytest.fixture(scope="session")
 def mongod():
 	if 'MONGO_HOSTNAME' in environ and 'MONGO_PORT' in environ:
 		yield 'mongodb://{}:{}'.format(environ['MONGO_HOSTNAME'], environ['MONGO_PORT'])
 	else:
-		gen = docker_service('mongo:3.6', 27017)
-		port = next(gen)
-		yield 'mongodb://localhost:{}/'.format(port)
+		pytest.skip("No Mongo instance configured")
 
 @pytest.fixture(scope="session")
 def graphite():
 	if 'GRAPHITE_HOSTNAME' in environ and 'GRAPHITE_PORT' in environ:
 		yield 'graphite://{}:{}'.format(environ['GRAPHITE_HOSTNAME'], environ['GRAPHITE_PORT'])
 	else:
-		gen = docker_service('gographite/go-graphite:latest', 2003)
-		port = next(gen)
-		yield 'graphite://localhost:{}/'.format(port)
-
-@pytest.fixture(scope="session")
-def postgres():
-	if 'ARCHIVE_HOSTNAME' in environ and 'ARCHIVE_PORT' in environ:
-		yield 'postgresql://ampel@{}:{}/ztfarchive'.format(environ['ARCHIVE_HOSTNAME'], environ['ARCHIVE_PORT'])
-	else:
-		gen = docker_service('ampelproject/postgres:10.6', 5432,
-			environ={'POSTGRES_USER': 'ampel', 'POSTGRES_DB': 'ztfarchive', 'ARCHIVE_READ_USER': 'archive-readonly', 'ARCHIVE_WRITE_USER': 'ampel-client'},
-			mounts=[(join(abspath(dirname(__file__)), 'deploy', 'production', 'initdb', 'archive'), '/docker-entrypoint-initdb.d/')],
-			healthcheck='pg_isready -U postgres -p 5432 -h `hostname` || exit 1')
-		port = next(gen)
-		yield 'postgresql://ampel@localhost:{}/ztfarchive'.format(port)
-
-@pytest.fixture(scope="session")
-def zudsarchive():
-	if 'ZUDSARCHIVE_HOSTNAME' in environ and 'ZUDSARCHIVE_PORT' in environ:
-		yield 'postgresql://ampel@{}:{}/zudsarchive'.format(environ['ZUDSARCHIVE_HOSTNAME'], environ['ZUDSARCHIVE_PORT'])
-	else:
-		gen = docker_service('ampelproject/postgres:10.6', 5432,
-			environ={'POSTGRES_USER': 'ampel', 'POSTGRES_DB': 'zudsarchive', 'ARCHIVE_READ_USER': 'archive-readonly', 'ARCHIVE_WRITE_USER': 'ampel-client'},
-			mounts=[(join(abspath(dirname(__file__)), 'deploy', 'production', 'initdb', 'zuds-archive'), '/docker-entrypoint-initdb.d/')],
-			healthcheck='pg_isready -U postgres -p 5432 -h `hostname` || exit 1')
-		port = next(gen)
-		yield 'postgresql://ampel@localhost:{}/zudsarchive'.format(port)
-
-@pytest.fixture
-def empty_archive(postgres):
-	"""
-	Yield archive database, dropping all rows when finished
-	"""
-	from sqlalchemy import select, create_engine, MetaData
-
-	engine = create_engine(postgres)
-	meta = MetaData()
-	meta.reflect(bind=engine)
-	try:
-		with engine.connect() as connection:
-			for name, table in meta.tables.items():
-				if name != 'versions':
-					connection.execute(table.delete())
-		yield postgres
-	finally:
-		with engine.connect() as connection:
-			for name, table in meta.tables.items():
-				if name != 'versions':
-					connection.execute(table.delete())
+		pytest.skip("No Graphite instance configured")
 
 import copy
 from ampel.alert.PhotoAlert import PhotoAlert
