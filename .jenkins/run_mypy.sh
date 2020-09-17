3e18ed5e25233d28aca18406c329176e5d079a0c
Jakob van Santen <jvansanten@gmail.com>
2020-09-17 13:11:21 +0200

    jenkins: work with bsd find

--- .jenkins/run_mypy.sh
+++ .jenkins/run_mypy.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 root=${1:-.}
 
 # find all directories containing Python files, and run mypy on them
-find $root -name test -prune -o -type f -name '*.py' -printf "%h\n" | sort -u | while read dir; do
+find $root -name test -prune -o -type f -name '*.py' -print0 | xargs -0 -L1 dirname | sort -u | while read dir; do
     pkg=$(echo $dir | sed -e 's|/|.|g')
     (mypy $dir | mypy2junit) > mypy.$pkg.xml
 done
