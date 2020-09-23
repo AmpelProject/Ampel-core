35557e74ed0f85cf42d110ec9d6de1c05c921f16
Jakob van Santen <jvansanten@gmail.com>
2020-09-23 21:20:53 +0200

    context: expose canonical keys as classvars

--- ampel/t3/context/T3AddLastRunTime.py
+++ ampel/t3/context/T3AddLastRunTime.py
@@ -7,7 +7,7 @@
 # Last Modified Date: 06.08.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, ClassVar
 from datetime import datetime, timedelta
 from ampel.db.query.events import get_last_run
 from ampel.t3.context.AbsT3RunContextAppender import AbsT3RunContextAppender
@@ -15,13 +15,17 @@ from ampel.t3.context.AbsT3RunContextAppender import AbsT3RunContextAppender
 
 class T3AddLastRunTime(AbsT3RunContextAppender):
 
+	key: ClassVar[str] = "last_run"
+
+	#: timedelta to search for previous run
 	lookup_range: Dict[str, int] = {'days': -7}
-	fall_back: Optional[Dict[str, int]] = None  # ex: {'days': -90}
+	#: timedelta to use if no previous run found
+	fallback: Optional[Dict[str, int]] = None  # ex: {'days': -90}
 
 	def update(self, context: Dict[str, Any]) -> None:
-
-		if 'last_run' in context:
-			self.logger.info("Field 'last_run' is already set in context dict")
+		"""Add last run time (UNIX epoch) as "last_run". """
+		if self.key in context:
+			self.logger.info(f"Field '{self.key}' is already set in context dict")
 			return
 
 		last_run = get_last_run(
@@ -34,8 +38,8 @@ class T3AddLastRunTime(AbsT3RunContextAppender):
 		if last_run is None:
 			self.logger.warn(f"Event {self.process_name}: last run time unavailable")
 			if self.fall_back:
-				context['last_run'] = (datetime.now() + timedelta(**self.fall_back)).timestamp()
-				self.logger.warn(f"Fallback last run time: {context['last_run']}")
+				context[self.key] = (datetime.now() + timedelta(**self.fallback)).timestamp()
+				self.logger.warn(f"Fallback last run time: {context[self.key]}")
 				return
 
-		context['last_run'] = last_run
+		context[self.key] = last_run
