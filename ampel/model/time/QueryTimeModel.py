AmpelProject/Ampel-core

commit d55e023a1d490d51836b9e1f8a623902b1a0005c

QueryTimeModel: truth test
This makes QueryTimeModel(None,None) equivalent to None in build_stock_query(),
restoring the (somewhat questionable) v0.6 behavior of selecting all transients
for time_last_run when no previous runs were found.

ampel/model/time/QueryTimeModel.py
@@ -19,6 +19,9 @@ class QueryTimeModel(StrictModel):
 	before: Optional[Union[int, float]] = Field(None, alias='$lt')
 	after: Optional[Union[int, float]] = Field(None, alias='$gt')
 
+	def __bool__(self) -> bool:
+		return self.before is not None and self.after is not None
+
 	def dict(self, **kwargs) -> Dict[str, Any]:
 		"""
 		Example:

commit d7753cee48ed8e7bc062f07c18fbd428d1131284

QueryTimeModel: fix truth test
Make on-sided constraints True, rather than two-sided only. Fixes a regression from d55e023a1d490d51836b9e1f8a623902b1a0005c.

ampel/model/time/QueryTimeModel.py
@@ -20,7 +20,7 @@ class QueryTimeModel(StrictModel):
 	after: Optional[Union[int, float]] = Field(None, alias='$gt')
 
 	def __bool__(self) -> bool:
-		return self.before is not None and self.after is not None
+		return self.before is not None or self.after is not None
 
 	def dict(self, **kwargs) -> Dict[str, Any]:
 		"""