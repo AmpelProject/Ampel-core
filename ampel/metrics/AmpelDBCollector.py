eb4880823d4e8e04e7aed6c89cde0841e00576c0
Jakob van Santen <jvansanten@gmail.com>
2020-12-10 12:15:13 +0100

    AmpelDBCollector: correct field name

--- ampel/metrics/AmpelDBCollector.py
+++ ampel/metrics/AmpelDBCollector.py
@@ -25,7 +25,7 @@ class AmpelDBCollector:
                     "ampel_t2_docs_queued",
                     "Number of T2 docs awaiting processing",
                     value=self.db.get_collection("t2").count_documents(
-                        {"state": T2RunState.TO_RUN}
+                        {"status": T2RunState.TO_RUN}
                     ),
                 )
             )
--- ampel/test/test_server.py
+++ ampel/test/test_server.py
@@ -49,7 +49,7 @@ async def test_db_metrics(test_client, db_collector, dev_context):
             raise ValueError(f"metric {name} not collected")
 
     await check_metric("ampel_t2_docs_queued", 0)
-    dev_context.db.get_collection("t2").insert_one({"state": T2RunState.TO_RUN})
+    dev_context.db.get_collection("t2").insert_one({"status": T2RunState.TO_RUN})
     await check_metric("ampel_t2_docs_queued", 1)
 
 
