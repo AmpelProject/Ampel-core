bbc8d687390f0ad86381282aeb8d88789e5e303a
vbrinnel <valery.brinnel@gmail.com>
2021-03-20 11:31:23 +0100

    AbsProcessorUnit: remove method new_run_id()
    Now defined in AmpelContext

--- ampel/abstract/AbsProcessorUnit.py
+++ ampel/abstract/AbsProcessorUnit.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 04.02.2020
-# Last Modified Date: 18.06.2020
+# Last Modified Date: 18.03.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Any, Dict, Optional
@@ -21,15 +21,19 @@ class AbsProcessorUnit(AdminUnit, abstract=True):
 
 	#: name of the process that instantiated this processor
 	process_name: str
+
 	#: channels associated with this processor
 	channel: Optional[ChannelId] = None
 
 	#: raise exceptions instead of catching and logging
 	raise_exc: bool = False
+
 	#: logging configuration to use, from the logging section of the Ampel config
 	log_profile: str = "default"
+
 	#: flag to apply to all log records created
 	base_log_flag: LogFlag = LogFlag.SCHEDULED_RUN
+
 	#: additional kwargs to pass to :class:`~ampel.log.handlers.DBLoggingHandler.DBLoggingHandler`
 	db_handler_kwargs: Optional[Dict[str, Any]] = None
 
@@ -40,18 +44,3 @@ class AbsProcessorUnit(AdminUnit, abstract=True):
 		Run processing steps.
 		"""
 		...
-
-
-	def new_run_id(self) -> int:
-		"""
-		Return an identifier that can be used to associate log entries from a
-		single process invocation. This ID is unique and monotonicaly increasing.
-		"""
-		return self.context.db \
-			.get_collection('counter') \
-			.find_one_and_update(
-				{'_id': 'current_run_id'},
-				{'$inc': {'value': 1}},
-				new=True, upsert=True
-			) \
-			.get('value')
