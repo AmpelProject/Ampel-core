1a595b30262996069e02c2849875ebe531f0c50d
vbrinnel <valery.brinnel@gmail.com>
2021-03-20 11:36:20 +0100

    ChannelConfigCollector: add version to channel dict
    It is the version of the distribution unless the channel definition
    defines (and then maintains) its own version

--- ampel/config/collector/ChannelConfigCollector.py
+++ ampel/config/collector/ChannelConfigCollector.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 16.10.2019
-# Last Modified Date: 06.02.2020
+# Last Modified Date: 19.03.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Dict, Any, Union
@@ -16,61 +16,61 @@ from ampel.log import VERBOSE
 class ChannelConfigCollector(AbsDictConfigCollector):
 
 	def add(self,
-		arg: Dict[str, Any],
+		chan_dict: Dict[str, Any],
 		dist_name: str,
 		version: Union[str, float, int],
 		register_file: str
 	) -> None:
 
-		if 'channel' not in arg:
-			self.missing_key(what='channel', key='channel', dist_name=dist_name, register_file=register_file)
+		if 'channel' not in chan_dict:
+			self.missing_key(
+				what='channel', key='channel',
+				dist_name=dist_name, register_file=register_file
+			)
 			return
 
 		try:
 
-			chan_name = arg['channel']
+			chan_name = chan_dict['channel']
 
 			if self.verbose:
 				self.logger.log(VERBOSE, f'Adding channel: {chan_name}')
 
-			if 'distrib' in arg:
-				dist_name = arg['distrib']
-			else:
-				if dist_name:
-					arg['distrib'] = dist_name
+			if 'distrib' not in chan_dict:
+				chan_dict['distrib'] = dist_name
 
-			if 'source' in arg:
-				register_file = arg['source']
-			else:
-				if register_file:
-					arg['source'] = register_file
+			if 'source' not in chan_dict:
+				chan_dict['source'] = register_file
+
+			if 'version' not in chan_dict:
+				chan_dict['version'] = version
 
 			# Check duplicated channel names
 			if self.get(chan_name):
 				self.duplicated_entry(
 					conf_key = chan_name,
-					new_file = register_file,
-					new_dist = dist_name,
+					new_file = chan_dict['source'],
+					new_dist = chan_dict['distrib'],
 					prev_file = self.get(chan_name).get('conf', 'unknown'), # type: ignore
 					prev_dist = self.get(chan_name).get('distrib', 'unknown') # type: ignore
 				)
 				return
 
-			if not ('NO_HASH' in arg.get('policy', []) or isinstance(chan_name, int)):
-				arg['hash'] = b2_short_hash(chan_name)
+			if not ('NO_HASH' in chan_dict.get('policy', []) or isinstance(chan_name, int)):
+				chan_dict['hash'] = b2_short_hash(chan_name)
 				for k, v in self.items():
-					if arg['hash'] == v.get('hash'):
+					if chan_dict['hash'] == v.get('hash'):
 						raise ValueError(
 							f'Channel name hash collision detected. '
 							f'Channel name 1: {k}. Hash value: {v.get("hash")}'
-							f'Channel name 2: {arg["channel"]}. Hash value: {arg.get("hash")}'
+							f'Channel name 2: {chan_dict["channel"]}. Hash value: {chan_dict.get("hash")}'
 						)
 
-			self.__setitem__(chan_name, arg)
+			self.__setitem__(chan_name, chan_dict)
 
 		except Exception as e:
 			self.error(
 				f'Error occured while loading channel config {self.distrib_hint(dist_name, register_file)}. '
-				f'Offending value: {arg}',
+				f'Offending value: {chan_dict}',
 				exc_info=e
 			)
