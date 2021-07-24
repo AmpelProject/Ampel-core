c0b63109195c55c94e8d44300f885a0b48ffa70a
vbrinnel <valery.brinnel@gmail.com>
2021-07-24 15:58:04 +0200

    AbsT3Filter: type hint update

--- ampel/t3/stage/filter/AbsT3Filter.py
+++ ampel/t3/stage/filter/AbsT3Filter.py
@@ -4,16 +4,18 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 12.02.2020
-# Last Modified Date: 20.06.2020
+# Last Modified Date: 21.04.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Sequence
-from ampel.base import AmpelABC, AmpelBaseModel, abstractmethod
+from typing import Sequence, Iterable
+from ampel.base.AmpelABC import AmpelABC
+from ampel.base.decorator import abstractmethod
+from ampel.base.AmpelBaseModel import AmpelBaseModel
 from ampel.struct.AmpelBuffer import AmpelBuffer
 
 
 class AbsT3Filter(AmpelBaseModel, AmpelABC, abstract=True):
 
 	@abstractmethod
-	def filter(self, abufs: Sequence[AmpelBuffer]) -> Sequence[AmpelBuffer]:
+	def filter(self, abufs: Iterable[AmpelBuffer]) -> Sequence[AmpelBuffer]:
 		...
