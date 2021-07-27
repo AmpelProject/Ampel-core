cb67dd8fff5f8cd7686d337ecbb52f0fb4f128b5
vbrinnel <valery.brinnel@gmail.com>
2021-07-27 16:59:58 +0200

    AbsT3Selector: update stock field identifier

--- ampel/abstract/AbsT3Selector.py
+++ ampel/abstract/AbsT3Selector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-core/ampel/t3/select/AbsT3Selector.py
+# File              : Ampel-core/ampel/abstract/AbsT3Selector.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 09.12.2019
@@ -8,18 +8,16 @@
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Iterable, ClassVar, Optional
-from ampel.base import abstractmethod
-from ampel.core.AdminUnit import AdminUnit
+from ampel.base.AmpelABC import AmpelABC
+from ampel.base.decorator import abstractmethod
+from ampel.core.ContextUnit import ContextUnit
 
 
-class AbsT3Selector(AdminUnit, abstract=True):
+class AbsT3Selector(AmpelABC, ContextUnit, abstract=True):
 
 	#: field used to identify stocks
-	field_name: ClassVar[str] = "_id"
+	field_name: ClassVar[str] = "stock"
 
 	@abstractmethod
 	def fetch(self) -> Optional[Iterable]:
-		"""
-		Get selected stock ids
-		"""
-		...
+		""" Get selected stock ids """
