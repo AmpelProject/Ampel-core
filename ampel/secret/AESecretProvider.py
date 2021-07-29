42b1c84d59624348825696c8b8da9bd1edd424b8
vbrinnel <valery.brinnel@gmail.com>
2021-07-29 13:00:26 +0200

    New class: AESecretProvider

---
+++ ampel/secret/AESecretProvider.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-core/ampel/secret/AEAbsSecretProvider.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 20.06.2021
+# Last Modified Date: 20.06.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from sjcl import SJCL
+from typing import Union, Iterable
+from ampel.abstract.AbsSecretProvider import AbsSecretProvider
+from ampel.secret.AESecret import AESecret
+from ampel.abstract.Secret import Secret
+
+
+class AESecretProvider(AbsSecretProvider):
+
+	def __init__(self, pwds: Union[str, Iterable[str]]):
+		self.sjcl = SJCL()
+		self.pwds = [pwds] if isinstance(pwds, str) else pwds
+
+
+	def tell(self, arg: Secret) -> bool:
+		"""
+		Potentially update an initialized Secret instance with
+		the actual sensitive information associable with it.
+		:returns: True if the Secret was told/resolved or
+		False if the provided Secret is unknown to this secret provider
+		"""
+
+		if isinstance(arg, AESecret):
+			for pwd in self.pwds:
+				try:
+					arg.set(
+						self.sjcl \
+							.decrypt(arg.dict(), pwd) \
+							.decode("utf-8")
+					)
+					return True
+				except Exception:
+					continue
+
+		return False
