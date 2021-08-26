163771b5ae2037c727f0362312488b75caf6ba71
Jakob van Santen <jvansanten@gmail.com>
2021-08-26 11:16:49 +0200

    Add ChainedT0Muxer
    for composing multiplexers

---
+++ ampel/ingest/ChainedT0Muxer.py
@@ -0,0 +1,42 @@
+from typing import List, Tuple, Optional
+from ampel.types import DataPointId, StockId
+from ampel.content.DataPoint import DataPoint
+
+from ampel.abstract.AbsT0Muxer import AbsT0Muxer
+from ampel.model.UnitModel import UnitModel
+
+
+class ChainedT0Muxer(AbsT0Muxer):
+    """
+    A sequence of muxers
+    """
+
+    muxers: list[UnitModel]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._muxers = [
+            self.context.loader.new_context_unit(
+                model=model, logger=self.logger, sub_type=AbsT0Muxer
+            )
+            for model in self.muxers
+        ]
+
+    def process(
+        self, dps: List[DataPoint], stock_id: Optional[StockId] = None
+    ) -> Tuple[Optional[List[DataPoint]], Optional[List[DataPoint]]]:
+        """
+        :returns: (to_insert,to_combine), where to_insert is the union of the insertable points
+          returned by each muxer, and to_combine is the final datapoint sequence
+        """
+        
+        dps_to_insert: dict[tuple[DataPointId, Optional[int]], DataPoint] = dict()
+        dps_to_combine: Optional[list[DataPoint]] = dps
+        
+        for muxer in self._muxers:
+            insert, dps_to_combine = muxer.process(dps_to_combine, stock_id)
+            for dp in (insert or []):
+                dps_to_insert[(dp["id"], dp.get("origin"))] = dp
+        
+        return list(dps_to_insert.values()) or None, dps_to_combine
