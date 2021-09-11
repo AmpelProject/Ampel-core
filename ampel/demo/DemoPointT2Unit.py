b8eca63040dfe1b4e4ed65af5290e8ad8d25acda
vbrinnel <valery.brinnel@gmail.com>
2021-09-11 15:17:03 +0200

    DemoPointT2Unit: randomly customize code and tag

--- ampel/demo/DemoPointT2Unit.py
+++ ampel/demo/DemoPointT2Unit.py
@@ -4,11 +4,12 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 25.03.2020
-# Last Modified Date: 30.05.2021
+# Last Modified Date: 11.09.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from time import time
 from typing import Union
+from random import randint
 from ampel.types import UBson
 from ampel.struct.UnitResult import UnitResult
 from ampel.content.DataPoint import DataPoint
@@ -20,8 +21,14 @@ class DemoPointT2Unit(AbsPointT2Unit):
 	test_parameter: int = 1
 
 	def process(self, datapoint: DataPoint) -> Union[UBson, UnitResult]:
-		return {
+
+		ret = {
 			"datapoint_id": datapoint['id'],
 			"time": time(),
 			"test_parameter": self.test_parameter
 		}
+
+		if randint(0, 1) > 0.5:
+			return ret
+		else:
+			return UnitResult(code=10, tag="MY_CUSTOM_TAG", body=ret)
