b1ff222e5b2a0b39f232c1cc44c9cac57d177d80
vbrinnel <valery.brinnel@gmail.com>
2021-09-19 11:07:56 +0200

    DemoT3Unit: add demo parameter

--- ampel/demo/DemoT3Unit.py
+++ ampel/demo/DemoT3Unit.py
@@ -4,7 +4,7 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 09.06.2020
-# Last Modified Date: 17.06.2021
+# Last Modified Date: 19.09.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Union, Generator
@@ -17,9 +17,11 @@ from ampel.view.SnapView import SnapView
 
 class DemoT3Unit(AbsT3Unit[SnapView]):
 
+	parameter: int = 10
+
 	def process(self, gen: Generator[SnapView, JournalAttributes, None]) -> Union[UBson, UnitResult]:
 
-		self.logger.info("DemoT3Unit output:")
+		self.logger.info(f"DemoT3Unit output (parameter={self.parameter}):")
 
 		for v in gen:
 
