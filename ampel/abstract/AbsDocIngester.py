9aa36fc21f5436b6cbdae1c793b0117a1073fa28
vbrinnel <valery.brinnel@gmail.com>
2021-10-09 11:58:05 +0200

    AbsDocIngester: update abstractmethod ingest() arguments

--- ampel/abstract/AbsDocIngester.py
+++ ampel/abstract/AbsDocIngester.py
@@ -4,10 +4,10 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 10.03.2020
-# Last Modified Date: 28.05.2021
+# Last Modified Date: 09.10.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Dict, Optional, Any, Generic, Union
+from typing import Dict, Optional, Any, Generic
 from ampel.types import T
 from ampel.base.AmpelABC import AmpelABC
 from ampel.base.decorator import abstractmethod
@@ -20,7 +20,7 @@ class AbsDocIngester(Generic[T], AmpelABC, AmpelBaseModel, abstract=True):
 	updates_buffer: DBUpdatesBuffer
 
 	@abstractmethod
-	def ingest(self, doc: T, now: Union[int, float]) -> None:
+	def ingest(self, doc: T) -> None:
 		...
 
 	def get_stats(self, reset: bool = True) -> Optional[Dict[str, Any]]:
