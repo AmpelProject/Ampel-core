c346481b22e523d2a9d94da4d596d193fd8333e8
vbrinnel <valery.brinnel@gmail.com>
2021-10-09 11:56:55 +0200

    AbsCompiler: add 'meta' build methods

--- ampel/abstract/AbsCompiler.py
+++ ampel/abstract/AbsCompiler.py
@@ -4,15 +4,28 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.05.2021
-# Last Modified Date: 01.10.2021
+# Last Modified Date: 08.10.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Optional, Literal, Sequence, Union, List, Set
-from ampel.types import Tag
+from typing import Optional, Literal, Sequence, Union, Dict, FrozenSet, Tuple, Set, Any, List
+from ampel.types import Tag, ChannelId
 from ampel.base.AmpelABC import AmpelABC
 from ampel.base.decorator import abstractmethod
 from ampel.base.AmpelBaseModel import AmpelBaseModel
 from ampel.abstract.AbsDocIngester import AbsDocIngester
+from ampel.content.MetaRecord import MetaRecord
+from ampel.content.MetaActivity import MetaActivity
+from ampel.enum.MetaActionCode import MetaActionCode
+
+
+# Type alias
+ActivityRegister = Dict[
+	FrozenSet[Tuple[str, Any]], # activity stripped out of channel
+	Union[
+		None, # activity is not channel-bound
+		Set[ChannelId] # will be added to MetaActivity during commit
+	]
+]
 
 
 class AbsCompiler(AmpelABC, AmpelBaseModel, abstract=True):
@@ -26,46 +39,170 @@ class AbsCompiler(AmpelABC, AmpelBaseModel, abstract=True):
 	def __init__(self, **kwargs) -> None:
 		super().__init__(**kwargs)
 		self._tag = None
+		self._ingest_tag_activity = None
 		if self.tag:
+			self._ingest_tag_activity = {'action': MetaActionCode.ADD_INGEST_TAG}
 			if isinstance(self.tag, (str, int)):
 				self._tag = [self.tag]
+				self._ingest_tag_activity['tag'] = self.tag
 			else:
 				self._tag = list(self.tag)
-
-
-	def merge_tags(self,
-		tag1: Union[Tag, Union[List[Tag], Set[Tag]]],
-		tag2: Union[Tag, Union[List[Tag], Set[Tag]]]
-	) -> Union[Tag, List[Tag]]:
-
-		if isinstance(tag1, (int, str)):
-			if isinstance(tag2, (int, str)):
-				if tag2 != tag1:
-					return [tag1, tag2]
-				return tag1
-			else:
-				if tag1 in tag2:
-					return tag2 if isinstance(tag2, list) else list(tag2)
-				l = list(tag2)
-				l.append(tag1)
-				return l
-		else:
-			if isinstance(tag2, (int, str)):
-				if tag2 in tag1:
-					return tag1 if isinstance(tag1, list) else list(tag1)
-				l = list(tag1)
-				l.append(tag2)
-				return l
-			else:
-				s = tag1 if isinstance(tag1, set) else set(tag1)
-				s.update(tag2)
-				return list(s)
+				self._ingest_tag_activity['tag'] = self._tag
 
 
 	@abstractmethod(var_args=True)
 	def add(self) -> None:
 		...
 
+
 	@abstractmethod
 	def commit(self, ingester: AbsDocIngester, now: Union[int, float]) -> None:
 		...
+
+
+	def register_meta_info(self,
+		ar: ActivityRegister,
+		extra_register: Dict[str, Any], # meta_extra
+		channel: ChannelId,
+		activity: Optional[Union[MetaActivity, List[MetaActivity]]] = None,
+		meta_extra: Optional[Dict[str, Any]] = None
+	) -> None:
+		"""
+		We could support the type List[Tuple[str, any]] for the parameter activity
+		as the dict form is actually superfluous (frozenset(dict.items()) is used in the end)
+		but the performance gain is negligible (~80ns) and it complicates static typing
+		"""
+
+		# "Add chan" activity 'key' (used in activity dict below)
+		add_chan = frozenset([('action', MetaActionCode.ADD_CHANNEL)])
+
+		if z := ar.get(add_chan):
+			z.add(channel)
+		else:
+			ar[add_chan] = {channel}
+
+		if activity:
+
+			for el in [activity] if isinstance(activity, dict) else activity:
+
+				# Channel-bound activity
+				if 'channel' in el:
+
+					# Strip out 'channel' from activity
+					# (to be able to merge similar activities accross channels)
+					x = frozenset([y for y in el.items() if y[0] != 'channel'])
+
+					# activity is already associated with another channel
+					if x in ar:
+
+						# Should not happen unless ingestion handler has an erroneous behavior
+						if (g := ar[x]) is None:
+							# Raising an error is probably a bad idea actually
+							raise ValueError("Channel-less / channel-bound activity conflict [0]")
+
+						else:
+							# Add current channel to previously registered activity
+							if isinstance(el['channel'], (int, str)):
+								g.add(el['channel'])
+							else:
+								g.update(el['channel'])
+					else:
+						ar[x] = {el['channel']} if isinstance(el['channel'], (int, str)) else set(el['channel'])
+
+				# Channel-less activity
+				else:
+
+					# Build register key
+					x = frozenset(el.items())
+
+					# activity was already registered
+					if x in ar:
+
+						# Previously registered activity is associated with a channel
+						if ar[x] is not None:
+							# Raising an error is probably a bad idea actually
+							raise ValueError("Channel-less / channel-bound activity conflict [1]")
+
+					# register new channel-less activity
+					else:
+						# (dict key contains activity content, value is None as in channel-less)
+						ar[x] = None
+
+		if meta_extra:
+			# No collision detection implemented yet
+			extra_register.update(meta_extra)
+
+
+	def new_meta_info(self,
+		channel: ChannelId,
+		activity: Optional[Union[MetaActivity, List[MetaActivity]]] = None,
+		meta_extra: Optional[Dict[str, Any]] = None
+	) -> Tuple[ActivityRegister, Dict[str, Any]]: # activity register, meta_extra
+
+		# New activity register
+		ar: ActivityRegister = {frozenset([('action', MetaActionCode.ADD_CHANNEL)]): {channel}}
+
+		if activity:
+			for el in [activity] if isinstance(activity, dict) else activity:
+				if 'channel' in el: # Channel-bound activity
+					k: FrozenSet[Tuple[str, Any]] = frozenset([y for y in el.items() if y[0] != 'channel'])
+					ar[k] = {el['channel']} if isinstance(el['channel'], (int, str)) else set(el['channel'])
+				else: # Channel-less activity
+					ar[frozenset(el.items())] = None
+
+		return ar, meta_extra.copy() if meta_extra else {}
+
+
+	def build_meta(self,
+		d: Dict[
+			FrozenSet[Tuple[str, Any]], # key: traceid
+			Tuple[ActivityRegister, Dict[str, Any]] # activity register, meta_extra
+		],
+		now: Union[int, float]
+	) -> Tuple[List[MetaRecord], List[Tag]]:
+
+		recs: List[MetaRecord] = []
+		tags = set(self._tag) if self._tag else set()
+
+		# v[1]: dict[traceid, (activity register, meta_extra)]
+		for tid, t in d.items():
+
+			mrec: MetaRecord = {
+				'run': self.run_id,
+				'ts': now,
+				'tier': self.tier
+			}
+
+			# add meta extra (for example: alert: 12345678)
+			mrec.update(t[1]) # type: ignore[typeddict-item]
+			al = []
+
+			for activity, chan in t[0].items():
+
+				ad = dict(
+					sorted(
+						(
+							list(activity) +
+							[("channel", next(iter(chan)) if len(chan) == 1 else list(chan))]
+						)
+						if chan else activity
+					)
+				)
+
+				if 'tag' in ad:
+					if isinstance(ad['tag'], list):
+						tags.update(ad['tag'])
+					else:
+						tags.add(ad['tag'])
+
+				al.append(ad)
+
+			# report ingest tag activity (only to first metarecord)
+			if len(recs) == 0 and self._tag:
+				al.append(self._ingest_tag_activity) # type: ignore[arg-type]
+
+			mrec['activity'] = al # type: ignore[typeddict-item]
+			mrec['traceid'] = dict(sorted(tid))
+			recs.append(mrec)
+
+		return recs, list(tags)
