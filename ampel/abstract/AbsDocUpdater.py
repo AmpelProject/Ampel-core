eb7c70014b1e39085588615e1519daf2476bbffd
vbrinnel <valery.brinnel@gmail.com>
2021-10-10 13:13:25 +0200

    AbsDocUpdater: let update() work for any tier

--- ampel/abstract/AbsDocUpdater.py
+++ ampel/abstract/AbsDocUpdater.py
@@ -4,14 +4,13 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 28.05.2021
-# Last Modified Date: 28.05.2021
+# Last Modified Date: 10.10.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Dict, Any, Literal
 from ampel.base.AmpelABC import AmpelABC
 from ampel.base.decorator import abstractmethod
 from ampel.base.AmpelBaseModel import AmpelBaseModel
-from ampel.mongo.update.DBUpdatesBuffer import DBUpdatesBuffer
 
 
 class AbsDocUpdater(AmpelABC, AmpelBaseModel, abstract=True):
@@ -20,9 +19,12 @@ class AbsDocUpdater(AmpelABC, AmpelBaseModel, abstract=True):
 	Not in use yet
 	"""
 
-	tier: Literal[0, 1, 2, 3]
-	updates_buffer: DBUpdatesBuffer
-
 	@abstractmethod
-	def update(self, match: Dict[str, Any], set: Dict[str, Any], push: Dict[str, Any], add_to_set: Dict[str, Any]) -> None:
+	def update(self,
+		tier: Literal[0, 1, 2],
+		match: Dict[str, Any],
+		let: Dict[str, Any],
+		push: Dict[str, Any],
+		add_to_set: Dict[str, Any]
+	) -> None:
 		...
