e83f8e6901816eb900982fd24e85ef39005a6372
vbrinnel <valery.brinnel@gmail.com>
2021-11-25 20:39:41 +0100

    Bump version: 0.8.1a8

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ entry_points = {
 
 setup(
     name = 'ampel-core',
-    version = '0.8.1.alpha-7',
+    version = '0.8.1.alpha-8',
     description = 'Asynchronous and Modular Platform with Execution Layers',
     author = 'Valery Brinnel',
     maintainer = 'Jakob van Santen',
