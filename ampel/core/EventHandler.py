c7476ed6d19a1864be77540cd55908d01548d70d
vbrinnel <valery.brinnel@gmail.com>
2021-12-15 00:14:50 +0100

    EventHandler: add raise_exc and dry_run options and handle_error method

--- ampel/core/EventHandler.py
+++ ampel/core/EventHandler.py
@@ -4,42 +4,62 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 26.09.2018
-# Last Modified Date: 31.08.2021
+# Last Modified Date: 13.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from time import time
-from typing import Dict, Any, Optional, Literal, TYPE_CHECKING
+from bson import ObjectId
+from typing import Any, Optional, Literal, TYPE_CHECKING
 from ampel.content.EventDocument import EventDocument
 from ampel.log.AmpelLogger import AmpelLogger
 from ampel.log.AmpelLoggingError import AmpelLoggingError
+from ampel.log.utils import report_exception
 
 if TYPE_CHECKING:
 	from ampel.core.AmpelDB import AmpelDB
 
+
+# TODO: (much later) remove explicit dependency on pymongo
 class EventHandler:
-	""" Handles the creation and publication of event documents into the event database """
+	"""
+	Handles the creation and publication of event documents into the event database
+	"""
 
 	def __init__(self,
-		ampel_db: 'AmpelDB', process_name: str, tier: Literal[-1, 0, 1, 2, 3],
-		run_id: Optional[int] = None, col_name: str = "events", extra: Optional[Dict[str, Any]] = None
+		process_name: str,
+		ampel_db: 'AmpelDB',
+		tier: Literal[-1, 0, 1, 2, 3],
+		run_id: Optional[int] = None,
+		col_name: str = "events",
+		extra: Optional[dict[str, Any]] = None,
+		raise_exc = False,
+		dry_run: bool = False
 	):
 		"""
 		:param col_name: name of db collection to use (default 'events').
 		"""
 
 		self.process_name = process_name
-		self.col = ampel_db.get_collection(col_name)
+		self.db = ampel_db
+		self.raise_exc = raise_exc
+		self.dry_run = dry_run
 		doc = EventDocument(process=process_name, tier=tier)
 
 		if run_id:
 			doc['run'] = run_id
 
 		if extra:
-			doc = {**extra, **doc} # type: ignore[misc]
+			doc |= extra # type: ignore[assignment]
 
 		self.dkeys = doc.keys()
-		self.extra: Optional[Dict[str, Any]] = None
-		self.ins_id = self.col.insert_one(doc).inserted_id
+		self.extra: Optional[dict[str, Any]] = None
+
+		if dry_run:
+			self.col = None
+			self.ins_id = ObjectId()
+		else:
+			self.col = ampel_db.get_collection(col_name)
+			self.ins_id = self.col.insert_one(doc).inserted_id
 
 
 	def add_extra(self, overwrite: bool = False, **extra) -> None:
@@ -54,10 +74,23 @@ class EventHandler:
 			self.extra[k] = v
 
 
+	def handle_error(self, e: Exception, logger: AmpelLogger) -> None:
+
+		self.add_extra(overwrite=True, success=False)
+
+		if self.raise_exc:
+			raise e
+
+		report_exception(
+			self.db, logger, exc=e,
+			info={'process': self.process_name}
+		)
+
+
 	def update(self, logger: AmpelLogger, save_duration: bool = True, **kwargs) -> None:
 		""" :raises: AmpelLoggingError """
 
-		upd: Dict[str, Any] = {}
+		upd: dict[str, Any] = {}
 
 		if self.extra:
 			for k, v in self.extra.items():
@@ -77,12 +110,13 @@ class EventHandler:
 		elif not upd:
 			return
 
-		res = self.col.update_one({'_id': self.ins_id}, {'$set': upd})
+		if not self.dry_run:
+			res = self.col.update_one({'_id': self.ins_id}, {'$set': upd}) # type: ignore[union-attr]
 
-		if res.modified_count == 0 and res.upserted_id is None:
-			raise AmpelLoggingError(
-				"Events collection update failed (%s)" % {
-					'mongoUpdateResult': res.raw_result,
-					'process': self.process_name
-				}
-			)
+			if res.modified_count == 0 and res.upserted_id is None:
+				raise AmpelLoggingError(
+					"Events collection update failed (%s)" % {
+						'mongoUpdateResult': res.raw_result,
+						'process': self.process_name
+					}
+				)
