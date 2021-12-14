b59b7fe7e474d2bcee0b3103046ad89324a8e0c0
vbrinnel <valery.brinnel@gmail.com>
2021-12-14 23:46:09 +0100

    ContextUnit: adopt Traceless

--- ampel/core/ContextUnit.py
+++ ampel/core/ContextUnit.py
@@ -4,14 +4,16 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.10.2019
-# Last Modified Date: 19.09.2021
+# Last Modified Date: 13.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Optional
+from ampel.types import Traceless, TRACELESS
 from ampel.base.AmpelBaseModel import AmpelBaseModel
 from ampel.core.AmpelContext import AmpelContext
-from ampel.log.AmpelLogger import AmpelLogger
 from ampel.secret.Secret import Secret
+
+ttf = type(Traceless)
 
 
 class ContextUnit(AmpelBaseModel):
@@ -19,24 +21,28 @@ class ContextUnit(AmpelBaseModel):
 	Base class for units requiring a reference to an AmpelContext instance
 	"""
 
+	context: Traceless[AmpelContext]
+
 	#: Private variable potentially set by UnitLoader for provenance purposes. Either:
 	#: * None if provanance flag is False
 	#: * 0 in case model content is not serializable
 	#: * any other signed int value
 	_trace_id: Optional[int] = None
 
-	def __init__(self, context: AmpelContext, **kwargs) -> None:
 
-		if context is None:
-			raise ValueError("Parameter context cannot be None")
+	def __init__(self, **kwargs) -> None:
 
 		super().__init__(**kwargs)
 
 		d = self.__dict__
+		excl = {
+			k for k, v in self._annots.items()
+			if type(v) is ttf and v.__metadata__[0] == TRACELESS
+		}
+
 		self._trace_content = {
 			k: d[k]
 			for k in sorted(d)
-			if not isinstance(d[k], (Secret, AmpelContext, AmpelLogger))
+			if k not in excl and
+			not isinstance(d[k], Secret)
 		}
-
-		self.context = context
