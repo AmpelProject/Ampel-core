086ab5a237bdd0fa46e58f0269b2c9d63c5dae33
vbrinnel <valery.brinnel@gmail.com>
2021-12-18 23:09:26 +0100

    AbsT3ControlUnit: require EventHandler and channel (optional)

--- ampel/abstract/AbsT3ControlUnit.py
+++ ampel/abstract/AbsT3ControlUnit.py
@@ -4,26 +4,30 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 08.12.2021
-# Last Modified Date: 12.12.2021
+# Last Modified Date: 17.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Optional, Generator
-from ampel.types import Traceless
+from ampel.types import Traceless, OneOrMany, ChannelId
 from ampel.view.T3Store import T3Store
 from ampel.base.AmpelABC import AmpelABC
 from ampel.base.decorator import abstractmethod
 from ampel.core.ContextUnit import ContextUnit
+from ampel.core.EventHandler import EventHandler
 from ampel.content.T3Document import T3Document
 from ampel.log.AmpelLogger import AmpelLogger
 
 
 class AbsT3ControlUnit(AmpelABC, ContextUnit, abstract=True):
 	"""
-	Generic abstract class for control T3 units receiving only a
-	T3Store instance as argument in process() method
+	Abstract class for control T3 units, which like AbsT3PlainUnit but
+	unlike AbsT3ReviewUnit, receive just a T3Store instance via
+	the method process()
 	"""
 
 	logger: Traceless[AmpelLogger]
+	event_hdlr: Traceless[EventHandler]
+	channel: Optional[OneOrMany[ChannelId]] = None
 
 
 	@abstractmethod
