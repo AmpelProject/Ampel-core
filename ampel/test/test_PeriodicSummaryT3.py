5057474518c857a517a466d8d369e4fe18c2ab67
vbrinnel <valery.brinnel@gmail.com>
2021-12-18 23:14:09 +0100

    test_PeriodicSummaryT3: follow DemoReviewT3Unit rename

--- ampel/test/test_PeriodicSummaryT3.py
+++ ampel/test/test_PeriodicSummaryT3.py
@@ -18,7 +18,7 @@ def test_validate(core_config, loader_directives, ampel_logger):
                 "schedule": "every().day.at('15:00')",
                 "channel": "FOO",
                 "load": loader_directives,
-                "run": {"unit": "DemoT3Unit"},
+                "run": {"unit": "DemoReviewT3Unit"},
             }
         )
         .get_process(config, ampel_logger) | {"version": 0}
@@ -36,7 +36,7 @@ def test_single_element_run_sequence(core_config, ampel_logger):
                 "schedule": "every().day.at('15:00')",
                 "channel": {"any_of": ["HU_GP_10", "HU_GP_59"]},
                 "load": ["TRANSIENT", "DATAPOINT", "T2RECORD"],
-                "run": [{"unit": "DemoT3Unit", "config": {}}],
+                "run": [{"unit": "DemoReviewT3Unit", "config": {}}],
             }
         )
         .get_process(config, ampel_logger) | {"version": 0}
