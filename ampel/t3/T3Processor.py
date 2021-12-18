eed1767c11ad59bbee5939ea9742f9fb09fb3297
vbrinnel <valery.brinnel@gmail.com>
2021-12-18 23:15:44 +0100

    T3Processor: v0.8.2 final

--- ampel/t3/T3Processor.py
+++ ampel/t3/T3Processor.py
@@ -4,54 +4,27 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 26.02.2018
-# Last Modified Date: 12.12.2021
+# Last Modified Date: 17.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from pydantic import BaseModel
-from typing import Any, Optional, Union, Sequence
+from importlib import import_module
+from typing import Any, Optional, Annotated
+
+from ampel.types import OneOrMany
 from ampel.abstract.AbsEventUnit import AbsEventUnit
 from ampel.abstract.AbsT3ControlUnit import AbsT3ControlUnit
-from ampel.t3.T3PlainUnitExecutor import T3PlainUnitExecutor
-from ampel.t3.T3ReviewUnitExecutor import T3ReviewUnitExecutor
+from ampel.abstract.AbsProcessorTemplate import AbsProcessorTemplate
 from ampel.view.T3Store import T3Store
 from ampel.view.T3DocView import T3DocView
 from ampel.model.UnitModel import UnitModel
+from ampel.model.t3.T3IncludeDirective import T3IncludeDirective
 from ampel.core.EventHandler import EventHandler
-from ampel.mongo.update.MongoStockUpdater import MongoStockUpdater
 from ampel.abstract.AbsT3Supplier import AbsT3Supplier
 from ampel.log import AmpelLogger, LogFlag, SHOUT
 
 
-class IncludeModel(BaseModel):
-	"""
-	:param session: models for AbsT3Supplier[dict] instances which populates the 'session' field of T3Store
-	Examples of session information are:
-	- Date and time the current process was last run
-	- Number of alerts processed since then
-	"""
-
-	#: Provides Iterable[T3Document]
-	docs: Optional[UnitModel]
-
-	#: Provides session information. Unit(s) must be a subclass of AbsT3Supplier
-	session: Union[None, Sequence[UnitModel], UnitModel]
-
-
-class T3ReviewDirective(BaseModel):
-	"""
-	Internal model used to configure T3ReviewUnitExecutor
-	"""
-
-	#: Unit must be a subclass of AbsT3Supplier
-	supply: UnitModel
-
-	#: Unit must be a subclass of AbsT3Stager
-	stage: UnitModel
-
-
 class T3Processor(AbsEventUnit):
 	"""
-	:param update_journal: Record the invocation of this event in the stock journal
 	:param update_events: Record this event in the events collection
 	:param extra_journal_tag:
 	  tag(s) to add to the :class:`~ampel.content.JournalRecord.JournalRecord`
@@ -63,51 +36,43 @@ class T3Processor(AbsEventUnit):
 	  the state of the database:
 	
 	  - use a log_profile without db logging (profiles are defined under 'logging' in the ampel config)
-	  - set update_journal = False (no update to the transient doc)
 	  - set update_events = False (no update to the events collection)
 	  - set raise_exc = True (troubles collection will not be populated if an exception occurs)
 	"""
 
-	include: Optional[IncludeModel]
+	template: Optional[str] = None
+	include: Optional[T3IncludeDirective]
+	execute: OneOrMany[Annotated[UnitModel, AbsT3ControlUnit]]
 
-	execute: Sequence[Union[UnitModel,T3ReviewDirective]]
 
+	def __init__(self, **kwargs) -> None:
 
-	def __init__(self,
-		update_journal: bool = True,
-		update_events: bool = True,
-		extra_journal_tag: Optional[Union[int, str]] = None,
-		**kwargs
-	) -> None:
-		"""
-		Note that update_journal, update_event and extra_journal_tag are admin run options and
-		should be set only on command line. They are thus not defined as part of the underlying model.
-		Please see class docstring for more info.
-		"""
+		if 'template' in kwargs:
+			tpl_name = kwargs.pop("template")
+			ctx = kwargs.pop("context")
+			if ctx is None:
+				raise ValueError("Context required")
 
-		self.react_first = True
-		for k in kwargs.keys():
-			if k == "react":
-				break
-			if k == "act":
-				self.react_first = False
-				break
+			if kwargs['template'] not in ctx.config._config['template']:
+				raise ValueError(f"Unknown process template: {tpl_name}")
+
+			fqn = ctx.config._config['template'][tpl_name]
+			class_name = fqn.split(".")[-1]
+			Tpl = getattr(import_module(fqn), class_name)
+			if not issubclass(Tpl, AbsProcessorTemplate):
+				raise ValueError(f"Unexpected template type: {Tpl}")
+
+			tpl = Tpl(**kwargs)
+			kwargs = tpl.get_model(ctx.config._config, kwargs).dict()
+			kwargs['context'] = ctx
 
 		super().__init__(**kwargs)
+		self.update_events = True
 
-		self.update_journal = update_journal
 
-		# Admins have the option to run a T3 process silently/anonymously
-		self.update_events = update_events
-
-		self.extra_journal_tag = extra_journal_tag
-
-		if 'db' not in self.context.config.get(f'logging.{self.log_profile}', dict, raise_exc=True):
-			for el in ('update_journal', 'update_events'):
-				if getattr(self, el):
-					raise ValueError(
-						f'{el} cannot be True without a logger associated with a db logging handler'
-					)
+	def set_no_event(self) -> None:
+		""" run t3 process \"silently\" """
+		self.update_events = False
 
 
 	def run(self) -> None:
@@ -142,9 +107,9 @@ class T3Processor(AbsEventUnit):
 
 				if (x := self.include.session):
 					sdict: dict[str, Any] = {}
-					for um in [x] if isinstance(x, UnitModel) else x:
+					for model in [x] if isinstance(x, UnitModel) else x:
 						rd = loader.new_context_unit(
-							model = um,
+							model = model,
 							context = self.context,
 							sub_type = AbsT3Supplier,
 							event_hdlr = event_hdlr,
@@ -155,54 +120,28 @@ class T3Processor(AbsEventUnit):
 					if sdict:
 						t3s.add_session_info(sdict)
 
-			stock_updr = MongoStockUpdater(
-				ampel_db = self.context.db, tier = 3, run_id = run_id,
-				process_name = self.process_name, logger = logger,
-				raise_exc = self.raise_exc, extra_tag = self.extra_journal_tag,
-				update_journal = self.update_journal,
-				bump_updated = False
-			)
+			units = self.context.config._config['unit']
+			for i, um in enumerate([self.execute] if isinstance(self.execute, UnitModel) else self.execute):
 
-			for i, model in enumerate(self.execute):
+				if um.unit not in units:
+					raise ValueError(f"Unknown unit: {um.unit}")
 
-				if isinstance(model, UnitModel):
+				if "AbsT3ControlUnit" not in units[um.unit]['base']:
+					raise ValueError("T3Processor executes only AbsT3ControlUnit units")
 
-					# Native AbsT3ControlUnit
-					if "ContextUnit" in self.context.config.get(f"unit.{model.unit}.base", tuple, raise_exc=True): # type: ignore
-						t3_unit = loader.new_context_unit(
-							model = model,
-							context = self.context,
-							sub_type = AbsT3ControlUnit,
-							logger = logger
-						)
+				t3_unit = loader.new_context_unit(
+					model = um,
+					context = self.context,
+					sub_type = AbsT3ControlUnit,
+					logger = logger,
+					event_hdlr = event_hdlr,
+					channel = self.channel
+				)
 
-					# AbsT3PlainUnit run by AbsT3ControlUnit
-					else:
-						# Dedicated T3ControlUnit
-						t3_unit = T3PlainUnitExecutor(
-							unit = model.unit,
-							config = model.config,
-							context = self.context,
-							logger = logger,
-							event_hdlr = event_hdlr,
-							stock_updr = stock_updr,
-							channel = self.channel
-						)
-
-				# AbsT3ReviewUnit run by AbsT3ControlUnit
-				else:
-
-					t3_unit = T3ReviewUnitExecutor(
-						logger = logger,
-						context = self.context,
-						event_hdlr = event_hdlr,
-						stock_updr = stock_updr,
-						channel = self.channel,
-						supply = model.supply,
-						stage = model.stage,
-					)
-
-				logger.info(f"Processing run block {i}", extra={'unit': t3_unit.__class__.__name__})
+				logger.info(
+					f"Processing run block {i}",
+					extra={'unit': t3_unit.__class__.__name__}
+				)
 
 				# Potential T3Document to be included in the t3 collection
 				if (ret := t3_unit.process(t3s)):
