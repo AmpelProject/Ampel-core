2a11a637bb44e05ce88f66df835edd52cf4f10f8
vbrinnel <valery.brinnel@gmail.com>
2021-12-18 23:13:49 +0100

    T3SimpleStager: minor type hint update

--- ampel/t3/stage/T3SimpleStager.py
+++ ampel/t3/stage/T3SimpleStager.py
@@ -4,11 +4,12 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 18.04.2021
-# Last Modified Date: 09.12.2021
+# Last Modified Date: 17.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Optional, Sequence, List, Generator, Union
+from typing import Optional, List, Generator
 
+from ampel.types import OneOrMany, Annotated
 from ampel.view.T3Store import T3Store
 from ampel.model.UnitModel import UnitModel
 from ampel.content.T3Document import T3Document
@@ -25,7 +26,7 @@ class T3SimpleStager(T3ThreadedStager):
 	"""
 
 	#: t3 units (AbsT3ReviewUnit) to execute
-	execute: Union[Sequence[UnitModel], UnitModel]
+	execute: OneOrMany[Annotated[UnitModel, AbsT3ReviewUnit]]
 
 	def __init__(self, **kwargs) -> None:
 
