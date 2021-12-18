c1c598c8cb08e5b480df90367ffe818d77f024bc
vbrinnel <valery.brinnel@gmail.com>
2021-12-18 23:12:37 +0100

    T3PlainUnitExecutor: update model: use the ampel way

--- ampel/t3/T3PlainUnitExecutor.py
+++ ampel/t3/T3PlainUnitExecutor.py
@@ -4,11 +4,11 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 12.12.2021
-# Last Modified Date: 12.12.2021
+# Last Modified Date: 17.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from time import time
-from typing import Optional, Generator, Any
+from typing import Optional, Annotated, Generator
 
 from ampel.view.T3Store import T3Store
 from ampel.abstract.AbsT3ControlUnit import AbsT3ControlUnit
@@ -20,19 +20,18 @@ from ampel.model.UnitModel import UnitModel
 
 class T3PlainUnitExecutor(AbsT3ControlUnit, T3DocBuilder):
 
-	unit: str
-	config: dict[str, Any]
+	target: Annotated[UnitModel, AbsT3PlainUnit]
 
 	def process(self, t3s: T3Store) -> Optional[Generator[T3Document, None, None]]:
 
 		t3_unit = self.context.loader.new_safe_logical_unit(
-			UnitModel(unit=self.unit, config=self.config),
+			UnitModel(unit=self.target.unit, config=self.target.config),
 			unit_type = AbsT3PlainUnit,
 			logger = self.logger,
 			_chan = self.channel
 		)
 
-		self.logger.info("Running T3unit", extra={'unit': self.unit})
+		self.logger.info("Running T3unit", extra={'unit': self.target.unit})
 		ret = t3_unit.process(t3s)
 		self.flush(t3_unit)
 		if ret:
