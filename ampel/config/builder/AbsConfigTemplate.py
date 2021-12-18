ee75dc63dadd6e03f3743cb1217f54a5cbf5adef
vbrinnel <valery.brinnel@gmail.com>
2021-12-18 23:09:59 +0100

    AbsConfigTemplate: inherit AmpelBaseModel

--- ampel/config/builder/AbsConfigTemplate.py
+++ ampel/config/builder/AbsConfigTemplate.py
@@ -4,15 +4,15 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 14.04.2020
-# Last Modified Date: 09.05.2020
+# Last Modified Date: 18.12.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Optional
 from ampel.base.AmpelABC import AmpelABC
-from ampel.model.StrictModel import StrictModel
+from ampel.base.AmpelBaseModel import AmpelBaseModel
 
 
-class AbsConfigTemplate(AmpelABC, StrictModel, abstract=True):
+class AbsConfigTemplate(AmpelABC, AmpelBaseModel, abstract=True):
 	""" Known direct subclasses: AbsProcessTemplate, AbsChannelTemplate """
 
 	template: Optional[str]
