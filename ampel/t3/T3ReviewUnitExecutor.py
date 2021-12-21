c8aaa2033dc695257bc0ae3d2a25227696130684
vbrinnel <valery.brinnel@gmail.com>
2021-12-21 18:09:18 +0100

    T3ReviewUnitExecutor: propagate T3DocBuilderModel config options to stager

--- ampel/t3/T3ReviewUnitExecutor.py
+++ ampel/t3/T3ReviewUnitExecutor.py
@@ -14,6 +14,7 @@ from ampel.abstract.AbsT3ControlUnit import AbsT3ControlUnit
 from ampel.t3.T3DocBuilder import T3DocBuilder
 from ampel.content.T3Document import T3Document
 from ampel.model.UnitModel import UnitModel
+from ampel.model.t3.T3DocBuilderModel import T3DocBuilderModel
 from ampel.abstract.AbsT3Supplier import AbsT3Supplier
 from ampel.abstract.AbsT3Stager import AbsT3Stager
 from ampel.log.AmpelLogger import AmpelLogger
@@ -48,6 +49,18 @@ class T3ReviewUnitExecutor(AbsT3ControlUnit, T3DocBuilder):
 			# Stager unit
 			#############
 
+			if self.stage.unit not in self.context.config._config['unit']:
+				raise ValueError(f"Unknown stager unit: {self.stage.unit}")
+
+			if "T3DocBuilder" in self.context.config._config['unit'][self.stage.unit]['base']:
+				kwargs = {
+					k: getattr(self, k)
+					for k in T3DocBuilderModel._annots
+					if hasattr(self, k) and not (k in self._defaults and getattr(self, k) == self._defaults[k])
+				}
+			else:
+				kwargs = {}
+
 			stager = self.context.loader.new_context_unit(
 				model = self.stage,
 				context = self.context,
@@ -58,7 +71,8 @@ class T3ReviewUnitExecutor(AbsT3ControlUnit, T3DocBuilder):
 					self.stage.config['channel'] # type: ignore
 					if self.stage.config and self.stage.config.get('channel') # type: ignore[union-attr]
 					else self.channel
-				)
+				),
+				**kwargs
 			)
 
 			return stager.stage(supplier.supply(t3s), t3s)
