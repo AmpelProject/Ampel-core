a0dd4913999b5101470fc68b8826ca9be5e2820f
valery brinnel <valery.brinnel@gmail.com>
2022-04-23 23:46:07 +0200

    util.distrib.get_files(): fix behavior for when package is installed in editable mode with pyproject.toml present

--- ampel/util/distrib.py
+++ ampel/util/distrib.py
@@ -4,7 +4,7 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                13.03.2021
-# Last Modified Date:  13.03.2021
+# Last Modified Date:  23.04.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 import os, re
@@ -16,9 +16,7 @@ from pkg_resources import ( # type: ignore[attr-defined]
 
 
 def get_dist_names(distrib_prefix: str = "ampel-") -> list[str]:
-	"""
-	Get all installed distributions whose names start with the provided prefix
-	"""
+	""" Get all installed distributions whose names start with the provided prefix """
 	ret = [
 		dist_name for dist_name in AvailableDistributions()
 		if distrib_prefix in dist_name
@@ -31,10 +29,12 @@ def get_dist_names(distrib_prefix: str = "ampel-") -> list[str]:
 	return ret
 
 
-def get_files(dist_name: str, lookup_dir: None | str = None, pattern: None | re.Pattern = None) -> list[str]:
-	"""
-	Loads all known conf files of the provided distribution (name)
-	"""
+def get_files(
+	dist_name: str,
+	lookup_dir: None | str = None,
+	pattern: None | re.Pattern = None
+) -> list[str]:
+	""" Loads all known conf files of the provided distribution (name) """
 
 	if lookup_dir and not lookup_dir.endswith("/"):
 		lookup_dir += "/"
@@ -43,10 +43,9 @@ def get_files(dist_name: str, lookup_dir: None | str = None, pattern: None | re.
 
 	# DistInfoDistribution: look for metadata RECORD (in <dist_name>.dist-info)
 	if isinstance(distrib, DistInfoDistribution):
-		# Example of the ouput of distrib.get_metadata_lines('RECORD'):
-		# 'conf/ampel-ztf.conf,sha256=FZkChNKKpcMPTO4pwyKq4WS8FAbznuR7oL9rtNYS7U0,322',
-		# 'ampel/model/ZTFLegacyChannelTemplate.py,sha256=zVtv4Iry3FloofSazIFc4h8l6hhV-wpIFbX3fOW2njA,2182',
-		# 'ampel/model/__pycache__/ZTFLegacyChannelTemplate.cpython-38.pyc,,',
+
+		# "pip3 install" in editable mode with pyproject.toml present
+		# 1) grab the pth file
 		if pth := next(
 			(
 				pth for el in distrib.get_metadata_lines('RECORD')
@@ -54,13 +53,30 @@ def get_files(dist_name: str, lookup_dir: None | str = None, pattern: None | re.
 			),
 			None
 		):
-			return list(walk_pth_file(pth, lookup_dir, pattern))
+
+			fname = pth if os.path.isfile(pth) else distrib.get_resource_filename(__name__, pth)
+			# 2) Manually look for files in referenced folder
+			with open(fname, "r") as f:
+				return list(
+					walk_dir(
+						f.read().strip(),
+						lookup_dir,
+						pattern
+					)
+				)
+
+		# <pip3 install .> in non-editable mode with pyproject.toml present
+		# Example of the ouput of distrib.get_metadata_lines('RECORD'):
+		# 'conf/ampel-ztf.conf,sha256=FZkChNKKpcMPTO4pwyKq4WS8FAbznuR7oL9rtNYS7U0,322',
+		# 'ampel/model/ZTFLegacyChannelTemplate.py,sha256=zVtv4Iry3FloofSazIFc4h8l6hhV-wpIFbX3fOW2njA,2182',
+		# 'ampel/model/__pycache__/ZTFLegacyChannelTemplate.cpython-38.pyc,,',
 		else:
 			return [
 				fname for el in distrib.get_metadata_lines('RECORD')
 				if _check_match((fname := el.split(",")[0]), lookup_dir, pattern)
 			]
 
+	# "pip3 install" in editable mode without pyproject.toml present
 	elif isinstance(distrib, EggInfoDistribution):
 		# Example of the ouput of distrib.get_metadata_lines('SOURCES.txt'):
 		# 'setup.py',
@@ -75,7 +91,11 @@ def get_files(dist_name: str, lookup_dir: None | str = None, pattern: None | re.
 		raise ValueError(f"Unsupported distribution type: '{type(distrib)}'")
 
 
-def _check_match(arg: str, lookup_dir: None | str, pattern: None | re.Pattern) -> bool:
+def _check_match(
+	arg: str,
+	lookup_dir: None | str,
+	pattern: None | re.Pattern
+) -> bool:
 	if lookup_dir and not arg.startswith(lookup_dir):
 		return False
 	if pattern and not pattern.match(arg):
@@ -83,10 +103,13 @@ def _check_match(arg: str, lookup_dir: None | str, pattern: None | re.Pattern) -
 	return True
 
 
-def walk_pth_file(pth: str, lookup_dir: None | str = None, pattern: None | re.Pattern = None) -> Generator[str, None, None]:
-	with open(pth) as f:
-		for root, dirs, files in os.walk(f"{f.read().strip()}/{lookup_dir}" if lookup_dir else f"{f.read().strip()}"):
-			for fname in files:
-				if pattern and not pattern.match(fname):
-					continue
-				yield os.path.join(root, fname)
+def walk_dir(
+	path: str,
+	lookup_dir: None | str = None,
+	pattern: None | re.Pattern = None
+) -> Generator[str, None, None]:
+	for root, dirs, files in os.walk(f"{path}/{lookup_dir}" if lookup_dir else f"{path}"):
+		for fname in files:
+			if pattern and not pattern.match(fname):
+				continue
+			yield os.path.join(root, fname)
