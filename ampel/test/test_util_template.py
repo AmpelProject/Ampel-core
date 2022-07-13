0a44a6c56ab9bdc3ba5440fcab95facf169f2ef3
Jakob van Santen <jvansanten@gmail.com>
2022-07-13 11:47:57 +0200

    tests: adjust number of units in testing config

--- ampel/test/test_util_template.py
+++ ampel/test/test_util_template.py
@@ -51,5 +51,5 @@ def all_units(first_pass_config: dict[str, Any]) -> list[UnitModel]:
     return [UnitModel(unit=name) for name in first_pass_config["unit"].keys()]
 
 def test_filter_units(all_units: list[UnitModel], first_pass_config: dict[str, Any]):
-    assert len(filter_units(all_units, "LogicalUnit", first_pass_config)) == 10
+    assert len(filter_units(all_units, "LogicalUnit", first_pass_config)) == 11
 
