161cb5122aed95e8260b4b9ac33e7b1d3bad9de5
valery brinnel <valery.brinnel@gmail.com>
2022-08-26 18:52:15 +0200

    Bump version: v0.8.3b1

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ entry_points = {
 
 setup(
     name = 'ampel-core',
-    version = '0.8.3.alpha-28',
+    version = '0.8.3.beta-1',
     description = 'Alice in Modular Provenance-Enabled Land',
     author = 'Valery Brinnel',
     maintainer = 'Jakob van Santen',
