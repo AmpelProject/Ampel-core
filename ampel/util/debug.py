ab331b000fc2b4042134b7619f8ddcacad9f0473
valery brinnel <valery.brinnel@gmail.com>
2022-09-26 14:04:30 +0200

    util.debug: add MockPool

--- ampel/util/debug.py
+++ ampel/util/debug.py
@@ -4,10 +4,10 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                24.07.2022
-# Last Modified Date:  17.08.2022
+# Last Modified Date:  24.09.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
-import sys, pdb, traceback
+import os, sys, pdb, traceback, multiprocessing.pool
 
 """
 Usage:
@@ -18,12 +18,37 @@ from ampel.util.debug import trace_prints
 -> trace_prints()
 """
 
+
+class MockPool(multiprocessing.pool.Pool):
+
+	def __init__(self, **kwargs):
+		self._terminate = self._check_running = lambda: True
+		if 'initializer' in kwargs:
+			kwargs['initializer'](*kwargs['initargs'])
+
+	def apply_async(self, func, args=(), kwds=None):
+		return MockFuture(func, args, kwds)
+
+
+class MockFuture:
+
+	def __init__(self, func, args=(), kwds=None):
+		self.func = func
+		self.args = args
+		self.kwds = kwds
+
+	def get(self, **kwargs):
+		if isinstance(self.kwds, dict):
+			return self.func(**self.kwds)
+		return self.func(*self.args)
+
+
 # https://stackoverflow.com/questions/26289153/how-to-use-ipdb-set-trace-in-a-forked-process
 class ForkedPdb(pdb.Pdb):
 	"""
 	A Pdb subclass that may be used within a multiprocessing process
 
-	Forword: If you are not using multiprocessing, just use breakpoint()
+	Forword: If you do not use multiprocessing, just use breakpoint()
 
 	Usage:
 	insert: "ForkedPdb().set_trace()" at the wished position
@@ -32,13 +57,13 @@ class ForkedPdb(pdb.Pdb):
 	<do debug>
 	when you are done, press CTRL-D
 	you are back in the pdb shell, type "continue" and press enter
-	Hope this helps
 	"""
 
 	def interaction(self, *args, **kwargs):
 		_stdin = sys.stdin
 		try:
 			sys.stdin = open('/dev/stdin')
+			print(f"ForkedPdb started (pid: {os.getpid()})")
 			pdb.Pdb.interaction(self, *args, **kwargs)
 		finally:
 			sys.stdin = _stdin
@@ -47,7 +72,7 @@ class ForkedPdb(pdb.Pdb):
 # https://stackoverflow.com/questions/1617494/finding-a-print-statement-in-python
 class TracePrints:
 
-	def __init__(self):    
+	def __init__(self):
 		self.stdout = sys.stdout
 
 	def write(self, s):
@@ -62,6 +87,7 @@ def trace_prints():
 def stop_trace_prints():
 	original_stdout.flush()
 	sys.stdout = original_stdout
+
 
 mp_breakpoint = ForkedPdb().set_trace
 original_stdout = sys.stdout
