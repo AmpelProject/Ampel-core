178bac9940e8b1f7e519c5de20a3f21d7fc3f2df
valery brinnel <valery.brinnel@gmail.com>
2022-11-25 16:44:48 +0100

    Bump version: v0.8.3b8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                Unspecified
-# Last Modified Date:  26.08.2022
+# Last Modified Date:  25.11.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from setuptools import setup, find_namespace_packages
@@ -54,7 +54,7 @@ entry_points = {
 
 setup(
     name = 'ampel-core',
-    version = '0.8.3.beta-7',
+    version = '0.8.3.beta-8',
     description = 'Alice in Modular Provenance-Enabled Land',
     author = 'Valery Brinnel',
     maintainer = 'Jakob van Santen',
