152f2487a9c12a2933183596f5d6ebb59bdf6a7b
valery brinnel <valery.brinnel@gmail.com>
2022-12-15 22:30:32 +0100

    DemoFirstPointT2Unit: add 'chatty' parameter

--- ampel/demo/DemoFirstPointT2Unit.py
+++ ampel/demo/DemoFirstPointT2Unit.py
@@ -4,7 +4,7 @@
 # License:             BSD-3-Clause
 # Author:              valery brinnel <firstname.lastname@gmail.com>
 # Date:                25.03.2020
-# Last Modified Date:  30.05.2021
+# Last Modified Date:  15.12.2022
 # Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
 
 from time import time
@@ -17,6 +17,9 @@ from ampel.abstract.AbsPointT2Unit import AbsPointT2Unit
 class DemoFirstPointT2Unit(AbsPointT2Unit):
 
 	ingest = {'filter': 'PPSFilter', 'sort': 'jd', 'select': 'first'}
+	chatty: bool = False
 
 	def process(self, datapoint: DataPoint) -> UBson | UnitResult:
+		if self.chatty:
+			self.logger.info(f"Parsing {datapoint['id']}")
 		return {"time": time()}
