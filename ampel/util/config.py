c840df3b7a6036191e26005427151d5d1a8a71e5
valery brinnel <valery.brinnel@gmail.com>
2023-04-06 20:55:37 +0200

    Add util.config

---
+++ ampel/util/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File:                Ampel-core/ampel/util/config.py
+# License:             BSD-3-Clause
+# Author:              valery brinnel <firstname.lastname@gmail.com>
+# Date:                06.04.2023
+# Last Modified Date:  06.04.2023
+# Last Modified By:    valery brinnel <firstname.lastname@gmail.com>
+
+from typing import Any
+from ampel.core.UnitLoader import UnitLoader
+from ampel.util.hash import build_unsafe_dict_id
+from ampel.model.UnitModel import UnitModel
+from ampel.log.AmpelLogger import AmpelLogger
+
+
+def get_unit_confid(loader: 'UnitLoader', unit: str, config: dict[str, Any]) -> int:
+	return build_unsafe_dict_id(
+		loader.new_logical_unit(
+			model = UnitModel(unit=unit, config=config),
+			logger = AmpelLogger.get_logger()
+		)._get_trace_content(),
+		ret = int
+	)
