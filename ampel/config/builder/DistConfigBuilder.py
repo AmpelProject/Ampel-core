0de845dceefc9df35309bbbe4b4fb1a6666aef78
Jakob van Santen <jvansanten@gmail.com>
2024-01-16 15:01:48 +0100

    DistConfigBuilder: use raw string for regex

--- ampel/config/builder/DistConfigBuilder.py
+++ ampel/config/builder/DistConfigBuilder.py
@@ -81,7 +81,7 @@ class DistConfigBuilder(ConfigBuilder):
 		try:
 
 			distrib = pkg_resources.get_distribution(dist_name)
-			all_conf_files = get_files(dist_name, conf_dir, re.compile(f".*\.{ext}$")) # noqa
+			all_conf_files = get_files(dist_name, conf_dir, re.compile(rf".*\.{ext}$")) # noqa
 
 			if all_conf_files and self.verbose:
 				self.logger.log(VERBOSE, "Following conf files will be parsed:")
