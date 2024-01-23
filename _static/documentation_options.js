e4174dc4877afd2a405a8df5bb66542cf43e9480
jvansanten <jvansanten@users.noreply.github.com>
2024-01-23 13:23:57 +0000

    deploy: 10347ab3738859cb4294c60166edb0836bfd6da9

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.8.9a0',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.10.0a4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
